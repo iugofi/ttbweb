376cc69a06298f660490c7be50b3ed3205a2ff56
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-04-23 15:07:46 +0530

    resources\views\User\total_internet.blade.php

--- resources/views/User/total_internet.blade.php
+++ resources/views/User/total_internet.blade.php
@@ -16,7 +16,7 @@
                         <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 col-span-12">
                             <div class="lg:py-[3rem]">
                                 <p class="landing-banner-heading mb-4 opacity-[0.9] pt-5"><span class="first-color">A single solution to all <br> your security needs.</p>
-                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] dark-text">Holisticly procrastinate mission-critical convergence with reliable customer service. Assertively underwhelm idea-sharing for impactful solutions.
+                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] text-dark">Holisticly procrastinate mission-critical convergence with reliable customer service. Assertively underwhelm idea-sharing for impactful solutions.
                                 </div>  
                                 <a href="index.html" class="m-1 ti-btn ti-btn-primary-full">
                                     Download Now
@@ -45,7 +45,7 @@
          <!-- Start::second Content -->
          <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem] third-back internet2-section">
          <div class="sectiontwo-heading pb-5 pt-5" style="text-align: center;">
-            <p class="landing-banner-heading mb-4 opacity-[0.9] pt-5"><span class="first-color" style="font-size:40px; font-weight:600;">Our Work Process</p>
+            <h2 class="landing-banner-heading mb-4 opacity-[0.9] pt-5"><span class="text-dark" style="font-size:40px; font-weight:600;">Our Work Process</h2>
                 <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] dark-text pb-5">Holisticly procrastinate mission-critical convergence with reliable customer service.<br> Assertively underwhelm idea-sharing for impactful solutions.
             </div>
             <div class="container main-banner-container section-two-icon">
