5cdfc26502a5887a31182237de81104008b7d772
“kunalverma320” <kunalverma320@gmail.com>
2024-04-24 09:29:58 +0530

    resources\views\User\Layouts\nav.blade.php

--- resources/views/User/Layouts/nav.blade.php
+++ resources/views/User/Layouts/nav.blade.php
@@ -168,6 +168,12 @@
                             class="ri-settings-3-line animate-spin-slow"></i></a>
                 </div>
                 @endif
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch"
+                        id="flexSwitchCheckDefault">
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Default switch
+                        checkbox input</label>
+                </div>
             </nav>
             <!-- End::nav -->
 
