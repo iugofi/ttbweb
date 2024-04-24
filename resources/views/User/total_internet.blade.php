068549ad99945d5129524876253dcc8ec1b70f1d
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-04-24 16:00:01 +0530

    resources\views\User\total_internet.blade.php

--- resources/views/User/total_internet.blade.php
+++ resources/views/User/total_internet.blade.php
@@ -179,7 +179,7 @@
 
          <!-- Start::forth Content -->
          <section class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]" id="features">
-            <div class="container main-banner-container">
+            <div class="container main-banner-container text-center">
                     <p class="card-text mb-4 pb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa iusto perspiciatis rerum ex sint? Possimus<br> harum aliquid molestiae voluptates. Laborum totam explicabo maiores qui culpa?<br> Corporis corrupti aperiam cupiditate officia.</p>
                 <div class="grid grid-cols-12 gap-6 forth-s-main">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
