ea9f5907380840ad16697d1422e66ce1ec333313
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-04-25 14:33:39 +0530

    resources\views\User\vpnshield.blade.php

--- resources/views/User/vpnshield.blade.php
+++ resources/views/User/vpnshield.blade.php
@@ -56,7 +56,7 @@
                                     </div>
                                     <div class="position-absolute p-4 w-75 rounded-custom d-flex secondary-bg hero-animated-card-2">
                                         <div class="small-card-img mr-3 text-white">
-                                            <img src="assets/img/client/1.jpg" alt="" width="80px" class="rounded-circle img-fluid">
+                                            <img src="assets/img/shape-1-1.jpg" alt="" width="80px" class="rounded-circle img-fluid">
                                         </div>
                                         <p class="gr-text-11 mb-0 text-white">“Ensuring the best return on investment for business”</p>
                                     </div>
