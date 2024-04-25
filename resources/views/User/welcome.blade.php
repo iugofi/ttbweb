f85970730d2475ed01dfdfdb9994ac35ad4b153b
neeraj <tbs.neeraj@gmail.com>
2024-04-25 15:53:05 +0530

    assets\css\style.css resources\views\User\welcome.blade.php

--- resources/views/User/welcome.blade.php
+++ resources/views/User/welcome.blade.php
@@ -645,15 +645,15 @@
       id="features">
       <div class="container main-banner-container">
         <div class="grid grid-cols-12 gap-6">
-            <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 col-span-12">
+            <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
                 <div class="lg:py-[3rem]">
                     <h2 class="landing-banner-heading mb-4 opacity-[0.9]"><span >What Our Client Say <br> About Apdash</h2>
-                    <div class="text-[1rem] mb-[3rem] !text-dark opacity-[0.9] ">Collaboratively actualize excellent schemas without effective <br> models. Synergistically engineer functionalized applications rather br than backend e-commerce.</div>
+                    <div class="text-[1rem] mb-[3rem] !text-dark opacity-[0.9] ">Collaboratively actualize excellent schemas without effective <br> models. Synergistically engineer functionalized applications rather<br> than backend e-commerce.</div>
                    
                     
                 </div>
             </div>
-            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12">
+            <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
 
                 <div class="box custom-box slider-box">
                     <div class="box-body">
@@ -664,12 +664,12 @@
                                   
                                    
                                         <div class="item">
-                                            <div class="testimonial-quote-wrap">
-                                                <div class="media author-info mb-3">
+                                            <div class="testimonial-quote-wrap ttb-bg">
+                                                <div class="media author-info mb-3 flex ">
                                                     <div class="author-img mr-3">
                                                         <img src="assets/img/client/1.jpg" alt="client" width="10px" >
                                                     </div>
-                                                    <div class="media-body text-white">
+                                                    <div class="media-body text-white p-5">
                                                         <h5 class="mb-0 text-white">John Charles</h5>
                                                         <span>Head Of Admin</span>
                                                     </div>
@@ -687,7 +687,7 @@
                                 
                              
                             </div>
-                            <div class="swiper-pagination"></div>  
+                            <div class="swiper-pagination"></div>
                           </div>
                     </div>
                 </div>
