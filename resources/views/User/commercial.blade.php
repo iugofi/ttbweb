8be6230cf5d3785e6b86ce8042860a80b2efb0e7
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-04-26 10:35:14 +0530

    resources\views\User\commercial.blade.php

--- resources/views/User/commercial.blade.php
+++ resources/views/User/commercial.blade.php
@@ -34,7 +34,6 @@
                         <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12">
                            
                             <div class="text-end landing-main-image landing-heading-img flex justify-end w-full">
-                                <img src="{{ asset('assets/img2/section-one-ttb.gif') }}" alt="" class="img-fluid" width="88%">
                             </div>
                            
                             <div class="dark-shap-two"><img src="{{ asset('assets/img/Shape-2.png') }}" alt=""
