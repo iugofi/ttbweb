db95c8d5e3bdff7db0b6e2b231c952d31d533461
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-04-26 14:48:35 +0530

    resources\views\User\commercial.blade.php

--- resources/views/User/commercial.blade.php
+++ resources/views/User/commercial.blade.php
@@ -66,13 +66,13 @@
                             <div class="row-span-3">
                                 <img src="assets/img/Best-Services-Iocn-1.png" class="img-fluid !inline-flex backimage">
                             </div>
-                            <div class="col-span-2"><h5 class="dark-text">Best Services</h5></div>
+                            <div class="col-span-2"><h5 class="dark-text px-6">Best Services</h5></div>
                             <div class="row-span-2 col-span-2"><p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
                                 tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p></div>
                         </div>
                           <div class="grid grid-rows-2 grid-flow-col gap-4">
                             <div class="row-span-3"><img src="assets/img/Best-Services-Iocn-2.png " class="img-fluid !inline-flex backimage"></div>
-                            <div class="col-span-2"><h5 class="dark-text">Support 24/7</h5></div>
+                            <div class="col-span-2"><h5 class="dark-text px-6">Support 24/7</h5></div>
                             <div class="row-span-2 col-span-2"><p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
                                 tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p></div>
                           </div>
