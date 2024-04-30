3c8777989587368aa46560779ac2198e10a41a05
“kunalverma320” <kunalverma320@gmail.com>
2024-04-30 17:50:30 +0530

    resources\views\User\ttbantivirusnew.blade.php

---
+++ resources/views/User/ttbantivirusnew.blade.php
@@ -0,0 +1,21 @@
+
+
+@extends('User.Layouts.layout')
+
+@section('title', 'ttbantiviruenew')
+@section('Description', '')
+@section('keywords', '')
+@section('canonical', '')
+
+
+
+@section('content')
+
+
+
+
+         
+
+
+
+@endsection
