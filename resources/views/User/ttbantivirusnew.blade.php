df99c7d1587dd77f22a74fcd301b9a122e1a4fa0
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-04-30 18:01:48 +0530

    resources\views\User\commercial.blade.php resources\views\User\ttbantivirusnew.blade.php

--- resources/views/User/ttbantivirusnew.blade.php
+++ resources/views/User/ttbantivirusnew.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('User.Layouts.layout')
 
 @section('title', 'ttbantiviruenew')
@@ -8,22 +6,60 @@
 @section('canonical', '')
 
 
-
 @section('content')
 
+    <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
 
-<div class="content">
-    <div class="main-content">
-        <div class="container my-[3rem] ">
+        <!-- Start::ttbantivirus section one -->
+        <div class="commericial-banner" id="commericial">
+            <section class="section sect-ban">
+                <div class="container main-banner-container">
+                    <div class="grid grid-cols-12 gap-6">
+                        <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 col-span-12">
+                            <div class="lg:py-[3rem] text-vartical"
+                                style="position: relative; top: 7rem; padding-right: 2rem;">
+                                <h1 class="lg:text-start font-semibold mb-2 text-[2rem]" style="color: #000;">Robust Threat
+                                    Protection and
+                                    Security Framework</h1>
+                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] dark-text"
+                                    style="color: #000;">Holisticly
+                                    procrastinate mission-critical convergence with reliable customer service. Assertively
+                                    underwhelm idea-sharing for impactful solutions.
+                                    Assertively underwhelm idea-sharing for impactful solutions.
+                                </div>
+                                <a href="index.html" class="m-1 ti-btn ti-btn-primary-full">
+                                    Download Now
+                                </a>
+                            </div>
+                        </div>
+                        <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12">
 
-   
+                            <div class="text-end landing-main-image landing-heading-img flex justify-end w-full">
+                            </div>
 
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-    </div>
-</div>
-
-         
+        <!-- End::ttbantivirus section one -->
 
 
 
-@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @endsection
