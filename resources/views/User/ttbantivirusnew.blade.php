6788f48b9289987e1cbe5d94d838418e43fd28f1
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-01 09:52:54 +0530

    resources\views\User\ttbantivirusnew.blade.php

--- resources/views/User/ttbantivirusnew.blade.php
+++ resources/views/User/ttbantivirusnew.blade.php
@@ -27,12 +27,14 @@
                                     underwhelm idea-sharing for impactful solutions.
                                     Assertively underwhelm idea-sharing for impactful solutions.
                                 </div>
+                                <div class="bnr-btn text-center">
                                 <a href="index.html" class="m-1 ti-btn ti-btn-primary-full">
                                     Buy Now
                                 </a>
                                 <a href="index.html" class="m-1 ti-btn bg-white">
                                     Download Now
                                 </a>
+                            </div>
                             </div>
                         </div>
                         {{-- <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12">
