99e4e457f2f57c765dab7a9e8e1b984401060d38
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-02 15:55:35 +0530

    resources\views\User\ttbantivirusnew.blade.php

--- resources/views/User/ttbantivirusnew.blade.php
+++ resources/views/User/ttbantivirusnew.blade.php
@@ -11,7 +11,7 @@
     <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
 
         <!-- Start::ttbantivirus section one -->
-        <div class="commericial-banner" id="commericial">
+        <div class="antivirus-banner" id="commericial">
             <section class="section sect-ban">
                 <div class="container main-banner-container">
                     <div class="grid grid-cols-12 gap-6">
