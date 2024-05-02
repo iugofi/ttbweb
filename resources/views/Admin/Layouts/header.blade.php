4b25453d6be806d4f56bea31e985bb47364c2c8a
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-02 15:36:14 +0530

    resources\views\Admin\Layouts\header.blade.php

--- resources/views/Admin/Layouts/header.blade.php
+++ resources/views/Admin/Layouts/header.blade.php
@@ -39,8 +39,7 @@
 
     <!-- MAIN JS -->
     <script src="assets/js/main.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
 
 
 
