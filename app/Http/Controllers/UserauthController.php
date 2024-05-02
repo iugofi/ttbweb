c2f866c2dd6cde7ffb86c243faf59c0af41297fe
“kunalverma320” <kunalverma320@gmail.com>
2024-05-02 10:06:42 +0530

    app\Http\Controllers\UserauthController.php

--- app/Http/Controllers/UserauthController.php
+++ app/Http/Controllers/UserauthController.php
@@ -9,19 +9,25 @@ class UserauthController extends Controller
 {
     public function __construct()
     {
-       
+        $this->middleware(function ($request, $next) {
+            $this->loggedInUser = session('loggedInUser');
+
+            if ($this->loggedInUser) {
+                // Retrieve and store user data
+                $this->userData = Users::find($this->loggedInUser);
+            }
+
+            return $next($request);
+        });
     }
 
     public function myprofile(){
         $loggedInUser = session('loggedInUser');
         $user_data = Users::where('id',$loggedInUser)->first();
-        
-        if($loggedInUser==true)
-        {
-            return view('User.myprofile', ['loggedInUser' => $loggedInUser],['user_data' => $user_data]);
-        }
-        else
-        {
+
+        if ($this->loggedInUser) {
+            return view('User.myprofile', ['loggedInUser' => $this->loggedInUser, 'user_data' => $this->userData]);
+        } else {
             return redirect('/signin');
         }
         
