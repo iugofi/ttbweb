44086d8e02939103365e290906cd9f8bab1df677
“kunalverma320” <kunalverma320@gmail.com>
2024-05-02 10:07:50 +0530

    app\Http\Controllers\UserauthController.php

--- app/Http/Controllers/UserauthController.php
+++ app/Http/Controllers/UserauthController.php
@@ -13,18 +13,14 @@ class UserauthController extends Controller
             $this->loggedInUser = session('loggedInUser');
 
             if ($this->loggedInUser) {
-                // Retrieve and store user data
                 $this->userData = Users::find($this->loggedInUser);
             }
-
             return $next($request);
         });
     }
 
     public function myprofile(){
-        $loggedInUser = session('loggedInUser');
-        $user_data = Users::where('id',$loggedInUser)->first();
-
+     
         if ($this->loggedInUser) {
             return view('User.myprofile', ['loggedInUser' => $this->loggedInUser, 'user_data' => $this->userData]);
         } else {
