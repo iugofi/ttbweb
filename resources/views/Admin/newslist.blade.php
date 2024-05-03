c31e2ea2d0693cb52647dc35738caea5bd4aa229
“kunalverma320” <kunalverma320@gmail.com>
2024-05-03 17:51:20 +0530

    resources\views\Admin\newslist.blade.php

--- resources/views/Admin/newslist.blade.php
+++ resources/views/Admin/newslist.blade.php
@@ -34,56 +34,10 @@
             </div>
             <!-- Page Header Close -->
 
-            <!-- Start::row-1 -->
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xl:col-span-12 col-span-12">
-                    <div class="box custom-box">
-                        <div class="box-header">
-                            <div class="box-title">
-                                Basic Table
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <div id="grid-example1"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--End::row-1 -->
 
-            <!-- Start:: row-2 -->
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xl:col-span-12 col-span-12">
-                    <div class="box custom-box">
-                        <div class="box-header">
-                            <div class="box-title">
-                                Table With Pagination
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <div id="grid-pagination"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End:: row-2 -->
 
-            <!-- Start:: row-3 -->
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xl:col-span-12 col-span-12">
-                    <div class="box custom-box">
-                        <div class="box-header">
-                            <div class="box-title">
-                                Table With Search
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <div id="grid-search"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End:: row-3 -->
+
+
 
             <!-- Start:: row-4 -->
             <div class="grid grid-cols-12 gap-6">
@@ -102,73 +56,10 @@
             </div>
             <!-- End:: row-4 -->
 
-            <!-- Start:: row-5 -->
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xl:col-span-12 col-span-12">
-                    <div class="box custom-box">
-                        <div class="box-header">
-                            <div class="box-title">
-                                Table Loading
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <div id="grid-loading"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End:: row-5 -->
 
-            <!-- Start:: row-6 -->
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xl:col-span-12 col-span-12">
-                    <div class="box custom-box">
-                        <div class="box-header">
-                            <div class="box-title">
-                                Wide Table
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <div id="grid-wide"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End:: row-6 -->
+  
 
-            <!-- Start:: row-7 -->
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xl:col-span-12 col-span-12">
-                    <div class="box custom-box">
-                        <div class="box-header">
-                            <div class="box-title">
-                                Fixed Header
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <div id="grid-header-fixed"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End:: row-7 -->
 
-            <!-- Start:: row-8 -->
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xl:col-span-12 col-span-12">
-                    <div class="box custom-box">
-                        <div class="box-header">
-                            <div class="box-title">
-                                Hidden Columns
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <div id="grid-hidden-column"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End:: row-8 -->
 
 
     </div>
