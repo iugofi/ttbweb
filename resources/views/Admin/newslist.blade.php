5d795ef13e6b9754872ee6a72da0098b9eb8d457
“kunalverma320” <kunalverma320@gmail.com>
2024-05-06 13:27:37 +0530

    resources\views\Admin\newslist.blade.php

--- resources/views/Admin/newslist.blade.php
+++ resources/views/Admin/newslist.blade.php
@@ -60,8 +60,8 @@
                                 <thead>
                                     <tr>
                                         <th class="px-4 py-2">Id</th>
-                                        <th class="px-4 py-2">Title</th>
                                         <th class="px-4 py-2">Image</th>
+                                        <th class="px-4 py-2">Title</th>
                                         <th class="px-4 py-2">Description</th>
                                         <th class="px-4 py-2">Status</th>
                                         <th class="px-4 py-2">Action</th>
@@ -70,13 +70,16 @@
                                 <tbody>
                                     @foreach ($news as $item)
                                         <tr>
-                                            <td class="border px-4 py-2"></td>
-                                            <td class="border px-4 py-2">{{ $item->title }}</td>
+                                            @php
+                                                $id = 1;
+                                            @endphp
+                                            <td class="border px-4 py-2">{{ $id++ }}</td>
                                             <td class="border px-4 py-2"><div class="me-2">
                                                 <span class="avatar avatar-md avatar-rounded">
                                                     <img src="https://www.ttbinternetsecurity.com/admin/uploads/news/{{ $item->image }}" alt="">
                                                 </span>
                                             </div></td>
+                                            <td class="border px-4 py-2">{{ $item->title }}</td>                              
                                             <td class="border px-4 py-2">{!! nl2br(e(Str::limit(strip_tags($item->description), 50))) !!}</td>
                                             <td class="border px-4 py-2">{{ $item->status }}</td>
                                             <td class="border px-4 py-2"></td>
