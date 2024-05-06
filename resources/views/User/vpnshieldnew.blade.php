d4a7bec3754e9d5c79b39984465896e63d123b8c
“kunalverma320” <kunalverma320@gmail.com>
2024-05-06 11:13:15 +0530

    app\Http\Controllers\UserController.php routes\web.php resources\views\User\vpnshieldnew.blade.php

---
+++ resources/views/User/vpnshieldnew.blade.php
@@ -0,0 +1,22 @@
+
+
+@extends('User.Layouts.layout')
+
+@section('title', 'Test')
+@section('Description', '')
+@section('keywords', '')
+@section('canonical', '')
+
+
+
+@section('content')
+
+
+
+    
+
+         
+
+
+
+@endsection
