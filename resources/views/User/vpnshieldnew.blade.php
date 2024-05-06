7ac8447f9030c22f5df8a5021d61be2c2ef98f16
neeraj <tbs.neeraj@gmail.com>
2024-05-06 16:47:38 +0530

    resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshieldnew.blade.php
+++ resources/views/User/vpnshieldnew.blade.php
@@ -9,13 +9,13 @@
 
 <section class="vpn-banner-top">
 <div class="container">
-<div class="grid-cols-6">
+<div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6">
   <h1>Explore The World Wide Safely With Netric VPN</h1>
   <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.   </p>
 <a>Get Started</a>
 <a><i class="fa fa-phone">Learn More</i></a>
 </div>
-<div class="grid lg:grid-cols-6 md:grid-cols-6 sm:grid-cols-12 ">
+<div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 ">
   
 </div>
 </div>
