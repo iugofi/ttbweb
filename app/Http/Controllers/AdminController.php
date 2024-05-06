2cbc141bb6dcb4048866e85f35e5a7a52a854b85
“kunalverma320” <kunalverma320@gmail.com>
2024-05-06 13:30:05 +0530

    app\Http\Controllers\AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -38,7 +38,7 @@ class AdminController extends Controller
 
     public function newslist(){
     if ($this->loggedInAdmin) {
-        $news = News::all();
+        $news = News::orderBy('id', 'desc')->get();
 
     return view('Admin.newslist',['news'=>$news]);
     } else {
