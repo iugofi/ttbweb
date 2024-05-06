10bdf106f570152bc034b7e865bf547765f10f32
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-06 10:22:37 +0530

    resources\views\User\ttbantivirusnew.blade.php

--- resources/views/User/ttbantivirusnew.blade.php
+++ resources/views/User/ttbantivirusnew.blade.php
@@ -178,7 +178,7 @@
                                             <div class="text-end ms-5">
                                                 <p class="text-[1.5625rem] font-semibold mb-0">$199</p>
                                                 <p
-                                                    class="text-[#000] dark:text-white/1 opacity-0 text-[.6875rem] font-semibold mb-0">
+                                                    class="text-[#000] dark:text-white/50 opacity-1 text-[.6875rem] font-semibold mb-0">
                                                     per month</p>
                                             </div>
                                         </div>
