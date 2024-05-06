7875402d16d78230482e624c658ed5ec79a048fb
“kunalverma320” <kunalverma320@gmail.com>
2024-05-06 11:23:31 +0530

    resources\views\Admin\newslist.blade.php

--- resources/views/Admin/newslist.blade.php
+++ resources/views/Admin/newslist.blade.php
@@ -46,11 +46,44 @@
                             </div>
                         </div>
                         <div class="box-body">
-                            <div id="grid-sorting"></div>
+                            <!-- Container for Grid.js -->
+                            <div id="grid-container"></div>
                         </div>
                     </div>
                 </div>
             </div>
+        
+            <!-- JavaScript code to initialize Grid.js -->
+            <script>
+                // Wait for the DOM to be fully loaded
+                document.addEventListener('DOMContentLoaded', function () {
+                    // Initialize Grid.js
+                    new gridjs.Grid({
+                        // Configuration options
+                        columns: [
+                            { name: "Date", width: "150px" },
+                            { name: "Name", width: "150px" },
+                            { name: "Email", width: "200px" },
+                            { name: "ID", width: "150px" },
+                            { name: "Price", width: "100px" },
+                            { name: "Quantity", width: "100px" },
+                            { name: "Total", width: "100px" }
+                        ],
+                        // Data array
+                        data: [
+                            ["24-10-2022 12:47", "john", "john123@gmail.com", "#12012", "$1799", "1", "$1799"],
+                            ["12-09-2022 04:24", "mark", "markzenner23@gmail.com", "#12013", "$2479", "2", "$4958"],
+                            ["18-11-2022 18:43", "eoin", "eoin1992@gmail.com", "#12014", "$769", "1", "$769"],
+                            ["10-09-2022 10:35", "sarahcdd", "sarahcdd129@gmail.com", "#12015", "$1299", "3", "$3997"],
+                            ["27-10-2022 09:55", "afshin", "afshin@example.com", "#12016", "$1449", "1", "$1449"]
+                        ],
+                        // Target element to render the grid
+                        // Ensure that the ID matches the container element's ID
+                        // In this case, it's 'grid-container'
+                        // Change this if your container ID is different
+                    }).render(document.getElementById("grid-container"));
+                });
+            </script>
             <!-- End:: row-4 -->
 
 
@@ -63,42 +96,7 @@
 </div>
 
 
-<script>
-    new gridjs.Grid({
-        pagination: true,
-        search: true,
-        sort: true,
-        columns: [{
-            name: "Date",
-            width: "150px",
-        }, {
-            name: "Name",
-            width: "150px",
-        }, {
-            name: "Email",
-            width: "200px",
-        }, {
-            name: "ID",
-            width: "150px",
-        }, {
-            name: "Price",
-            width: "100px",
-        }, {
-            name: "Quantity",
-            width: "100px",
-        }, {
-            name: "Total",
-            width: "100px",
-        }],
-        data: [
-            ["24-10-2022 12:47", "john", "john123@gmail.com", "#12012", "$1799", "1", "$1799"],
-            ["12-09-2022 04:24", "mark", "markzenner23@gmail.com", "#12013", "$2479", "2", "$4958"],
-            ["18-11-2022 18:43", "eoin", "eoin1992@gmail.com", "#12014", "$769", "1", "$769"],
-            ["10-09-2022 10:35", "sarahcdd", "sarahcdd129@gmail.com", "#12015", "$1299", "3", "$3997"],
-            ["27-10-2022 09:55", "afshin", "afshin@example.com", "#12016", "$1449", "1", "$1449"]
-        ],
-    }).render(document.getElementById("grid-sorting"));;
-</script>
+
 
 
      <!-- Grid JS -->
