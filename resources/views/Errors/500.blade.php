3f84cc7c908b1aacccd29257a7171ef4438958b0
“kunalverma320” <kunalverma320@gmail.com>
2024-05-07 13:17:40 +0530

    resources\views\Errors\500.blade.php

--- resources/views/Errors/500.blade.php
+++ resources/views/Errors/500.blade.php
@@ -55,7 +55,7 @@
                                         <p class="mb-0 opacity-[0.7]">We are sorry for the inconvenience,The page you are trying to access has been removed or never been existed.</p>
                                     </div>
                                 </div>
-                                <a href="index.html" class="ti-btn bg-primary text-white font-semibold"><i class="ri-arrow-left-line align-middle inline-block"></i>BACK TO HOME</a>
+                                <a href="{{route('user.index')}}" class="ti-btn bg-primary text-white font-semibold"><i class="ri-arrow-left-line align-middle inline-block"></i>BACK TO HOME</a>
                             </div>
                             <div class="xl:col-span-3"></div>
                         </div>
