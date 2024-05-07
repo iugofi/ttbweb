5f3c969ca1b0cfd36f65d7f9062c88e0b5f3bfa7
“kunalverma320” <kunalverma320@gmail.com>
2024-05-07 10:49:52 +0530

    resources\views\Admin\Layouts\header.blade.php

--- resources/views/Admin/Layouts/header.blade.php
+++ resources/views/Admin/Layouts/header.blade.php
@@ -71,6 +71,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
     integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 
 
