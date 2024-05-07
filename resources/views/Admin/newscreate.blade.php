34585f98a9220977d701853efe5a20cc55f4c70e
“kunalverma320” <kunalverma320@gmail.com>
2024-05-07 10:07:03 +0530

    resources\views\Admin\newscreate.blade.php

--- resources/views/Admin/newscreate.blade.php
+++ resources/views/Admin/newscreate.blade.php
@@ -7,6 +7,13 @@
 @section('content')
 
 
+<style>
+    .ck-blurred.ck.ck-content.ck-editor__editable.ck-rounded-corners.ck-editor__editable_inline {
+    padding: 100px;
+}
+</style>
+
+
 
 
 <div class="content">
@@ -74,7 +81,7 @@
                               
                                 <div class="xl:col-span-12 col-span-12">
                                     <label class="form-label">News Discription</label>
-                                    <textarea id="content" name="content" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" rows="4" cols="50" required></textarea>
+                                    <textarea id="content" name="content" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" rows="6" required></textarea>
                                 </div>
                                
                                 <div class="xl:col-span-12 col-span-12 blog-images-container">
