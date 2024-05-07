5356bbf28929b7f9a162f8844e2a9eb1c8136f9c
“kunalverma320” <kunalverma320@gmail.com>
2024-05-07 12:37:33 +0530

    app\Http\Controllers\AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -95,7 +95,8 @@ public function savenews(Request $request){
             if ($request->hasFile('news_images')) {
                 $image = $request->file('news_images');
                 $imageName = time().'.'.$image->getClientOriginalExtension(); 
-                $image->move('assets/images/dailynews', $imageName);
+                $image->move(public_path('assets/images/dailynews'), $imageName);
+                dd($image);
 
                 $news = new News();
                 $news->title = $request->news_title;
@@ -109,7 +110,7 @@ public function savenews(Request $request){
                 $news->status =$request->news_status;
                 $news->image=$imageName;
                 $news->save();
-
+                
                 } else {
                     return response()->json([
                         'status' => 400,
