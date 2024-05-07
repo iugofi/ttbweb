5ccf91f2f26af23d267cc366478dfb048cdb0d13
neeraj <tbs.neeraj@gmail.com>
2024-05-07 10:29:42 +0530

    resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshieldnew.blade.php
+++ resources/views/User/vpnshieldnew.blade.php
@@ -11,7 +11,7 @@
 
         <div class="container main-banner-container">
             <div class="grid grid-cols-12 gap-6">
-                <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 col-span-12">
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
                     <div class="lg:py-[3rem]">
                         <h2>Explore The World Wide Easy With Netric VPN</h2>
                         <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
@@ -23,7 +23,7 @@
                         <span class="pl-2 text-black-light wtc-spn"> Watch Now</span>
                     </div>
                 </div>
-                <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12">
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
                    
                     <div class="text-end landing-main-image landing-heading-img flex justify-end w-full">
                         <img src="{{ asset('assets/img/ttb vpn shield.png') }}" alt="" class="img-fluid" width="100%">
