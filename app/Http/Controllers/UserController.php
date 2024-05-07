2ac612df86c9d428eb0a5db14e7e571373ff8f00
“kunalverma320” <kunalverma320@gmail.com>
2024-05-07 14:20:57 +0530

    app\Http\Controllers\UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -204,7 +204,7 @@ class UserController extends Controller
     public function newspage()
     {
         try {
-            $newsscroll = News::orderBy('id', 'desc')->where('status', 101)->get()->random(8);
+            $newsscroll = News::orderBy('id', 'desc')->random(8)->where('status', 101)->get();
             $newsscroll2 = News::orderBy('id', 'desc')->where('status', 101)->take(2)->get();
             $newsscroll3 = News::where('id', '<=', function ($query) {
                 $query->select('id')
