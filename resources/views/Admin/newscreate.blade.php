531085cde21b6a64c2f790562d35267526ae37e6
“kunalverma320” <kunalverma320@gmail.com>
2024-05-08 14:30:07 +0530

    resources\views\Admin\newscreate.blade.php

--- resources/views/Admin/newscreate.blade.php
+++ resources/views/Admin/newscreate.blade.php
@@ -98,9 +98,7 @@
 
                                     <div class="xl:col-span-12 col-span-12">
                                         <label class="form-label">News Discription</label>
-                                        <textarea id="news_description" name="news_description"
-                                            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                                            rows="6"></textarea>
+                                        <div id="editor"></div>
                                         <div class="invalid-feedback"></div>
                                     </div>
 
@@ -164,15 +162,17 @@
 
         </div>
     </div>
+    <script src="https://cdn.ckeditor.com/ckeditor5/41.3.1/classic/ckeditor.js"></script>
 
     <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
     <script>
         ClassicEditor
-            .create(document.querySelector('#content'))
-            .catch(error => {
-                console.error(error);
-            });
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
     </script>
+    
 
 
 
