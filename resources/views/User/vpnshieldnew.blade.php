a576bd861434e8b576ae274f05225fa7c7d23c54
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-08 16:01:46 +0530

    resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshieldnew.blade.php
+++ resources/views/User/vpnshieldnew.blade.php
@@ -21,7 +21,7 @@
                     </a>
                     <a href="#" class="popup-youtube btn btn-white btn-circle btn-icon"><i
                             class="bx bx-play watch-icon mx-2"></i> </a>
-                    <span class="pl-2 text-white wtc-spn mx-1"> <a href="#"> Watch Now </a></span>
+                    <span class="pl-2 text-white wtc-spn mx-1"> <a href="#"> Watch Now </a></span>  
                 </div>
             </div>
             <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
