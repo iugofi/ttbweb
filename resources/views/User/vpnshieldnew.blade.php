d889e744b4bfb3dc57158ff87cf088a2cb9e2eed
neeraj <tbs.neeraj@gmail.com>
2024-05-09 11:57:07 +0530

    resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshieldnew.blade.php
+++ resources/views/User/vpnshieldnew.blade.php
@@ -88,7 +88,7 @@
                 </div>
                 <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
                     <h3>World's Best VPN Services That Provide Own DNS System</h3>
-                    <p class="mt-3 mb-5">I Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    <p class="mt-3 mb-6">I Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut
                         labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                     <p class="text-[0.75rem] font-semibold mb-0 text-[#8C9097] dark:text-white/50">Fast VPN<span
