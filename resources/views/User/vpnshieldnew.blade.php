429cb0dbfa0a6b7b053495c12747488bd6dfbeea
neeraj <tbs.neeraj@gmail.com>
2024-05-09 10:21:10 +0530

    resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshieldnew.blade.php
+++ resources/views/User/vpnshieldnew.blade.php
@@ -21,7 +21,7 @@
                     </a>
                     <a href="#" class="popup-youtube btn btn-white btn-circle btn-icon"><i
                             class="bx bx-play watch-icon mx-2"></i> </a>
-                    <span class="pl-2 text-white wtc-spn mx-1"> <a href="#"> Watch Now </a></span>  
+                    <span class="pl-2 text-white wtc-spn mx-1"> <a href="#"> Watch Now </a></span>
                 </div>
             </div>
             <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
@@ -75,7 +75,51 @@
                 <h3>Supported By World's Renowned Venture Capital's</h3>
             </div>
             <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when
+                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                    distribution of letters.</p>
+            </div>
+        </div>
+        <hr>
+        <div class="vpn-innersection-two">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
+                    <h3>World's Best VPN Services That Provide Own DNS System</h3>
+                </div>
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
+                    <p>ILorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        <h6 class="font-semibold mb-2">Project Dependencies</h6>
+    <div class="progress-stacked progress-xl mb-[3rem] flex">
+        <div class="progress-bar w-1/2"
+            >25%</div>
+        <div class="progress-bar !bg-secondary !rounded-none w-[35%]"
+            >35%</div>
+        <div class="progress-bar !bg-danger !rounded-s-none !rounded-e-full w-2/5"
+            >40%</div>
+    </div>
+    <div class="grid grid-cols-12 justify-center">
+        <div class="xl:col-span-5 col-span-12">
+            <div class="border p-4">
+                <p class="text-[0.75rem] font-semibold mb-0 text-[#8C9097] dark:text-white/50">Html<span class="ltr:float-right rtl:float-left text-[0.625rem] font-normal">25%</span></p>
+                <div class="progress progress-xs mb-4 progress-animate" >
+                    <div class="progress-bar bg-primary w-1/2">
+                    </div>
+                </div>
+                <p class="text-[0.75rem] font-semibold mb-0 text-[#8C9097] dark:text-white/50">Css<span class="ltr:float-right rtl:float-left text-[0.625rem] font-normal">35%</span></p>
+                <div class="progress progress-xs mb-4 progress-animate " >
+                    <div class="progress-bar !rounded-none !bg-secondary w-[35%]">
+                    </div>
+                </div>
+                <p class="text-[0.75rem] font-semibold mb-0 text-[#8C9097] dark:text-white/50">Js<span class="ltr:float-right rtl:float-left text-[0.625rem] font-normal">40%</span></p>
+                <div class="progress progress-xs mb-0 progress-animate " >
+                    <div class="progress-bar !rounded-e-full !bg-danger w-2/5">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                </div>
             </div>
         </div>
     </div>
