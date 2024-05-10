7a4c6dc389cc059d94808028550a82e714241c74
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-10 16:15:30 +0530

    resources\views\Admin\bloglist.blade.php

--- resources/views/Admin/bloglist.blade.php
+++ resources/views/Admin/bloglist.blade.php
@@ -28,9 +28,9 @@
             <!-- Page Header -->
             <div class="block justify-between page-header md:flex">
                 <div>
-                    <h3
+                    {{-- <h3
                         class="!text-defaulttextcolor dark:!text-defaulttextcolor/70 dark:text-white dark:hover:text-white text-[1.125rem] font-semibold">
-                        News</h3>
+                        News</h3> --}}
                 </div>
                 <ol class="flex items-center whitespace-nowrap min-w-0">
                     <li class="text-[0.813rem] ps-[0.5rem]">
@@ -38,12 +38,12 @@
                             href="javascript:void(0);">
                             Tables
                             <i
-                                class="ti ti-chevrons-right flex-shrink-0 text-[#8c9097] dark:text-white/50 px-[0.5rem] overflow-visible rtl:rotate-180"></i>
+                                class="ti ti-chevrons-right flex-shrink-0 text-[#8c9097] dark:text-white/50 px-[0.5rem] overflow-visible rtl:rotate-180"> Blog</i>
                         </a>
                     </li>
                     <li class="text-[0.813rem] text-defaulttextcolor font-semibold hover:text-primary dark:text-[#8c9097] dark:text-white/50 "
                         aria-current="page">
-                        News
+                        List
                     </li>
                 </ol>
             </div>
