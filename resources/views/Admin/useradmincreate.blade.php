7e0f5ea1a5867d2c3cd879a794566604602683ac
“kunalverma320” <kunalverma320@gmail.com>
2024-05-10 10:32:11 +0530

    resources\views\Admin\useradmincreate.blade.php

--- resources/views/Admin/useradmincreate.blade.php
+++ resources/views/Admin/useradmincreate.blade.php
@@ -125,7 +125,20 @@
                         </form>
                     </div>
                 </div>
-             
+                <div class="xxl:col-span-3 xl:col-span-12 lg:col-span-12 md:col-span-12 sm:col-span-12 col-span-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <div class="box-title">
+                                Recent Admin
+                            </div>
+                        </div>
+                        <div class="box-body" id="recentpost">
+                            <ul class="list-group">
+                                <!-- Recent posts will be dynamically added here -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!--End::row-1 -->
 
@@ -140,7 +153,60 @@
 
     <script>
         $(document).ready(function() {
-           
+            function fetchNews() {
+                $.ajax({
+                    url: "{{ route('fatch.news') }}",
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(response) {
+                        console.log(response);
+                        if (response.length > 0) {
+                            var recentPostHtml = '';
+                            $.each(response, function(index, post) {
+                                var truncatedDescription = post.description.split(' ').slice(0,
+                                    2).join(' ');
+                                var truncatedTitle = post.title.split(' ').slice(0, 2).join(
+                                ' ');
+
+                                var imageUrl =
+                                    "assets/images/dailynews/" +
+                                    post.image;
+
+                                recentPostHtml += '<li class="list-group-item">';
+                                recentPostHtml +=
+                                    '<div class="flex gap-2 flex-wrap items-center">';
+                                recentPostHtml += '<span class="avatar avatar-xl me-1">';
+                                recentPostHtml += '<img src="' + imageUrl +
+                                    '" class="img-fluid !rounded-md" alt="...">';
+                                recentPostHtml += '</span>';
+                                recentPostHtml += '<div class="flex-grow">';
+                                recentPostHtml +=
+                                    '<a href="" class="text-[0.875rem] font-semibold mb-0">' +
+                                    truncatedTitle + '</a>';
+                                recentPostHtml +=
+                                    '<p class="mb-1 popular-blog-content text-truncate">' +
+                                    truncatedDescription + '</p>';
+                                recentPostHtml +=
+                                    '<span class="text-[#8c9097] dark:text-white/50 text-[0.6875rem]">' +
+                                    post.created_at + '</span>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '<div>';
+                                recentPostHtml +=
+                                    '<button aria-label="button" type="button" class="ti-btn ti-btn-light ti-btn-sm rtl:rotate-180"><i class="ri-arrow-right-s-line"></i></button>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '</li>';
+                            });
+                            $('#recentpost .list-group').html(recentPostHtml);
+                        } else {
+                            // Handle case where no posts are returned
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        // Handle error
+                    }
+                });
+            }
 
             $('#news_form').submit(function(e) {
                 e.preventDefault();
@@ -177,13 +243,13 @@
                             $('#news_form')[0].reset();
                             removeValidationClass("#news_form");
                             $('#news_save_btn').val('Post News');
-                         
+                            fetchNews();
                         }
                     },
                     error: function(xhr, status, error) {}
                 });
             });
-       
+            fetchNews();
         });
     </script>
 
