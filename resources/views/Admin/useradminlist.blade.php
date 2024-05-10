1bf3b20333e0ccaa83876ff9d29f0cd34e6c7cdd
“kunalverma320” <kunalverma320@gmail.com>
2024-05-10 15:01:32 +0530

    resources\views\Admin\useradminlist.blade.php

--- resources/views/Admin/useradminlist.blade.php
+++ resources/views/Admin/useradminlist.blade.php
@@ -134,7 +134,10 @@
                                            
                                             <td class="border px-4 py-2">
                                                 <div class="flex flex-row items-center !gap-2 text-[0.9375rem]">
-                                                    <a aria-label="anchor" href="{{ route('edit.adminuseredit',  \Crypt::decrypt($item->id)) }}"
+                                                    @php
+                                                        $iden=\Crypt::decrypt($item->id);
+                                                    @endphp
+                                                    <a aria-label="anchor" href="{{ route('edit.adminuseredit', $iden) }}"
                                                         class="ti-btn ti-btn-icon ti-btn-wave !gap-0 !m-0 !h-[1.75rem] !w-[1.75rem] text-[0.8rem] bg-primary/10 text-primary hover:bg-primary hover:text-white hover:border-primary">
                                                         <i class="ri-edit-line"></i>
                                                     </a>
