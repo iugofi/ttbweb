87aba5c524704c153b2d5dd18f6b7c9f719ddf92
neeraj <tbs.neeraj@gmail.com>
2024-05-13 12:23:02 +0530

    assets\css\style.css resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshieldnew.blade.php
+++ resources/views/User/vpnshieldnew.blade.php
@@ -216,7 +216,7 @@
                     </div>
                 </div> --}}
                             <img src="{{ asset('assets/img/map-world.png') }}" alt="" class="img-fluid"
-                            width="100%">
+                            width="70%">
             </div>
 
         </div>
