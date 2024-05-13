bdb35e2be2d08a49dc4fd446424a9929725f49fe
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-13 14:41:25 +0530

    resources\views\User\endpoint_protection.blade.php

--- resources/views/User/endpoint_protection.blade.php
+++ resources/views/User/endpoint_protection.blade.php
@@ -104,7 +104,7 @@
                     class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6  antivirus-image text-center">
                         <div class="lg:text-start !flex items-center lg:justify-start justify-center">
-                            <img src="{{ asset('assets/img/ttb-second-section-img-2.png') }}" alt=""
+                            <img src="{{ asset('assets/img/scnd-section-img,jpg') }}" alt=""
                                 class="img-fluid">
                         </div>
                     </div>
