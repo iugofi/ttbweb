f7ac7758833e8cec09e6a6d9fb71fd7546309326
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-13 16:58:54 +0530

    resources\views\User\endpoint_protection.blade.php

--- resources/views/User/endpoint_protection.blade.php
+++ resources/views/User/endpoint_protection.blade.php
@@ -245,7 +245,7 @@
                     class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6  customize-image text-center">
                         <div class="lg:text-start !flex items-center lg:justify-start justify-center">
-                            <img src="{{ asset('assets/img/Second-Section-img.png') }}" alt="" class="img-fluid">
+                            <img src="{{ asset('assets/img/Take-the-next-step.png') }}" alt="" class="img-fluid">
                         </div>
                     </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 pt-5 pb-0 lg:px-2 !text-start  my-auto">
