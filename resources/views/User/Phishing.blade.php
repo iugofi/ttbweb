70faf6efa419219c767639460417ef5d0db32a15
neeraj <tbs.neeraj@gmail.com>
2024-05-15 14:42:39 +0530

    assets\css\style.css resources\views\User\Phishing.blade.php

--- resources/views/User/Phishing.blade.php
+++ resources/views/User/Phishing.blade.php
@@ -19,12 +19,11 @@
             <div class="container main-banner-container">
                 <div class="grid grid-cols-12 gap-6">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
+                        <h2>Lorem Ipsum is simply dummy text of the printing</h2>
                     </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-
-                        <div class="text-end landing-main-image landing-heading-img flex justify-end w-full">
-
-                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</p>
                     </div>
                 </div>
             </div>
