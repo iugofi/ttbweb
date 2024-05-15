36166bf9f9c1908783069aa4db1a8aaad27813e6
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-05-15 15:52:15 +0530

    resources\views\User\networkprotection.blade.php

--- resources/views/User/networkprotection.blade.php
+++ resources/views/User/networkprotection.blade.php
@@ -20,7 +20,7 @@
                 <div class="container network-container">
                     <div class="grid grid-cols-12 gap-6">
                         <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
-                            <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 7rem;">
+                            <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 10rem;">
                                 <h1 class="text-left font-semibold mb-2 text-[2rem]" style="color: #fff;">Smarter Network Protection<br>
                                     for business</h1>
                                 <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] text-left dark-text"
