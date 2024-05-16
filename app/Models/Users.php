202b2ea673f448d7721691052bf802388c9f7294
“kunalverma320” <kunalverma320@gmail.com>
2024-05-16 11:04:01 +0530

    app\Models\Users.php

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -8,5 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Users extends Model
 {
     protected $table = 'usersall';
+    protected $fillable = [
+        'password',
+        'firstname',
+        'lastname',
+        'email',
+        'phone',
+        'address',
+        'status',
+        'otp',
+        'activation_key',
+        'reset_tokens'
+
+
+
+    ];
     use HasFactory;
 }
