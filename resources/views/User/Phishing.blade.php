446f9ec7685a4f6d04e9bb0330ea3fc7cf00a73c
neeraj <tbs.neeraj@gmail.com>
2024-05-16 11:42:44 +0530

    resources\views\User\Phishing.blade.php

--- resources/views/User/Phishing.blade.php
+++ resources/views/User/Phishing.blade.php
@@ -13,59 +13,69 @@
     {{-- <div class="content">
     <div class="main-content"> --}}
 
-    <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
+    <section class="Phishing-banner-top">
+        <div class="container main-banner-container">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
+                    <h2>Security Analytics For Big Data.</h2>
+                </div>
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                        Aenean commodo ligula eget dolor. Aenean massa.
+                        Cum sociis natoque penatibus et magnis .</p>
+                    <a href="index.html" class="m-1 ti-btn ti-btn-primary-full">
+                        Explore More
+                    </a>
+                    <a href="#" class="popup-youtube btn btn-white btn-circle btn-icon"><i
+                            class="bx bx-play watch-icon"></i>
+                        <span class="pl-2 text-black-light wtc-spn">Watch Now</a></span>
+                </div>
+            </div>
+        </div>
+    </section>
 
-        <section class="Phishing-banner-top">
-            <div class="container main-banner-container">
-                <div class="grid grid-cols-12 gap-6">
-                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                        <h2>Lorem Ipsum is simply dummy text of the printing</h2>
+    <section class="Phishing-section-two">
+        <div class="container main-banner-container">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
+                    <div class="phishing-icon-box">
+                        <i class="fa fa-box"></i>
                     </div>
-                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</p>
+                    <h4 class="mt-5">Simply dummy text </h4>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+                        dolor.
+                        Aenean massa. </p>
+                </div>
+                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
+                    <div class="phishing-icon-box">
+                        <i class="fa fa-box"></i>
                     </div>
+                    <h4 class="mt-5">Simply dummy text</h4>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+                        dolor.
+                        Aenean massa. </p>
+                </div>
+                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
+                    <div class="phishing-icon-box">
+                        <i class="fa fa-box"></i>
+                    </div>
+                    <h4 class="mt-5">Simply dummy text</h4>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+                        dolor.
+                        Aenean massa. </p>
+                </div>
+                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
+                    <div class="phishing-icon-box">
+                        <i class="fa fa-box"></i>
+                    </div>
+                    <h4 class="mt-5">Simply dummy text</h4>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+                        dolor.
+                        Aenean massa. </p>
                 </div>
             </div>
-        </section>
-        <section class="Phishing-section-two">
-            <div class="container main-banner-container">
-                <div class="grid grid-cols-12 gap-6">
-                    <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                        <div class="phishing-icon-box">
-                            <i class="fa fa-box"></i>
-                        </div>
-                        <h4 class="mt-5">Simply dummy text </h4>
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                            Aenean massa. </p>
-                    </div>
-                    <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                        <div class="phishing-icon-box">
-                            <i class="fa fa-box"></i>
-                        </div>
-                        <h4 class="mt-5">Simply dummy text</h4>
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                            Aenean massa. </p>
-                    </div>
-                    <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                        <div class="phishing-icon-box">
-                            <i class="fa fa-box"></i>
-                        </div>
-                        <h4 class="mt-5">Simply dummy text</h4>
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                            Aenean massa. </p>
-                    </div>
-                    <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                        <div class="phishing-icon-box">
-                            <i class="fa fa-box"></i>
-                        </div>
-                        <h4 class="mt-5">Simply dummy text</h4>
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                            Aenean massa. </p>
-                    </div>
-                </div>
-            </div>
-        </section>
+        </div>
+    </section>
 
 
 
