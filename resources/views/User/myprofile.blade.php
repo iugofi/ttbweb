5dd73f725350502014e966c525cca6a7dfb73a6e
“kunalverma320” <kunalverma320@gmail.com>
2024-05-16 11:16:42 +0530

    resources\views\User\myprofile.blade.php

--- resources/views/User/myprofile.blade.php
+++ resources/views/User/myprofile.blade.php
@@ -245,7 +245,7 @@
                                                                             Edit Profile
                                                                         </h6>
                                                                         <button type="button"
-                                                                            class="hs-dropdown-toggle ti-modal-close-btn"
+                                                                            class="hs-dropdown-toggle ti-modal-close-btn" id="modalCloseButton"
                                                                             data-hs-overlay="#hs-vertically-centered-scrollable-modal">
                                                                             <span class="sr-only">Close</span>
                                                                             <svg class="w-3.5 h-3.5" width="8"
