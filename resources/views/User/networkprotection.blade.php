c790b69e99fdcc18e17253827f67ddc10824421e
kulvinder <iugofi.kulvinder@gmail.com>
2024-05-17 17:21:15 +0530

    resources\views\User\networkprotection.blade.php assets\img\second-last-section.png

--- resources/views/User/networkprotection.blade.php
+++ resources/views/User/networkprotection.blade.php
@@ -541,11 +541,11 @@
                     class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6  customize-image text-center">
                         <div class="lg:text-start !flex items-center lg:justify-start justify-center">
-                            <img src="{{ asset('assets/img/Second-Section-img.png') }}" alt="" class="img-fluid">
+                            <img src="{{ asset('assets/img/second-last-section.png') }}" alt="" class="img-fluid">
                         </div>
-                        <div class="lg:text-start !flex items-center lg:justify-start justify-center animate-bounce">
+                        {{-- <div class="lg:text-start !flex items-center lg:justify-start justify-center animate-bounce">
                             <h6 class="uppercase">25 year of exprience</h6>
-                        </div>
+                        </div> --}}
                     </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 pt-5 pb-0 lg:px-2 !text-start  my-auto">
                         <span class="lg:text-center font-semibold mb-2 text-[1rem]" style="color:#12D2B3;">What We Do</span>
