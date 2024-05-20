dc04ced2ae672afe419c638009766f6fcf09a135
kulvinder <iugofi.kulvinder@gmail.com>
2024-05-20 12:31:43 +0530

    assets\css\style.css resources\views\User\commercial.blade.php

--- resources/views/User/commercial.blade.php
+++ resources/views/User/commercial.blade.php
@@ -235,8 +235,7 @@
 
         {{-- Start:: commericial section fifth --}}
 
-        <section
-            class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70"
+        <section class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70"
             id="features">
             <div class="container main-banner-container">
                 <div class="grid grid-cols-12 gap-6">
@@ -554,8 +553,7 @@
 
         {{-- Start:: commericial section saven --}}
 
-        <section
-            class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70"
+        <section class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70"
             id="features">
             <div class="container main-banner-container">
                 <div class="grid grid-cols-12 gap-6">
