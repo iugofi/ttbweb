8348d7e0c0a040f4d082baa89f90d8bff324d128
neeraj <tbs.neeraj@gmail.com>
2024-05-20 15:33:32 +0530

    assets\css\style.css resources\views\User\threat.blade.php

--- resources/views/User/threat.blade.php
+++ resources/views/User/threat.blade.php
@@ -37,7 +37,10 @@
                     <img src="{{ asset('assets/img/Shape-2.png') }}" alt="" class="img-fluid">
                 </div>
                 <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12">
+                   <div class="flex">
                     <h5>About Us!</h5>
+                    <hr>
+                   </div>
                     <h2 class="mt-3">We Provide <span> The Solution.</span></h2>
                     <h3>Grow Your Business From Good to Great</h3>
                     <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores quaerat, quia saepe
