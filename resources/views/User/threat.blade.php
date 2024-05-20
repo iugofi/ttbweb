60c510c1918f86968cdf750c78a8cae5807d4a99
neeraj <tbs.neeraj@gmail.com>
2024-05-20 14:38:01 +0530

    resources\views\User\threat.blade.php

--- resources/views/User/threat.blade.php
+++ resources/views/User/threat.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('User.Layouts.layout')
 
 @section('title', 'threat-protection')
@@ -12,29 +10,52 @@
 @section('content')
 
 
-{{-- <div class="content">
+    {{-- <div class="content">
     <div class="main-content"> --}}
 
-        <section class="threat-banner-top">
-            <div class="container main-banner-container">
-                <div class="grid grid-cols-12 gap-6">
-                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                        <h2>Successful <span>Business </span>Make Profitable.</h2>
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br>
-                            Aenean commodo ligula eget dolor.</p>
-                    </div>
-                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                    </div>
+
+
+    <section class="threat-banner-top">
+        <div class="container main-banner-container">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
+                    <h2>Successful <span>Business </span>Make Profitable.</h2>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br>
+                        Aenean commodo ligula eget dolor.</p>
+                </div>
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
+
+
+    <section class="threat-section-two">
+        <div class="container main-banner-container">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 col-span-12">
+                    <img src="{{ asset('assets/img/Shape-2.png') }}" alt="" class="img-fluid">
+                </div>
+                <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12">
+                    <h2>We Provide The Solution.</h2>
+                    <h3>Grow Your Business From Good to Great</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores quaerat, quia saepe
+                        consequuntur, laudantium, cum at expedita eligendi perferendis amet fugiat? Assumenda numquam nisi
+                        at, laborum eligendi necessitatibus est?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
 
 
 
     {{-- </div>
 </div> --}}
 
-         
+
 
 
 
