aeff341526ad67a9964fc50b985765ad56adc5d2
neeraj <tbs.neeraj@gmail.com>
2024-05-20 12:45:45 +0530

    resources\views\User\threat.blade.php

--- resources/views/User/threat.blade.php
+++ resources/views/User/threat.blade.php
@@ -19,10 +19,9 @@
             <div class="container main-banner-container">
                 <div class="grid grid-cols-12 gap-6">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                        <h2>Successful Business Make Profitable.</h2>
+                        <h2>Successful <span>Business </span> Profitable.</h2>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br>
-                            Aenean commodo ligula eget dolor. Aenean massa.
-                            Cum sociis <br>natoque penatibus et magnis .</p>
+                            Aenean commodo ligula eget dolor.</p>
                     </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
                     </div>
