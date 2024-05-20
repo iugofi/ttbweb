b7307b1bedd64ac0eaf218bfe1545d22cda85d8c
kunal <kunal.iugofi@gmail.com>
2024-05-20 17:24:06 +0530

    app\Models\Storepick.php

--- app/Models/Storepick.php
+++ app/Models/Storepick.php
@@ -9,6 +9,14 @@ class Storepick extends Model
 {
     protected $table = 'storepick';
     public $timestamps = false;
+    protected $fillable = [
+        'STORE_ID',
+        'STORE_TYPE',
+        'PICK_TEXT',
+        'PICK_ID',
+        'STORE_INDEX_SEQUENCE',
+        'PICK_TEXT_EXTEND', 
+    ];
 
     use HasFactory;
 }
