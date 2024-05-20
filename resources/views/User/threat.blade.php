c93a24cee5c87e6e43edd1bc98a68df8aeb246b3
neeraj <tbs.neeraj@gmail.com>
2024-05-20 12:40:59 +0530

    resources\views\User\threat.blade.php

--- resources/views/User/threat.blade.php
+++ resources/views/User/threat.blade.php
@@ -23,16 +23,8 @@
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br>
                             Aenean commodo ligula eget dolor. Aenean massa.
                             Cum sociis <br>natoque penatibus et magnis .</p>
-                        <a href="index.html" class="m-1 ti-btn ti-btn-primary-full mx-3">
-                            Explore More
-                        </a>
-                        <a href="#" class="popup-youtube btn btn-white btn-circle btn-icon"><i
-                                class="bx bx-play watch-icon"></i>
-                            <span class="pl-2 text-black-light wtc-spn mx-2">Watch Now</a></span>
                     </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                        <img src="{{ asset('assets/img/phishing-banner-image.png') }}" alt="" class="img-fluid"
-                            width="100%">
                     </div>
                 </div>
             </div>
