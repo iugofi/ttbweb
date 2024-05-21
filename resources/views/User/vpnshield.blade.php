9400488c994630de9adb25c868eb150a6bff1b05
kulvinder <iugofi.kulvinder@gmail.com>
2024-05-21 12:29:18 +0530

    resources\views\User\vpnshield.blade.php

--- resources/views/User/vpnshield.blade.php
+++ resources/views/User/vpnshield.blade.php
@@ -90,8 +90,8 @@
 
         {{-- Start:: pricing plan slider  --}}
 
-        <div class="carousel-container container main-banner-container">
-            <h3 class="font-semibold mb-2 text-center">Pricing Plan</h3>
+        <div class="carousel-container container main-banner-container mt-5 mb-5 pt-5">
+            <h3 class="font-semibold mb-2 pt-5 text-center">Pricing Plan</h3>
             <div class="grid grid-cols-12 text-center">
                 <div class="col-span-12">
                     <p class="text-[#8c9097] dark:text-white/50 text-[0.9375rem] mb-6 font-normal">We have shared some
