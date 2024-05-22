c2671c2b88c6ffdf8d4f09e7064346392592203b
kulvinder <iugofi.kulvinder@gmail.com>
2024-05-22 10:12:32 +0530

    assets\css\style.css resources\views\User\ttbantivirusnew.blade.php

--- resources/views/User/ttbantivirusnew.blade.php
+++ resources/views/User/ttbantivirusnew.blade.php
@@ -57,7 +57,7 @@
             <div class="container ttbantivirus-container">
                 <div
                     class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
-                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 antivirus-image text-center">
+                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-6 antivirus-image text-center">
                         <div class="lg:text-start !flex items-center lg:justify-start">
                             <img src="{{ asset('assets/img/ttb-second-section-img-2.png') }}" alt=""
                                 class="img-fluid">
@@ -66,7 +66,7 @@
                             <img src="{{ asset('assets/img/Box.png') }}" alt="" class="img-fluid">
                         </div>
                     </div>
-                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-6 pt-5 pb-0 lg:px-2 !text-start">
                         <span class="lg:text-center font-semibold mb-2 text-[1rem]" style="color:#12D2B3;">About Us</span>
                         <h4 class="lg:text-start font-semibold mb-2 text-[2rem]">Good Qualification Services <br>
                             And Better Skills</h4>
