7f2d641f921a5064ae8612359d4519e2a4c9ca98
neeraj <tbs.neeraj@gmail.com>
2024-05-23 09:35:58 +0530

    resources\views\User\threat.blade.php

--- resources/views/User/threat.blade.php
+++ resources/views/User/threat.blade.php
@@ -37,26 +37,44 @@
                     <img src="{{ asset('assets/img/threat-image1.png') }}" alt="" class="img-fluid" width="90%">
                 </div>
                 <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 col-span-12 pt-5">
-                   <div class="flex">
-                    <h5>About Us!</h5>
-                    <hr>
-                   </div>
+                    <div class="flex">
+                        <h5>About Us!</h5>
+                        <hr>
+                    </div>
                     <h2 class="mt-3">We Provide <span> The Solution.</span></h2>
                     <h3>Grow Your Business From Good to Great</h3>
-                    <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores quaerat, quia saepe
+                    <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores quaerat,
+                        quia saepe
                         consequuntur, laudantium, cum at expedita eligendi perferendis amet fugiat? Assumenda numquam nisi
                         at, laborum eligendi necessitatibus est?</p>
-                        <p class="mt-3 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores quaerat, quia saepe
-                            consequuntur laudantium, cum at expedita eligendi perferendis amet fugiat.</p>
-                        <div class="btn-box">
-                          <a class="btn-cnt" href="#">Contact Us<i class="fas fa-angle-double-right"></i></a>
-                          <a class="btn-srv mx-3" href="#">Our Services <i class="fas fa-angle-double-right"></i></a>
-                        </div>
+                    <p class="mt-3 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores quaerat,
+                        quia saepe
+                        consequuntur laudantium, cum at expedita eligendi perferendis amet fugiat.</p>
+                    <div class="btn-box">
+                        <a class="btn-cnt" href="#">Contact Us<i class="fas fa-angle-double-right"></i></a>
+                        <a class="btn-srv mx-3" href="#">Our Services <i class="fas fa-angle-double-right"></i></a>
                     </div>
+                </div>
             </div>
         </div>
     </section>
 
+
+    <section class="threat-section-three">
+        <div class="container">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="xxl:col-span-4 xl:col-span-4  md:col-span-4 sm:col-span-12 col-span-12">
+               
+                </div>
+                <div class="xxl:col-span-4 xl:col-span-4  md:col-span-4 sm:col-span-12 col-span-12">
+
+                </div>
+                <div class="xxl:col-span-4 xl:col-span-4  md:col-span-4 sm:col-span-12 col-span-12">
+
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 
