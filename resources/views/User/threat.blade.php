8624eb7fea540f1ee732fb328747b4726d9fb0cd
neeraj <tbs.neeraj@gmail.com>
2024-05-25 17:12:53 +0530

    resources\views\User\threat.blade.php

--- resources/views/User/threat.blade.php
+++ resources/views/User/threat.blade.php
@@ -72,15 +72,15 @@
                 </div>
 
                 <div class="xxl:col-span-4 xl:col-span-4  md:col-span-4 sm:col-span-12 col-span-12 img-boxthree">
-                    <img src="{{ asset('assets/img/sec3-img1.png') }}" alt="" class="img-fluid" width="90%">
+                    <img src="{{ asset('assets/img/sec3-img1.png') }}" alt="" class="img-fluid" width="100%">
 
                 </div>
                 <div class="xxl:col-span-4 xl:col-span-4  md:col-span-4 sm:col-span-12 col-span-12 img-boxthree">
-                    <img src="{{ asset('assets/img/sec3-img2.png') }}" alt="" class="img-fluid" width="90%">
+                    <img src="{{ asset('assets/img/sec3-img2.png') }}" alt="" class="img-fluid" width="100%">
 
                 </div>
                 <div class="xxl:col-span-4 xl:col-span-4  md:col-span-4 sm:col-span-12 col-span-12 img-boxthree">
-                    <img src="{{ asset('assets/img/sec3-img3.png') }}" alt="" class="img-fluid" width="90%">
+                    <img src="{{ asset('assets/img/sec3-img3.png') }}" alt="" class="img-fluid" width="100%">
 
                 </div>
 
