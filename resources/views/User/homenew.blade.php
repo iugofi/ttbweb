f6f57bf8252268b2d4d7451c4e62e0ce1ebd34a2
neeraj <tbs.neeraj@gmail.com>
2024-05-28 14:31:59 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -38,7 +38,7 @@
     <section class="section-home-two">
         <div class="container">
             <div class="grid grid-cols-12 gap-6">
-                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12">
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
                      <h2>Trusted by millions of customers.</h2>
                     </div>
                 </div>
