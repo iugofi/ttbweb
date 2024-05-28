efe3010cfa35101fc03169f55d54b8675dda32eb
neeraj <tbs.neeraj@gmail.com>
2024-05-28 11:48:26 +0530

    assets\css\style.css resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -19,7 +19,11 @@
         <div class="container">
             <div class="grid grid-cols-12 gap-6">
                 <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
-                     
+                     <h2>Protect Your Data With Powerful</h2>
+                     <h3>Cyber Security</h3>
+                     <p>Cyber attacks can cause on average $1.5 billion to a company, with our advanced AI system we can block the attack before it happens.</p>
+                     <a href="#">Get Started Now </a>
+                     <a href="#">Contact Us</a>
                 </div>
                 <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
                      
