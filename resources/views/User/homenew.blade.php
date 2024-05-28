a5e54af090df54c87b299ed408c8a5abe74d56e7
kulvinder <iugofi.kulvinder@gmail.com>
2024-05-28 16:07:07 +0530

    assets\css\style.css resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -36,7 +36,6 @@
   
     
     <section class="section-home-two">
-        <div class="container">
             <div class="grid grid-cols-12 gap-6">
                 <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
 
@@ -63,7 +62,6 @@
                     </div>
                     </div>
                 </div>
-            </div>
         </div>
     </section>
 
