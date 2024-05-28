bb100386c6fde9d7d9ce6021d0b110b629f822ae
neeraj <tbs.neeraj@gmail.com>
2024-05-28 11:58:36 +0530

    assets\css\style.css resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -21,7 +21,7 @@
                 <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
                      <h2>Protect Your <br> Data With Powerful</h2>
                      <h3>Cyber Security</h3>
-                     <p>Cyber attacks can cause on average $1.5 billion to a company, with our advanced AI system we can block the attack before it happens.</p>
+                     <p class="mt-4 mb-5">Cyber attacks can cause on average $1.5 billion to a company, with our advanced AI system we can block the attack before it happens.</p>
                      <a href="#" class="btn-home-now">Get Started Now </a>
                      <a href="#" class="btn-home-cnt">Contact Us</a>
                 </div>
