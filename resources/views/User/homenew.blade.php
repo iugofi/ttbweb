dcb83ed790e8ea61c8997fbd71c7c2c0895d383f
neeraj <tbs.neeraj@gmail.com>
2024-05-28 11:37:24 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -15,6 +15,20 @@
 <div class="content">
     <div class="main-content">
 
+    <section class="banner-home-one">
+        <div class="container">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
+                     
+                </div>
+                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
+                     
+                </div>
+            </div>
+        </div>
+    </section>
+     
+
 
 
 
