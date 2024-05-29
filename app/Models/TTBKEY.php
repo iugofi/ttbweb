8c6e1dfa4eca8ff84dd71c70bfcff64ca182f7a8
“kunalverma320” <kunalverma320@gmail.com>
2024-05-29 16:04:45 +0530

    app\Http\Controllers\AdminController.php app\Models\TTBKEY.php

--- app/Models/TTBKEY.php
+++ app/Models/TTBKEY.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TTBKEY extends Model
+class Ttbkey extends Model
 {
     protected $table = 'ttbkey';
     protected $fillable = [
