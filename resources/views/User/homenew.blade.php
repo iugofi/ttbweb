ee0f80b3138dfa45c04c8268b9abaffe45840041
neeraj <tbs.neeraj@gmail.com>
2024-05-30 14:25:10 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -218,8 +218,35 @@
                     </div>
                 </div>
             </section>
+       
+            <section class="section-home2-six">
+                <div class="container">
+                    <div class="grid grid-cols-12 gap-6">
+                        <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
+                            <span>ABOUT US</span>
+                            <h2 class="mt-4">Identifying And Responding To Emerging Risks</h2>
+                            <p class="mt-3 mb-5">Cybersecurity refers to the practice of protecting computer systems,
+                                networks, devices, and
+                                data from unauthorized access, cyberattacks, theft, or damage. It encompasses a range of
+                                technologies, processes. and practices designed to safeguard digital assets and ensure </p>
+                            <ul class="icn-list">
+                                <li><i class="far fa-edit"></i>High Quality Service</li>
+                                <li><i class="far fa-edit"></i>Website Security Services</li>
+                                <li><i class="far fa-edit"></i>Effective Protection</li>
+                                <li><i class="far fa-edit"></i>Security Management</li>
+                                <li><i class="far fa-edit"></i>Database Security</li>
+                            </ul>
 
-
+                            <a href="#" class="mt-5">Discover More</a>
+                        </div>
+                        <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12 ">
+                            <img src="{{ asset('assets/home-two-img/11111111.svg') }}" alt="" class="img-fluid"
+                            width="20%">
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
 
 
 
