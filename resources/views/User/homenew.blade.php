17b77ff236b43732ff546f6bf7163c792f1340d0
neeraj <tbs.neeraj@gmail.com>
2024-05-30 15:17:07 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -281,6 +281,7 @@
             </section>
 
             <section class="home2-seven-pricing">
+                <div class="container">
                 <div class="grid grid-cols-12 gap-6 justify-center mb-[3rem]">
                     <div class="xxl:col-span-2 col-span-12"></div>
                     <div class="xxl:col-span-8 xl:col-span-12 col-span-12">
@@ -767,6 +768,7 @@
                     </div>
                     <div class="xxl:col-span-2 col-span-12"></div>
                 </div>
+            </div>
             </section>
 
         </div>
