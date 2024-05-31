51a3e54c20460039c844dc2954f5bcdc31097c28
neeraj <tbs.neeraj@gmail.com>
2024-05-31 10:39:22 +0530

    assets\css\style.css resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -55,7 +55,7 @@
                                     <img src="{{ asset('assets/home-two-img/brand_3_6.svg') }}" alt=""
                                         class="img-fluid" width="20%">
                                 </div>
-                                <div class="photobanner">
+                                <div class="photobanner"style="margin-left: 12em;">
                                     <img src="{{ asset('assets/home-two-img/brand_3_1.svg') }}" alt=""
                                         class="img-fluid" width="20%">
                                     <img src="{{ asset('assets/home-two-img/brand_3_2.svg') }}" alt=""
