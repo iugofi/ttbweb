f701954a716080d013c03df022e7b6edd2343b99
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-04 10:18:08 +0530

    resources\views\User\Phishing.blade.php

--- resources/views/User/Phishing.blade.php
+++ resources/views/User/Phishing.blade.php
@@ -9,136 +9,7 @@
 
 @section('content')
 
-
-    {{-- <div class="content"> --}}
-    {{-- <div class="main-content"> --}}
-
-    <section class="Phishing-banner-top">
-        <div class="container main-banner-container">
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                    <h2>Phishing Refers By Hackers Cyber Criminals</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br>
-                        Aenean commodo ligula eget dolor. Aenean massa.
-                        Cum sociis <br>natoque penatibus et magnis .</p>
-                    <a href="index.html" class="m-1 ti-btn ti-btn-primary-full mx-3">
-                        Explore More
-                    </a>
-                    <a href="#" class="popup-youtube btn btn-white btn-circle btn-icon"><i
-                            class="bx bx-play watch-icon"></i>
-                        <span class="pl-2 text-black-light wtc-spn mx-2">Watch Now</a></span>
-                </div>
-                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                    <img src="{{ asset('assets/img/phishing-banner-image.png') }}" alt="" class="img-fluid"
-                        width="100%">
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="Phishing-section-two">
-        <div class="container main-banner-container">
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                    <div class="phishing-icon-box">
-                        <i class="fa fa-box"></i>
-                    </div>
-                    <h4 class="mt-5">Simply dummy text </h4>
-                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer hrey loubk adipiscing elit
-                        dolor.
-                    </p>
-                </div>
-                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                    <div class="phishing-icon-box">
-                        <i class="fa fa-box"></i>
-                    </div>
-                    <h4 class="mt-5">Simply dummy text</h4>
-                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer hrey loubk adipiscing elit
-                        dolor.
-                    </p>
-                </div>
-                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                    <div class="phishing-icon-box">
-                        <i class="fa fa-box"></i>
-                    </div>
-                    <h4 class="mt-5">Simply dummy text</h4>
-                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer hrey loubk adipiscing elit
-                        dolor.
-                    </p>
-                </div>
-                <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 box-icon-full">
-                    <div class="phishing-icon-box">
-                        <i class="fa fa-box"></i>
-                    </div>
-                    <h4 class="mt-5">Simply dummy text</h4>
-                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetuer hrey loubk adipiscing elit
-                        dolor.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="Phishing-section-three">
-        <div class="container main-banner-container">
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                    <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
-                </div>
-                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                    <p class="mt-2">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                        unknown printer
-                        took a galley of type and scrambled it to make a type specimen book.</p>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="Phishing-section-four">
-        <div class="container main-banner-container">
-            <div class="grid grid-cols-12 gap-6">
-                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                    <img src="{{ asset('assets/img/phishing image two.png') }}" alt="" class="img-fluid" width="100%">
-                </div>
-                <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                    <h4>Printing and Typesetting</h4>
-                    <h2 class="mt-2">There are Many Variations of Passages of Lorem Ipsum.</h2>
-                    <p class="mt-3">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                        unknown printer
-                        took a galley of type and scrambled it to make a type specimen book.</p><br>
-                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                        classical Latin literature from 45 BC, making it over 2000 years old.</p>
-                    <div class="grid grid-cols-12 gap-1 mt-7">
-                        <div class="xxl:col-span-2 xl:col-span-2 lg:col-span-2 col-span-12 ">
-                            <div class="icon-box-left">
-                                <i class="fas fa-user-shield"></i>
-                            </div>
-                        </div>
-                        <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 col-span-12 ">
-                            <h5>Exclusive Design </h5>
-                            <p>Lorem Ipsum is not simply random text.</p>
-                        </div>
-                        <div class="xxl:col-span-2 xl:col-span-2 lg:col-span-2 col-span-12">
-                            <div class="icon-box-left">
-                                <i class="fas fa-user-shield"></i>
-                            </div>
-                        </div>
-                        <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 col-span-12 ">
-                            <h5>Exclusive Design </h5>
-                            <p>Lorem Ipsum is not simply random text.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-        </div>
-    </section>
-
-
-
-
-    </div>
+<div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
 
 
 
@@ -150,12 +21,20 @@
 
 
 
-    {{-- </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 </div>
-     --}}
-
-
-
-
 
 @endsection
