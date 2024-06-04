e1a918c6b1a9ef7ecee1482a88c29bfe9d3c859d
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-04 10:23:58 +0530

    resources\views\User\Phishing.blade.php

--- resources/views/User/Phishing.blade.php
+++ resources/views/User/Phishing.blade.php
@@ -11,6 +11,30 @@
 
 <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
 
+        <!-- Start:: anti-phishing section one -->
+
+        <div class="Malware-banner" id="Malware">
+            <section class="section sect-ban">
+                <div class="container main-banner-container">
+                    <div class="grid grid-cols-12 gap-6">
+                        <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
+                            <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 7rem;">
+                                <h1 class="text-left font-semibold mb-2 text-[4rem]" style="color: #fff;">IT Solutions<br>
+                                    For Your<br>
+                                    Business.</h1>
+                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] text-left dark-text"
+                                    style="color: #fff;">The first rule any technology used in business automantion
+                                    <br>applied to an efficient operation will magnify.
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <!-- End:: anti-phishing section one -->
 
 
 
@@ -34,7 +58,6 @@
 
 
 
-    
 </div>
 
 @endsection
