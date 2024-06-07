e35a9d97f757f4ec6c8ff5c1d55f53da4e941587
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-06-07 15:31:11 +0530

    resources\views\User\Phishing.blade.php

--- resources/views/User/Phishing.blade.php
+++ resources/views/User/Phishing.blade.php
@@ -75,7 +75,7 @@
                                     <div class="ltr:ml-3 rtl:mr-3">
                                         <p class="text-sm font-medium text-slate-700 group-hover:text-slate-900">FEATURES
                                         </p>
-                                        <h5 class="text-sm font-bold text-xl text-slate-500 group-hover:text-slate-700">Top
+                                        <h5 class="text-sm font-bold text-xl text-slate-500 group-hover:text-slate-700">
                                             Innovative Solution<br>
                                             For Business</h5>
                                     </div>
