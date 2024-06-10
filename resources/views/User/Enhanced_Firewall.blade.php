f673f310ec1ff8726bba3827df35713090db7bf0
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-10 15:59:04 +0530

    resources\views\User\Enhanced_Firewall.blade.php

--- resources/views/User/Enhanced_Firewall.blade.php
+++ resources/views/User/Enhanced_Firewall.blade.php
@@ -14,6 +14,35 @@
 
 <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
 
+        <!-- Start::Enhanced Firewall section one -->
+        <div class="endpoint-protection" id="endpoint">
+            <section class="section sect-ban">
+                <div class="container main-banner-container">
+                    <div class="grid grid-cols-12 gap-6">
+                        <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
+                            <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 7rem;">
+                                <h1 class="text-left font-semibold mb-2 text-[2rem]" style="color: #fff;">Limited Time
+                                    Offer Flat 50% off - No<br>
+                                    Hidden Chnages Hurry Up!!!</h1>
+                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] text-left dark-text"
+                                    style="color: #fff;">Holisticly
+                                    procrastinate mission-critical convergence with reliable customer service.
+                                    <br>Assertively
+                                    underwhelm idea-sharing for impactful solutions.
+                                </div>
+                                <div class="bnr-btn text-left">
+                                    <a href="index.html" class="m-1 ti-btn ti-btn-primary-full bnr-btn-1"
+                                        style="padding: 8px 34px;">
+                                        Buy Now
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- End::Enhanced Firewall section one -->
 
 
 
@@ -32,7 +61,6 @@
 
 
 
-    
 
 
 
