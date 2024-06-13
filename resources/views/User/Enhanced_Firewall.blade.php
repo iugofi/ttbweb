d76c8ae074da9b384b94310da53e9e74592b7f1f
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-13 16:27:02 +0530

    resources\views\User\Enhanced_Firewall.blade.php

--- resources/views/User/Enhanced_Firewall.blade.php
+++ resources/views/User/Enhanced_Firewall.blade.php
@@ -20,16 +20,16 @@
                     <div class="grid grid-cols-12 gap-6">
                         <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
                             <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 7rem;">
-                                <h1 class="text-left font-semibold mb-2 text-[2rem]" style="color: #fff;">Limited Time
+                                <h1 class="text-center font-semibold mb-2 text-[2rem]" style="color: #fff;">Limited Time
                                     Offer Flat 50% off - No<br>
                                     Hidden Chnages Hurry Up!!!</h1>
-                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] text-left dark-text"
+                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] text-center dark-text"
                                     style="color: #fff;">Holisticly
                                     procrastinate mission-critical convergence with reliable customer service.
                                     <br>Assertively
                                     underwhelm idea-sharing for impactful solutions.
                                 </div>
-                                <div class="bnr-btn text-left">
+                                <div class="bnr-btn text-center">
                                     <a href="index.html" class="m-1 ti-btn ti-btn-primary-full bnr-btn-1"
                                         style="padding: 8px 34px;">
                                         Buy Now
