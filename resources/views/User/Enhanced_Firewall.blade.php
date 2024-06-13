9e7bed385bd6170d7af269a4c3d50e4068a0835c
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-13 17:01:17 +0530

    resources\views\User\Enhanced_Firewall.blade.php

--- resources/views/User/Enhanced_Firewall.blade.php
+++ resources/views/User/Enhanced_Firewall.blade.php
@@ -50,7 +50,6 @@
                 <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 ms-5 !text-start">
                     <span class="lg:text-center font-semibold mb-2 text-[1rem]" style="color:#12D2B3;">Our Service</span>
                     <h4 class="lg:text-start font-semibold mb-2 pt-5 pb-5 text-[2rem]">We Shape The Perfect Solutions</h4>
-                    <hr>
                 </div>
                 <div
                     class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
