c2eeee8e3d580ae4f8097a71d49d5c78e9cfa914
“kunalverma320” <kunalverma320@gmail.com>
2024-06-14 15:22:57 +0530

    app\Models\Payments.php

--- app/Models/Payments.php
+++ app/Models/Payments.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payments extends Model
 {
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    
     protected $table = 'payments';
 
-    use HasFactory;
+    use HasFactory ,SoftDeletes;
 }
