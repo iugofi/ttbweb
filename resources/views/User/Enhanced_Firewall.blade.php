f7970f0d1b224c58425d94eb42da1eec1ef56157
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-17 10:21:42 +0530

    resources\views\User\Enhanced_Firewall.blade.php

--- resources/views/User/Enhanced_Firewall.blade.php
+++ resources/views/User/Enhanced_Firewall.blade.php
@@ -254,41 +254,54 @@
 
 
         <!-- Start:: Enhanced Firewall section five -->
-        <div class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70">
+        <div class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70 ">
             <div class="container main-banner-container">
-                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 ms-5 !text-start">
-                    <span class="lg:text-center font-semibold mb-2 text-[1rem]" style="color:#12D2B3;">Our Service</span>
-                    <h4 class="lg:text-start font-semibold mb-2 pt-5 pb-5 text-[2rem]">The Perfect Solutions</h4>
-                    <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Lorem ipsum dolor sit amet, eiusmod
-                        tempor. Lorem<br> ipsum dolor sit amet, eiusmod tempor.
-                    </p>
-                </div>
-                <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                <div
+                    class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-6 customize-image text-center">
                         <div class="lg:text-start !flex items-center lg:justify-start justify-center">
                             <img src="{{ asset('assets/img/Last-Img.png') }}" alt="" class="img-fluid">
                         </div>
-                    </div> 
+                        {{-- <div class="lg:text-start !flex items-center lg:justify-start justify-center animate-bounce">
+                            <h6 class="uppercase">25 year of exprience</h6>
+                        </div> --}}
+                    </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-6 pt-5 pb-0 lg:px-2 !text-start  my-auto">
-                        <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Lorem ipsum dolor sit amet, eiusmod
-                            tempor. Lorem ipsum dolor sit amet, eiusmod tempor. Objectively deliver professional
+                        <span class="lg:text-center font-semibold mb-2 text-[1rem]" style="color:#12D2B3;">About Your Company</span>
+                        <h4 class="lg:text-start font-semibold mb-2 text-[2rem]">The Largest Business Expert<br>
+                            Company!</h4>
+                        <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Objectively deliver professional
                             value with diverse web-readiness. Collaboratively transition wireless customer service without
-                            goal-oriented catalysts for change. Collaboratively Collaboratively transition wireless customer service without
-                            goal-oriented catalysts for change.Lorem ipsum dolor sit amet, eiusmod tempor. Lorem ipsum dolor sit amet, eiusmod tempor. Objectively deliver professional
-                            value with diverse web-readiness. Collaboratively transition wireless customer service without
-                            goal-oriented catalysts for change. Collaboratively Collaboratively transition wireless customer service without
-                            goal-oriented catalysts for change.
+                            goal-oriented catalysts for change. Collaboratively.
                         </p>
-                        <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Lorem ipsum dolor sit amet, eiusmod
-                            tempor. Lorem ipsum dolor sit amet, eiusmod tempor. Objectively deliver professional
-                            value with diverse web-readiness.
-                        </p>
-                        <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Lorem ipsum dolor sit amet, eiusmod
-                            tempor. Lorem ipsum dolor sit amet, eiusmod tempor.
-                        </p>
-                        
+
+                        <div class="grid grid-rows-2 grid-flow-col gap-1">
+                            <div class="row-span-3">
+                                <img src="assets/img/Best-Services-Iocn-1.png" class="img-fluid !inline-flex backimage">
+                            </div>
+                            <div class="col-span-2">
+                                <h5 class="dark-text px-6">Best Services</h5>
+                            </div>
+                            <div class="row-span-2 col-span-2">
+                                <p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
+                                    tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p>
+                            </div>
+                        </div>
+                        <hr style="width:94%">
+                        <div class="grid grid-rows-2 grid-flow-col pt-4 gap-1">
+                            <div class="row-span-3"><img src="assets/img/Best-Services-Iocn-2.png "
+                                    class="img-fluid !inline-flex backimage"></div>
+                            <div class="col-span-2">
+                                <h5 class="dark-text px-6">Support 24/7</h5>
+                            </div>
+                            <div class="row-span-2 col-span-2">
+                                <p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
+                                    tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p>
+                            </div>
+                        </div>
+
                         {{-- <div class="action-btns mt-4">
-                            <a href="#" class="ti-btn bg-primary text-white">Schevule a Demo Now</a>
+                            <a href="#" class="ti-btn bg-primary text-white">Explore More Us</a>
                         </div> --}}
                     </div>
                 </div>
