ce27bcdf2e8179755c834b7c3388e8401b061b75
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-18 15:16:31 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -13,7 +13,7 @@
 <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
 
         <!-- Start::virus removal service section one -->
-        <h1 text-center>hello neeraj</h1>
+        <h1 text-center color-dark font-lg>hello neeraj</h1>
         <!-- End::virus removal service section one -->
 
 
