2a73dae9227ffe772e9fb6e673ada6a13eea9b12
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-18 14:50:39 +0530

    resources\views\User\E_Support_Concierge.blade.php

---
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -0,0 +1,59 @@
+@extends('User.Layouts.layout')
+
+@section('title', 'virus removal service')
+@section('Description', '')
+@section('keywords', '')
+@section('canonical', '')
+
+ 
+
+@section('content')
+
+
+<div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
+
+        <!-- Start::virus removal service section one -->
+        <div class="enhanced-firewall" id="enhanced">
+            <section class="section sect-ban">
+                <div class="container main-banner-container">
+                    <div class="grid grid-cols-12 gap-6">
+                        <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
+                            <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 14rem;">
+                                <h1 class="text-center font-semibold mb-2 text-[2rem]" style="color: #fff;">Limited Time
+                                    Offer Flat 50% off - No<br>
+                                    Hidden Chnages Hurry Up!!!</h1>
+                                <div class="text-[1rem] mb-[1.3rem] text-black-light opacity-[0.9] text-center dark-text"
+                                    style="color: #fff;">Holisticly
+                                    procrastinate mission-critical convergence with reliable customer service.
+                                    <br>Assertively
+                                    underwhelm idea-sharing for impactful solutions.
+                                </div>
+                                <div class="bnr-btn text-center">
+                                    <a href="index.html" class="m-1 ti-btn ti-btn-primary-full bnr-btn-1"
+                                        style="padding: 8px 34px;">
+                                        Buy Now
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- End::virus removal service section one -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+@endsection
