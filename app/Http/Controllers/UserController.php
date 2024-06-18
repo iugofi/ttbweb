1c0aa05574d762ba15e06d379c57bb044bd6a8d2
“kunalverma320” <kunalverma320@gmail.com>
2024-06-18 15:36:05 +0530

    app\Http\Controllers\UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -153,6 +153,7 @@ class UserController extends Controller
         ->join('storepick', 'storepick.PICK_ID', '=', 'product_details.key_type')
         ->select('product_details.id', 'planname.name as name' , 'product_details.price', 'product_details.discount', 'product_details.coupons', DB::raw('(product_details.price)/12 AS monthlyprice'))
         ->where('storepick.STORE_ID','=','key_type')
+        ->where('storepick.PICK_ID','=',502)
         ->get();
         $queryLog = DB::getQueryLog();
         dd($queryLog);
