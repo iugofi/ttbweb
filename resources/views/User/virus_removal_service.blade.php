1e752fbaf891251d51a02d810d5643f3a490a0e4
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-18 17:05:58 +0530

    resources\views\User\virus_removal_service.blade.php

--- resources/views/User/virus_removal_service.blade.php
+++ resources/views/User/virus_removal_service.blade.php
@@ -79,13 +79,11 @@
             <div class="container"> 
                 <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-6 pt-5 pb-0 lg:px-2 !text-start  my-auto">
-                        <span class="lg:text-center font-semibold mb-2 text-[1rem]" style="color:#12D2B3;">About Your Company</span>
-                        <h4 class="lg:text-start font-semibold mb-2 text-[2rem]">The Largest Business Expert<br>
-                            Company!</h4>
-                        <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Objectively deliver professional
+                        <h4 class="lg:text-start font-semibold mb-2 text-[2rem]">Comprehensive Services:<br> We Cover It All</h4>
+                        {{-- <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Objectively deliver professional
                             value with diverse web-readiness. Collaboratively transition wireless customer service without
                             goal-oriented catalysts for change. Collaboratively.
-                        </p>
+                        </p> --}}
 
                         <div class="grid grid-rows-2 grid-flow-col gap-1">
                             <div class="row-span-3">
@@ -109,8 +107,29 @@
                                 <p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
                                     tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p>
                             </div>
+                        </div><br>
+                        <hr style="width:94%">
+                        <div class="grid grid-rows-2 grid-flow-col pt-4 gap-1">
+                            <div class="row-span-3"><img src="assets/img/Best-Services-Iocn-2.png " class="img-fluid !inline-flex backimage"></div>
+                            <div class="col-span-2">
+                                <h5 class="dark-text px-6">Support 24/7</h5>
+                            </div>
+                            <div class="row-span-2 col-span-2">
+                                <p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
+                                    tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p>
+                            </div>
                         </div>
-
+                        <hr style="width:94%">
+                        <div class="grid grid-rows-2 grid-flow-col pt-4 gap-1">
+                            <div class="row-span-3"><img src="assets/img/Best-Services-Iocn-2.png " class="img-fluid !inline-flex backimage"></div>
+                            <div class="col-span-2">
+                                <h5 class="dark-text px-6">Support 24/7</h5>
+                            </div>
+                            <div class="row-span-2 col-span-2">
+                                <p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
+                                    tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p>
+                            </div>
+                        </div>
                         
                     </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-6 customize-image text-center" style="margin: auto;">
