113f04dbffa7f00c48ca353a978ebabb58c771de
kulvinder <iugofi.kulvinder@gmail.com>
2024-06-18 16:55:02 +0530

    resources\views\User\virus_removal_service.blade.php

--- resources/views/User/virus_removal_service.blade.php
+++ resources/views/User/virus_removal_service.blade.php
@@ -59,39 +59,13 @@
                         <span class="lg:text-center font-semibold mb-2 text-[1rem]" style="color:#12D2B3;">About Your Company</span>
                         <h4 class="lg:text-start font-semibold mb-2 text-[2rem]">Understanding the Threat: Identify<br>
                              and Eliminate Viruses & Malware<br></h4>
-                        <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">Viruses and malware can wreak havoc on your devices. They may<br>
+                        <p class=" text-[1rem] text-[#8c9097] dark:text-white/50 mb-4 ">Viruses and malware can wreak havoc on your devices. They may<br>
                             compromise your privacy and device security. TTB is an expert at<br>
                             identifying and removing these digital pests from your computers,<br>
                             smartphones, and other gadgets.<br><br>
                             We don't just remove viruses; we equip your device with the latest<br>
                             security tools and practices to safeguard it from future threats.
                         </p>
-
-                        <div class="grid grid-rows-2 grid-flow-col gap-1">
-                            <div class="row-span-3">
-                                <img src="assets/img/Best-Services-Iocn-1.png" class="img-fluid !inline-flex backimage">
-                            </div>
-                            <div class="col-span-2">
-                                <h5 class="dark-text px-6">Best Services</h5>
-                            </div>
-                            <div class="row-span-2 col-span-2">
-                                <p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
-                                    tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p>
-                            </div>
-                        </div><br>
-                        <hr style="width:94%">
-                        <div class="grid grid-rows-2 grid-flow-col pt-4 gap-1">
-                            <div class="row-span-3"><img src="assets/img/Best-Services-Iocn-2.png " class="img-fluid !inline-flex backimage"></div>
-                            <div class="col-span-2">
-                                <h5 class="dark-text px-6">Support 24/7</h5>
-                            </div>
-                            <div class="row-span-2 col-span-2">
-                                <p class="text-gray-light px-6">Lorem ipsum dolor sit amet, eiusmod
-                                    tempor. Lorem ipsum dolor sit amet, eiusmod tempor.</p>
-                            </div>
-                        </div>
-
-                        
                     </div>
                 </div>
             </div>
