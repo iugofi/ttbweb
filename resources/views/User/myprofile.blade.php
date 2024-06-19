9121bbcc760ab13d3528b2c27885ecadafe08e86
“kunalverma320” <kunalverma320@gmail.com>
2024-06-19 18:00:46 +0530

    resources\views\User\myprofile.blade.php

--- resources/views/User/myprofile.blade.php
+++ resources/views/User/myprofile.blade.php
@@ -81,7 +81,8 @@
                                     </p>
                                     <div class="flex mb-0">
                                         <div class="me-6">
-                                            <p class="font-bold text-[1.25rem] text-white text-shadow mb-0">{{$keydetails->count}}</p>
+                                            <p class="font-bold text-[1.25rem] text-white text-shadow mb-0">{{ isset($keydetails->count) ? $keydetails->count : 'N/A' }}
+                                            </p>
                                             <p class="mb-0 text-[.6875rem] opacity-[0.5] text-white">Antivirus</p>
                                         </div>
                                         {{-- <div class="me-6">
@@ -90,7 +91,8 @@
                                             <p class="mb-0 text-[.6875rem] opacity-[0.5] text-white">Total Security</p>
                                         </div> --}}
                                         <div class="me-6">
-                                            <p class="font-bold text-[1.25rem] text-white text-shadow mb-0">{{$keydetails->count}}</p>
+                                            <p class="font-bold text-[1.25rem] text-white text-shadow mb-0">{{ isset($keydetails->count) ? $keydetails->count : 'N/A' }}
+                                            </p>
                                             <p class="mb-0 text-[.6875rem] opacity-[0.5] text-white">Vpn Shield</p>
                                         </div>
                                     </div>
