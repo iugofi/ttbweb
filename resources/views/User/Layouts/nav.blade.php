f51156c3ffc349357dc4634ce69a026de4a109f9
“kunalverma320” <kunalverma320@gmail.com>
2024-06-20 14:40:41 +0530

    resources\views\User\Layouts\nav.blade.php

--- resources/views/User/Layouts/nav.blade.php
+++ resources/views/User/Layouts/nav.blade.php
@@ -1,38 +1,56 @@
 <style>
-    /* Hide submenu by default */
-.main-menu .slide-menu {
+    /* Styles for the main menu */
+.main-menu {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+.slide {
+    position: relative;
+}
+
+.has-sub > .slide-menu {
     display: none;
     position: absolute;
     left: 100%;
     top: 0;
+    margin-left: -1px;
+    background-color: white;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 }
 
-/* Show submenu on hover */
-.main-menu .slide.has-sub:hover > .slide-menu {
+.has-sub:hover > .slide-menu {
     display: block;
 }
 
-/* Optional: Styling for the submenu */
-.main-menu .slide-menu {
-    background-color: #fff; /* Background color */
-    border: 1px solid #ddd; /* Border */
-    padding: 10px; /* Padding */
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* Shadow */
-    z-index: 1000; /* Ensure it’s above other elements */
+.slide-menu {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
 }
 
-/* Styling for the parent menu item */
-.main-menu .side-menu__item {
+.slide-menu > .slide {
     position: relative;
-    display: block;
-    padding: 10px 15px;
-    color: #333; /* Text color */
-    text-decoration: none; /* Remove underline */
 }
 
-/* Styling for hover state of the parent menu item */
-.main-menu .side-menu__item:hover {
-    background-color: #f0f0f0; /* Background color on hover */
+.child1 {
+    position: absolute;
+    left: 100%;
+    top: 0;
+    display: none;
+}
+
+.child2 {
+    position: absolute;
+    left: 100%;
+    top: 0;
+    display: none;
+}
+
+.has-sub:hover > .child1,
+.has-sub:hover > .child2 {
+    display: block;
 }
 
 </style>
@@ -54,147 +72,122 @@
                         width="24" height="24" viewBox="0 0 24 24">
                         <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
                     </svg></div>
-                <ul class="main-menu">
-                    <!-- Start::slide -->
-                    {{-- <li class="slide">
-                        <a class="side-menu__item" href="{{ route('user.index') }}">
-                            <span class="side-menu__label">Home</span>
-                        </a>
-                    </li> --}}
-
-                    <li class="slide has-sub">
-                        <a href="javascript:void(0);" class="side-menu__item">
-                            <span class="side-menu__label me-2">Home</span>
-                            <i class="fe fe-chevron-right side-menu__angle op-8"></i>
-                        </a>
-                        <ul class="slide-menu child1">
-                            <li class="slide">
-                                <a href="{{route('user.homenew')}}" class="side-menu__item">Home-New</a>
-                            </li>
-                         
-                            
-                        </ul>
-                    </li>
+                    <ul class="main-menu">
+                        <li class="slide has-sub">
+                            <a href="javascript:void(0);" class="side-menu__item">
+                                <span class="side-menu__label me-2">Home</span>
+                                <i class="fe fe-chevron-right side-menu__angle op-8"></i>
+                            </a>
+                            <ul class="slide-menu child1">
+                                <li class="slide">
+                                    <a href="{{route('user.homenew')}}" class="side-menu__item">Home-New</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="slide has-sub">
+                            <a href="javascript:void(0);" class="side-menu__item">
+                                <span class="side-menu__label me-2">Product</span>
+                                <i class="fe fe-chevron-right side-menu__angle op-8"></i>
+                            </a>
+                            <ul class="slide-menu child1">
+                                <li class="slide">
+                                    <a href="{{route('user.vpnshield')}}" class="side-menu__item">TTB VPN Shield</a>
+                                </li>
+                                <li class="slide">
+                                    <a href="{{route('user.ttbantivirusnew')}}" class="side-menu__item">TTB Antivirus</a>
+                                </li>
+                                <li class="slide">
+                                    <a href="{{route('user.totalinternet')}}" class="side-menu__item">TTB Total Security</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="slide has-sub">
+                            <a href="javascript:void(0);" class="side-menu__item">
+                                <span class="side-menu__label me-2">Solutions</span>
+                                <i class="fe fe-chevron-right side-menu__angle op-8"></i>
+                            </a>
+                            <ul class="slide-menu child1">
+                                <li class="slide">
+                                    <a href="{{route('user.commercial')}}" class="side-menu__item">Commercial</a>
+                                </li>
+                                <li class="slide has-sub">
+                                    <a href="javascript:void(0);" class="side-menu__item">Enterprise
+                                        <i class="fe fe-chevron-right side-menu__angle"></i></a>
+                                    <ul class="slide-menu child2">
+                                        <li class="slide">
+                                            <a href="{{route('user.endpoint_protection')}}" class="side-menu__item">Endpoint Protection</a>
+                                        </li>
+                                        <li class="slide">
+                                            <a href="{{route('user.networkprotection')}}" class="side-menu__item">Network Protection</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="slide has-sub">
+                                    <a href="javascript:void(0);" class="side-menu__item">Threat Intelligence
+                                        <i class="fe fe-chevron-right side-menu__angle"></i></a>
+                                    <ul class="slide-menu child2">
+                                        <li class="slide">
+                                            <a href="{{route('user.threat')}}" class="side-menu__item">Threat Protection</a>
+                                        </li>
+                                        <li class="slide">
+                                            <a href="{{route('user.Behavioral_Protection')}}" class="side-menu__item">Behavioral Protection</a>
+                                        </li>
+                                        <li class="slide">
+                                            <a href="{{route('user.Enhanced_Firewall')}}" class="side-menu__item">Enhanced Firewall</a>
+                                        </li>
+                                        <li class="slide">
+                                            <a href="{{route('user.malware_detection')}}" class="side-menu__item">Malware Detection</a>
+                                        </li>
+                                        <li class="slide">
+                                            <a href="{{route('user.Phishing')}}" class="side-menu__item">Real Time Anti-phishing</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="slide has-sub">
+                            <a href="javascript:void(0);" class="side-menu__item">
+                                <span class="side-menu__label me-2">Resources</span>
+                                <i class="fe fe-chevron-right side-menu__angle op-8"></i>
+                            </a>
+                            <ul class="slide-menu child1">
+                                <li class="slide">
+                                    <a href="{{route('user.news')}}" class="side-menu__item">News</a>
+                                </li>
+                                <li class="slide">
+                                    <a href="{{route('user.blogpage')}}" class="side-menu__item">Blog</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="slide has-sub">
+                            <a href="javascript:void(0);" class="side-menu__item">
+                                <span class="side-menu__label me-2">Company</span>
+                                <i class="fe fe-chevron-right side-menu__angle op-8"></i>
+                            </a>
+                            <ul class="slide-menu child1">
+                                <li class="slide">
+                                    <a href="{{route('user.about_as')}}" class="side-menu__item">About Us</a>
+                                </li>
+                                <li class="slide">
+                                    <a href="{{route('user.contact_as')}}" class="side-menu__item">Contact Us</a>
+                                </li>
+                            </ul>
+                        </li>
+                        @if (session()->has('loggedInUser'))
+                        <li class="slide has-sub">
+                            <a href="javascript:void(0);" class="side-menu__item">
+                                <span class="side-menu__label me-2">My Account</span>
+                                <i class="fe fe-chevron-right side-menu__angle op-8"></i>
+                            </a>
+                            <ul class="slide-menu child1">
+                                <li class="slide">
+                                    <a href="{{ route('user.myprofile') }}" class="side-menu__item">My Profile</a>
+                                </li>
+                            </ul>
+                        </li>
+                        @endif
+                    </ul>
                     
-                    <!-- End::slide -->
-                    <!-- Start::slide -->
-                    <li class="slide has-sub">
-                        <a href="javascript:void(0);" class="side-menu__item" id="product-menu">
-                            <span class="side-menu__label me-2">Product</span>
-                            <i class="fe fe-chevron-right side-menu__angle op-8"></i>
-                        </a>
-                        <ul class="slide-menu child1" id="product-submenu">
-                            <li class="slide">
-                                <a href="{{route('user.vpnshield')}}" class="side-menu__item">TTB VPN Shield</a>
-                            </li>
-                            <li class="slide">
-                                <a href="{{route('user.ttbantivirusnew')}}" class="side-menu__item">TTB Antivirus</a>
-                            </li>
-                            <li class="slide">
-                                <a href="{{route('user.totalinternet')}}" class="side-menu__item">TTB Total Security</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <!-- End::slide -->
-                    <!-- Start::slide -->
-                    <li class="slide has-sub">
-                        <a href="javascript:void(0);" class="side-menu__item">
-                            <span class="side-menu__label me-2">Solutions</span>
-                            <i class="fe fe-chevron-right side-menu__angle op-8"></i>
-                        </a>
-                        <ul class="slide-menu child1">
-                            <li class="slide">
-                                <a href="{{route('user.commercial')}}" class="side-menu__item">Commercial</a>
-                            </li>
-                           
-                            <li class="slide has-sub">
-                                <a href="javascript:void(0);" class="side-menu__item">Enterprise
-                                    <i class="fe fe-chevron-right side-menu__angle"></i></a>
-                                <ul class="slide-menu child2">
-                                    <li class="slide">
-                                        <a href="{{route('user.endpoint_protection')}}" class="side-menu__item">Endpoint Protection</a>
-                                    </li>
-                                    <li class="slide">
-                                        <a href="{{route('user.networkprotection')}}" class="side-menu__item">Network Protection</a>
-                                    </li>
-                                    
-                                </ul>
-                            </li>
-                            <li class="slide has-sub">
-                                <a href="javascript:void(0);" class="side-menu__item">Threat Intelligence
-                                    <i class="fe fe-chevron-right side-menu__angle"></i></a>
-                                <ul class="slide-menu child2">
-                                    <li class="slide">
-                                        <a href="{{route('user.threat')}}" class="side-menu__item">Threat Protection</a>
-                                    </li>
-                                    <li class="slide">
-                                        <a href="{{route('user.Behavioral_Protection')}}" class="side-menu__item">Behavioral Protection</a>
-                                    </li>
-                                    <li class="slide">
-                                        <a href="{{route('user.Enhanced_Firewall')}}" class="side-menu__item">Enhanced Firewall</a>
-                                    </li>
-                                    <li class="slide">
-                                        <a href="{{route('user.malware_detection')}}" class="side-menu__item">Malware Detection</a>
-                                    </li>
-                                    <li class="slide">
-                                        <a href="{{route('user.Phishing')}}" class="side-menu__item">Real Time Anti-phishing</a>
-                                    </li>
-                                    
-                                    
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="slide has-sub">
-                        <a href="javascript:void(0);" class="side-menu__item">
-                            <span class="side-menu__label me-2">Resources</span>
-                            <i class="fe fe-chevron-right side-menu__angle op-8"></i>
-                        </a>
-                        <ul class="slide-menu child1">
-                            <li class="slide">
-                                <a href="{{route('user.news')}}" class="side-menu__item">News</a>
-                            </li>
-                            <li class="slide">
-                                <a href="{{route('user.blogpage')}}" class="side-menu__item">Blog</a>
-                            </li>
-                          
-                            
-                        </ul>
-                    </li>
-                    <li class="slide has-sub">
-                        <a href="javascript:void(0);" class="side-menu__item">
-                            <span class="side-menu__label me-2">Company</span>
-                            <i class="fe fe-chevron-right side-menu__angle op-8"></i>
-                        </a>
-                        <ul class="slide-menu child1">
-                            <li class="slide">
-                                <a href="{{route('user.about_as')}}" class="side-menu__item">About Us</a>
-                            </li>
-                            <li class="slide">
-                                <a href="{{route('user.contact_as')}}" class="side-menu__item">Contact Us</a>
-                            </li>
-                          
-                            
-                        </ul>
-                    </li>
-                    @if (session()->has('loggedInUser'))
-                    <li class="slide has-sub">
-                        <a href="javascript:void(0);" class="side-menu__item">
-                            <span class="side-menu__label me-2">My Account</span>
-                            <i class="fe fe-chevron-right side-menu__angle op-8"></i>
-                        </a>
-                        <ul class="slide-menu child1">
-                            <li class="slide">
-                                <a href="{{ route('user.myprofile') }}" class="side-menu__item">My Profile</a>
-                            </li>
-                           
-                          
-                            
-                        </ul>
-                    </li>
-                    @endif
-
-                </ul>
                 <div class="slide-right hidden" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191"
                         width="24" height="24" viewBox="0 0 24 24">
                         <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z">
@@ -228,4 +221,22 @@
         <!-- End::main-sidebar -->
     </div>
 </aside>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+    var menuItems = document.querySelectorAll('.slide.has-sub > a');
+    menuItems.forEach(function(menuItem) {
+        menuItem.addEventListener('click', function(e) {
+            e.preventDefault();
+            var subMenu = menuItem.nextElementSibling;
+            if (subMenu.style.display === "block") {
+                subMenu.style.display = "none";
+            } else {
+                subMenu.style.display = "block";
+            }
+        });
+    });
+});
+
+</script>
 
