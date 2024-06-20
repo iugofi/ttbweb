902ccebdba19d5123942a76ce42d7af5749a56a0
“kunalverma320” <kunalverma320@gmail.com>
2024-06-20 10:24:57 +0530

    resources\views\Admin\customerslist.blade.php

--- resources/views/Admin/customerslist.blade.php
+++ resources/views/Admin/customerslist.blade.php
@@ -121,7 +121,7 @@
                                             </td>                              
                                             <td class="border px-4 py-2">{{ $item->activation_key }}</td>   
                                             <td class="border px-4 py-2">{!! nl2br(e(Str::limit(strip_tags($item->reset_tokens), 20))) !!}</td>                               
-                                            <td class="border px-4 py-2">{{ $item->otp }}</td>
+                                            <td class="border px-4 py-2">{!! nl2br(e(Str::limit(strip_tags($item->otp), 20))) !!}</td>
                                             <td class="border px-4 py-2">{{ $item->status }}</td>                                                                       
                                            
                                             <td class="border px-4 py-2">
