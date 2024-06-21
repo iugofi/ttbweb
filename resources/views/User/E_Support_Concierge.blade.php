d39834e13639e3c81fe2582fbef49b3fae20841b
neeraj <tbs.neeraj@gmail.com>
2024-06-21 13:02:56 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -12,18 +12,13 @@
 
 <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
 
-        <!-- Start::virus removal service section one -->
-        <h1 text-center color-dark font-lg>hello neeraj</h1>
-        <!-- End::virus removal service section one -->
-
-
-    sdgxcvbdfgdfgdfgdf dbcvbdfbcvdbdf sdsdvfdfgfgdf
-gdfgdhsdfhc dfgsdfgsdf
 
 
 
 
 
+
+    
 
 
 
