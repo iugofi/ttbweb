961a3d13dac2da98b25bd9c1bc1c4900835a7c96
neeraj <tbs.neeraj@gmail.com>
2024-06-21 14:51:22 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -40,7 +40,7 @@
         <section class="e-suport-section-two">
             <div class="container">
                 <div class="grid grid-cols-12 gap-6">
-                    <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12">
+                    <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
                     <h2>What is TTB eSupport Concierge?</h2>
                     <p>TTB eSupport Concierge offers top-quality services to users for all their smart devices. With a
                         single click, you can have top-notch assistance at your beck and call.</p>
