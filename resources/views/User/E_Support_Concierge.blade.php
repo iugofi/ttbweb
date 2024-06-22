f9fab199a71c6f398014c0772874c9c16ccd29e6
neeraj <tbs.neeraj@gmail.com>
2024-06-22 13:18:36 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -54,9 +54,9 @@
                             <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 md:col-span-7 col-span-7">
                                 <i class="fa-solid fa-arrow-right"></i><span class="ms-5">Read More</span>
                             </div>
-                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 box-img">
-                                {{-- <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
-                                    width="20%"> --}}
+                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 ">
+                                <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
+                                    width="20%">
                             </div>
                         </div>
                     </div>
@@ -66,9 +66,9 @@
                             <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 md:col-span-7 col-span-7">
                                 <i class="fa-solid fa-arrow-right"></i><span class="ms-5">Read More</span>
                             </div>
-                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 box-img">
-                                {{-- <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
-                                    width="20%"> --}}
+                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 ">
+                                <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
+                                    width="20%">
                             </div>
                         </div>
                     </div>
@@ -78,9 +78,9 @@
                             <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 md:col-span-7 col-span-7">
                                 <i class="fa-solid fa-arrow-right"></i><span class="ms-5">Read More</span>
                             </div>
-                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 box-img">
-                                {{-- <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
-                                    width="20%"> --}}
+                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 ">
+                                <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
+                                    width="20%">
                             </div>
                         </div>
                     </div>
@@ -90,9 +90,9 @@
                             <div class="xxl:col-span-7 xl:col-span-7 lg:col-span-7 md:col-span-7 col-span-7">
                                 <i class="fa-solid fa-arrow-right"></i><span class="ms-5">Read More</span>
                             </div>
-                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 box-img">
-                                {{-- <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
-                                    width="20%"> --}}
+                            <div class="xxl:col-span-5 xl:col-span-5 lg:col-span-5 md:col-span-5 col-span-5 ">
+                                <img src="{{ asset('assets/e-support/guaranteed.png') }}" alt="" class="img-fluid"
+                                    width="20%">
                             </div>
                         </div>
                     </div>
