c374f90d0b26c9bc9fc77bc5193bb04f4465e3ce
neeraj <tbs.neeraj@gmail.com>
2024-06-22 17:46:28 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -132,18 +132,31 @@
                 <div class="grid grid-cols-12 gap-6">
                     <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
                         <h2>Smart Tech Assistance</h2>
-                        <p class="mb-5 mt-2">From smart home devices to tech gadgets, we can help you set up and troubleshoot a wide <br>range of smart technologies.</p>
+                        <p class="mb-5 mt-2">From smart home devices to tech gadgets, we can help you set up and
+                            troubleshoot a wide <br>range of smart technologies.</p>
                     </div>
                 </div>
                 <div class="grid grid-cols-12 gap-6 mt-5">
-                    <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12">
-
+                    <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12 bx-pricing">
+                        <i></i>
+                        <h2></h2>
+                        <h3></h3>
+                        <p></p>
+                        <i></i><span></span>
                     </div>
-                    <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12">
-                        
+                    <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12 bx-pricing">
+                        <i></i>
+                        <h2></h2>
+                        <h3></h3>
+                        <p></p>
+                        <i></i><span></span>
                     </div>
-                    <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12">
-                        
+                    <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12 bx-pricing">
+                        <i></i>
+                        <h2></h2>
+                        <h3></h3>
+                        <p></p>
+                        <i></i><span></span>
                     </div>
                 </div>
             </div>
