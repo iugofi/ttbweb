d573d572cf7d6f88a758644639a79a3375db9ee5
“kunalverma320” <kunalverma320@gmail.com>
2024-06-24 12:52:01 +0530

    app\Http\Controllers\AdminController.php resources\views\Admin\blogcommentedit.blade.php

---
+++ resources/views/Admin/blogcommentedit.blade.php
@@ -0,0 +1,243 @@
+@extends('Admin.Layouts.layout')
+
+@section('title', 'Blog Comment Edit')
+@section('Description', '')
+@section('keywords', '')
+@section('canonical', 'vbhdvbh')
+@section('content')
+
+
+
+
+
+
+    <div class="content">
+        <div class="main-content">
+
+
+            <!-- Page Header -->
+            <div class="block justify-between page-header md:flex">
+                <div>
+                    <h3
+                        class="!text-defaulttextcolor dark:!text-defaulttextcolor/70 dark:text-white dark:hover:text-white text-[1.125rem] font-semibold">
+                        Blog Edit</h3>
+                </div>
+                <ol class="flex items-center whitespace-nowrap min-w-0">
+                    <li class="text-[0.813rem] ps-[0.5rem]">
+                        <a class="flex items-center text-primary hover:text-primary dark:text-primary truncate"
+                            href="javascript:void(0);">
+                            Blog Comment
+                            <i
+                                class="ti ti-chevrons-right flex-shrink-0 text-[#8c9097] dark:text-white/50 px-[0.5rem] overflow-visible rtl:rotate-180"></i>
+                        </a>
+                    </li>
+                    <li class="text-[0.813rem] text-defaulttextcolor font-semibold hover:text-primary dark:text-[#8c9097] dark:text-white/50 "
+                        aria-current="page">
+                        Blog Comment Edit
+                    </li>
+                </ol>
+            </div>
+            <!-- Page Header Close -->
+
+            <!-- Start::row-1 -->
+            <div class="grid grid-cols-12 gap-x-6">
+                <div class="xxl:col-span-9 xl:col-span-12 lg:col-span-12 md:col-span-12 sm:col-span-12 col-span-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <div class="box-title">Blog Comment Edit</div>
+                        </div>
+                        <form method="post" id="edit__com_blog_form" enctype="multipart/form-data">
+                            @csrf
+                            <div class="box-body">
+                                <div class="box text-center">
+                                    <div class="" id="show_success_alert"></div>
+                                </div>
+                                <div class="grid grid-cols-12 gap-4">
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="blog-title" class="form-label">Blog Id</label>
+                                        <input type="text" name="main_id" value="{{ isset($editblog->id) ? $editblog->id : '' }}" hidden>
+                                        <input type="text" class="form-control block w-full text-[0.875rem] !rounded-md"
+                                            id="" name="" value="{{ isset($editblog->blog_id) ? $editblog->blog_id : '' }}" placeholder="Blog Id" disabled>
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="blog-title" class="form-label">First Name</label>
+                                        <input type="text" class="form-control block w-full text-[0.875rem] !rounded-md"
+                                            id="" name="" value="{{ isset($editblog->first_name) ? $editblog->first_name : '' }}" placeholder="Blog Id" disabled>
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="blog-title" class="form-label">Last Name</label>
+                                        <input type="text" class="form-control block w-full text-[0.875rem] !rounded-md"
+                                            id="" name="" value="{{ isset($editblog->last_name) ? $editblog->last_name : '' }}" placeholder="Blog Id" disabled>
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="blog-title" class="form-label">Email</label>
+                                        <input type="text" class="form-control block w-full text-[0.875rem] !rounded-md"
+                                            id="" name="" value="{{ isset($editblog->email) ? $editblog->email : '' }}" placeholder="Blog Id" disabled>
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="blog-title" class="form-label">Comment</label>
+                                        <textarea name="" class="form-control block w-full text-[0.875rem] !rounded-md" id="" cols="30" rows="10" disabled>{{ isset($editblog->comment) ? $editblog->comment : '' }}</textarea>
+                                        
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+                                   
+
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="blog-Language" class="form-label">Status</label>
+                                        <select class="form-control block w-full text-[0.875rem] !rounded-md" data-trigger
+                                            name="blog_com_status" id="blog_com_status">
+                                            <option value="">Select</option>
+                                            @php
+                                                $status = DB::table('storepick')
+                                                    ->where('STORE_ID', 'Status')
+                                                    ->orderBy('STORE_INDEX_SEQUENCE', 'asc')
+                                                    ->get();
+                                            @endphp
+                                            @foreach ($status as $statuss)
+                                                <option value="{{ $statuss->PICK_ID }}" {{ $statuss->PICK_ID == $editblog->status ? 'selected' : '' }}>{{ $statuss->PICK_TEXT }}</option>
+                                            @endforeach
+
+                                        </select>
+                                        <div class="invalid-feedback"></div>
+
+                                    </div>
+
+
+                                </div>
+                            </div>
+                            <div class="box-footer">
+                                <div class="text-end">
+                                    {{-- <button type="button" class="ti-btn !py-1 !px-2 ti-btn-light !text-[0.75rem] !font-medium me-2">Save As Draft</button> --}}
+                                    <input type="submit" value="Edit Blog" id="blog__com_edit_btn"
+                                        class="ti-btn bg-primary text-white !py-1 !px-2 !text-[0.75rem] !font-medium">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="xxl:col-span-3 xl:col-span-12 lg:col-span-12 md:col-span-12 sm:col-span-12 col-span-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <div class="box-title">
+                                Recent Blog
+                            </div>
+                        </div>
+                        <div class="box-body" id="recentpost">
+                            <ul class="list-group">
+                                <!-- Recent posts will be dynamically added here -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--End::row-1 -->
+
+
+        </div>
+    </div>
+
+ 
+
+
+
+
+    <script>
+        $(document).ready(function() {
+            function fetchNews() {
+                $.ajax({
+                    url: "{{ route('fatch.news') }}",
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(response) {
+                        console.log(response);
+                        if (response.length > 0) {
+                            var recentPostHtml = '';
+                            $.each(response, function(index, post) {
+                                var truncatedDescription = post.description.split(' ').slice(0,
+                                    2).join(' ');
+                                var truncatedTitle = post.title.split(' ').slice(0, 2).join(
+                                ' ');
+
+                                var imageUrl = "{{asset('assets/images/dailynews/')}}"+ '/' + post.image;
+
+                                recentPostHtml += '<li class="list-group-item">';
+                                recentPostHtml +=
+                                    '<div class="flex gap-2 flex-wrap items-center">';
+                                recentPostHtml += '<span class="avatar avatar-xl me-1">';
+                                recentPostHtml += '<img src="' + imageUrl +
+                                    '" class="img-fluid !rounded-md" alt="...">';
+                                recentPostHtml += '</span>';
+                                recentPostHtml += '<div class="flex-grow">';
+                                recentPostHtml +=
+                                    '<a href="" class="text-[0.875rem] font-semibold mb-0">' +
+                                    truncatedTitle + '</a>';
+                                recentPostHtml +=
+                                    '<p class="mb-1 popular-blog-content text-truncate">' +
+                                    truncatedDescription + '</p>';
+                                recentPostHtml +=
+                                    '<span class="text-[#8c9097] dark:text-white/50 text-[0.6875rem]">' +
+                                    post.created_at + '</span>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '<div>';
+                                recentPostHtml +=
+                                    '<button aria-label="button" type="button" class="ti-btn ti-btn-light ti-btn-sm rtl:rotate-180"><i class="ri-arrow-right-s-line"></i></button>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '</li>';
+                            });
+                            $('#recentpost .list-group').html(recentPostHtml);
+                        } else {
+                            // Handle case where no posts are returned
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        // Handle error
+                    }
+                });
+            }
+
+            $('#edit__com_blog_form').submit(function(e) {
+                e.preventDefault();
+                $('#blog__com_edit_btn').val('please wait..');
+                var token = $('meta[name="csrf-token"]').attr('content');
+                var formData = new FormData($(this)[0]);
+                
+                $.ajax({
+                    url:  "{{ route('edit.editcommentblog') }}",
+                    method: 'post',
+                    data: formData,
+                    headers: {
+                        'X-CSRF-TOKEN': token
+                    },
+                    contentType: false,
+                    processData: false,
+                    success: function(response) {
+                        if (response.status == 400) {
+                            showError('blog_com_status', response.messages.news_com_status);
+                            $('#blog__com_edit_btn').val('Edit Blog Comment');
+                        } else if (response.status == 200) {
+                            $('.invalid-feedback').empty();
+                            // $("#show_success_alert").html(showMessage('success', response
+                            //     .messages));
+                            $('#edit__com_blog_form')[0].reset();
+                            removeValidationClass("#edit__com_blog_form");
+                            $('#blog__com_edit_btn').val('Edit Blog Comment');
+                            alert(response.messages);
+                            window.location.href = "{{ route('admin.blogcommentlist') }}";
+                            fetchNews();
+                        }
+                    },
+                    error: function(xhr, status, error) {}
+                });
+            });
+            fetchNews();
+        });
+    </script>
+
+
+
+@endsection
