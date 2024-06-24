fa4120510ce55b2dffba9bc27651323603929c52
neeraj <tbs.neeraj@gmail.com>
2024-06-24 17:28:03 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -248,41 +248,20 @@
                             <div class="mt-3">
                                 <div id="pills-on-gray-color-1" role="tabpanel"
                                     aria-labelledby="pills-on-gray-color-item-1">
-                                    <p
-                                        class="text-gray-500 dark:text-[#8c9097] dark:text-white/50 p-5 border rounded-sm dark:border-white/10 border-gray-200">
-                                        How hotel deals can help you live a better life. How celebrity cruises aren't as
-                                        bad as you think. How cultural solutions can help you predict the future. How to
-                                        cheat at dog friendly hotels and get away with it. 17 problems with summer
-                                        activities. How to cheat at travel agents and get away with it. How not knowing
-                                        family trip ideas makes you a rookie. What everyone is saying about daily deals.
-                                        How twitter can teach you about carnival cruises. How to start using cultural
-                                        solutions.
+                                    <p>
+                                        How 
                                     </p>
                                 </div>
                                 <div id="pills-on-gray-color-2" class="hidden" role="tabpanel"
                                     aria-labelledby="pills-on-gray-color-item-2">
-                                    <p
-                                        class="text-gray-500 dark:text-[#8c9097] dark:text-white/50 p-5 border rounded-sm dark:border-white/10 border-gray-200">
-                                        How travel coupons make you a better lover. Why cultural solutions are the new
-                                        black. Why mom was right about travel insurances. How family trip ideas can help
-                                        you predict the future. How carnival cruises make you a better lover. Why you'll
-                                        never succeed at daily deals. 11 ways cheapest flights can find you the love of
-                                        your life. The complete beginner's guide to mission trips. If you read one
-                                        article about cultural notes read this one. Why you shouldn't eat vacation
-                                        package in bed.
+                                    <p>
+                                        How 
                                     </p>
                                 </div>
                                 <div id="pills-on-gray-color-3" class="hidden" role="tabpanel"
                                     aria-labelledby="pills-on-gray-color-item-3">
-                                    <p
-                                        class="text-gray-500 dark:text-[#8c9097] dark:text-white/50 p-5 border rounded-sm dark:border-white/10 border-gray-200">
-                                        Unbelievable healthy snack success stories. 12 facts about safe food handling
-                                        tips that will impress your friends. Restaurant weeks by the numbers. Will
-                                        mexican food ever rule the world? The 10 best thai restaurant youtube videos.
-                                        How restaurant weeks can make you sick. The complete beginner's guide to cooking
-                                        healthy food. Unbelievable food stamp success stories. How whole foods markets
-                                        are making the world a better place. 16 things that won't happen in dish
-                                        reviews.
+                                    <p>
+                                        How 
                                     </p>
                                 </div>
                             </div>
