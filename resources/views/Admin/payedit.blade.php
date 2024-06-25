a9cbf086d6dbb60f82873cd0a6476fa474416cf6
kunal <kunal.iugofi@gmail.com>
2024-06-25 12:52:39 +0530

    app\Http\Controllers\AdminController.php resources\views\Admin\payshow.blade.php routes\web.php resources\views\Admin\payedit.blade.php

---
+++ resources/views/Admin/payedit.blade.php
@@ -0,0 +1,223 @@
+@extends('Admin.Layouts.layout')
+
+@section('title', 'Payment Edit')
+@section('Description', '')
+@section('keywords', '')
+@section('canonical', 'vbhdvbh')
+@section('content')
+
+
+
+
+
+
+    <div class="content">
+        <div class="main-content">
+
+
+            <!-- Page Header -->
+            <div class="block justify-between page-header md:flex">
+                <div>
+                    <h3
+                        class="!text-defaulttextcolor dark:!text-defaulttextcolor/70 dark:text-white dark:hover:text-white text-[1.125rem] font-semibold">
+                        Payment Create </h3>
+                </div>
+                <ol class="flex items-center whitespace-nowrap min-w-0">
+                    <li class="text-[0.813rem] ps-[0.5rem]">
+                        <a class="flex items-center text-primary hover:text-primary dark:text-primary truncate"
+                            href="javascript:void(0);">
+                            Payment
+                            <i
+                                class="ti ti-chevrons-right flex-shrink-0 text-[#8c9097] dark:text-white/50 px-[0.5rem] overflow-visible rtl:rotate-180"></i>
+                        </a>
+                    </li>
+                    <li class="text-[0.813rem] text-defaulttextcolor font-semibold hover:text-primary dark:text-[#8c9097] dark:text-white/50 "
+                        aria-current="page">
+                        Payment Create
+                    </li>
+                </ol>
+            </div>
+            <!-- Page Header Close -->
+
+            <!-- Start::row-1 -->
+            <div class="grid grid-cols-12 gap-x-6">
+                <div class="xxl:col-span-9 xl:col-span-12 lg:col-span-12 md:col-span-12 sm:col-span-12 col-span-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <div class="box-title">Payment Create</div>
+                        </div>
+                        <form method="post" id="edit_news_form" enctype="multipart/form-data">
+                            @csrf
+                            <div class="box-body">
+                                <div class="box text-center">
+                                    <div class="" id="show_success_alert"></div>
+                                </div>
+                                <div class="grid grid-cols-12 gap-4">
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="blog-title" class="form-label">Payment Id</label>
+                                        <input type="text" name="main_id" value="{{ isset($payedit->id) ? $payedit->id : '' }}" hidden>
+                                        <input type="text" class="form-control block w-full text-[0.875rem] !rounded-md"
+                                            id="news_title" name="news_title" value="{{ isset($payedit->pay_id) ? $payedit->pay_id : '' }}" placeholder="Payment Id">
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label for="News Slug" class="form-label">product_key</label>
+                                        <input type="text" class="form-control block w-full text-[0.875rem] !rounded-md"
+                                            id="news_slug" name="news_slug" value="{{ isset($payedit->product_key) ? $payedit->product_key : '' }}" placeholder="Product Key">
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+   
+                                    <div class="xl:col-span-12 col-span-12">
+                                        <label class="form-label">Amount</label>
+                                        <textarea  id="amount_total" name="amount_total"
+                                            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                                            rows="6">{{ isset($payedit->amount_total) ? $payedit->amount_total : '' }}</textarea>
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+
+                                
+
+                                  
+
+                                </div>
+                            </div>
+                            <div class="box-footer">
+                                <div class="text-end">
+                                    {{-- <button type="button" class="ti-btn !py-1 !px-2 ti-btn-light !text-[0.75rem] !font-medium me-2">Save As Draft</button> --}}
+                                    <input type="submit" value="Edit News" id="news_edit_btn"
+                                        class="ti-btn bg-primary text-white !py-1 !px-2 !text-[0.75rem] !font-medium">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="xxl:col-span-3 xl:col-span-12 lg:col-span-12 md:col-span-12 sm:col-span-12 col-span-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <div class="box-title">
+                                Recent News
+                            </div>
+                        </div>
+                        <div class="box-body" id="recentpost">
+                            <ul class="list-group">
+                                <!-- Recent posts will be dynamically added here -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--End::row-1 -->
+
+
+        </div>
+    </div>
+
+ 
+
+
+
+
+    <script>
+        $(document).ready(function() {
+            function fetchNews() {
+                $.ajax({
+                    url: "{{ route('fatch.news') }}",
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(response) {
+                        console.log(response);
+                        if (response.length > 0) {
+                            var recentPostHtml = '';
+                            $.each(response, function(index, post) {
+                                var truncatedDescription = post.description.split(' ').slice(0,
+                                    2).join(' ');
+                                var truncatedTitle = post.title.split(' ').slice(0, 2).join(
+                                ' ');
+
+                                var imageUrl = "{{asset('assets/images/dailynews/')}}"+ '/' + post.image;
+
+                                recentPostHtml += '<li class="list-group-item">';
+                                recentPostHtml +=
+                                    '<div class="flex gap-2 flex-wrap items-center">';
+                                recentPostHtml += '<span class="avatar avatar-xl me-1">';
+                                recentPostHtml += '<img src="' + imageUrl +
+                                    '" class="img-fluid !rounded-md" alt="...">';
+                                recentPostHtml += '</span>';
+                                recentPostHtml += '<div class="flex-grow">';
+                                recentPostHtml +=
+                                    '<a href="" class="text-[0.875rem] font-semibold mb-0">' +
+                                    truncatedTitle + '</a>';
+                                recentPostHtml +=
+                                    '<p class="mb-1 popular-blog-content text-truncate">' +
+                                    truncatedDescription + '</p>';
+                                recentPostHtml +=
+                                    '<span class="text-[#8c9097] dark:text-white/50 text-[0.6875rem]">' +
+                                    post.created_at + '</span>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '<div>';
+                                recentPostHtml +=
+                                    '<button aria-label="button" type="button" class="ti-btn ti-btn-light ti-btn-sm rtl:rotate-180"><i class="ri-arrow-right-s-line"></i></button>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '</div>';
+                                recentPostHtml += '</li>';
+                            });
+                            $('#recentpost .list-group').html(recentPostHtml);
+                        } else {
+                            // Handle case where no posts are returned
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        // Handle error
+                    }
+                });
+            }
+
+            $('#edit_news_form').submit(function(e) {
+                e.preventDefault();
+                $('#news_edit_btn').val('please wait..');
+                var token = $('meta[name="csrf-token"]').attr('content');
+                var formData = new FormData($(this)[0]);
+                
+                $.ajax({
+                    url:  "{{ route('edit.news') }}",
+                    method: 'post',
+                    data: formData,
+                    headers: {
+                        'X-CSRF-TOKEN': token
+                    },
+                    contentType: false,
+                    processData: false,
+                    success: function(response) {
+                        if (response.status == 400) {
+                            showError('news_title', response.messages.news_title);
+                            showError('news_slug', response.messages.news_slug);
+                            showError('meta_title', response.messages.meta_title);
+                            showError('canonical_url', response.messages.canonical_url);
+                            showError('meta_keyword', response.messages.meta_keyword);
+                            showError('meta_desc', response.messages.meta_desc);
+                            showError('editoronebest', response.messages.news_description);
+                            showError('news_status', response.messages.news_status);
+
+                            $('#news_edit_btn').val('Edit News');
+                        } else if (response.status == 200) {
+                            $('.invalid-feedback').empty();
+                            // $("#show_success_alert").html(showMessage('success', response
+                            //     .messages));
+                            $('#edit_news_form')[0].reset();
+                            removeValidationClass("#edit_news_form");
+                            $('#news_edit_btn').val('Edit News');
+                            alert(response.messages);
+                            window.location.href = "{{ route('admin.newslist') }}";
+                            fetchNews();
+                        }
+                    },
+                    error: function(xhr, status, error) {}
+                });
+            });
+            fetchNews();
+        });
+    </script>
+
+
+
+@endsection
