69d26897d0e0ae653cb332e3c627bf574ac5ed21
neeraj <tbs.neeraj@gmail.com>
2024-06-25 10:57:36 +0530

    assets\css\style.css resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -373,19 +373,25 @@
                                     class="hs-tab-active:bg-gray-200 hs-tab-active:text-gray-800 hs-tab-active:hover:text-gray-800 dark:hs-tab-active:bg-light dark:hs-tab-active:text-white py-3 px-4 inline-flex items-center gap-2 bg-transparent text-sm font-medium text-center text-defaulttextcolor rounded-sm hover:text-primary dark:text-[#8c9097] dark:text-white/50 dark:hover:text-white active"
                                     id="pills-on-gray-color-item-1" data-hs-tab="#pills-on-gray-color-1"
                                     aria-controls="pills-on-gray-color-1">
-                                    Business Network Setup
+                                    Remove Virus/Malware
                                 </button>
                                 <button type="button"
                                     class="hs-tab-active:bg-gray-200 hs-tab-active:text-gray-800 hs-tab-active:hover:text-gray-800 dark:hs-tab-active:bg-light dark:hs-tab-active:text-white py-3 px-4 inline-flex items-center gap-2 bg-transparent text-sm font-medium text-center text-defaulttextcolor rounded-sm hover:text-primary dark:text-[#8c9097] dark:text-white/50 dark:hover:text-white"
                                     id="pills-on-gray-color-item-2" data-hs-tab="#pills-on-gray-color-2"
                                     aria-controls="pills-on-gray-color-2">
-                                    Smart Device Setup
+                                    Smart Devices
                                 </button>
                                 <button type="button"
                                     class="hs-tab-active:bg-gray-200 hs-tab-active:text-gray-800 hs-tab-active:hover:text-gray-800 dark:hs-tab-active:bg-light dark:hs-tab-active:text-white py-3 px-4 inline-flex items-center gap-2 bg-transparent text-sm font-medium text-center text-defaulttextcolor rounded-sm hover:text-primary dark:text-[#8c9097] dark:text-white/50 dark:hover:text-white"
                                     id="pills-on-gray-color-item-3" data-hs-tab="#pills-on-gray-color-3"
                                     aria-controls="pills-on-gray-color-3">
-                                    Software Installation
+                                    Operating System Issues
+                                </button>
+                                <button type="button"
+                                    class="hs-tab-active:bg-gray-200 hs-tab-active:text-gray-800 hs-tab-active:hover:text-gray-800 dark:hs-tab-active:bg-light dark:hs-tab-active:text-white py-3 px-4 inline-flex items-center gap-2 bg-transparent text-sm font-medium text-center text-defaulttextcolor rounded-sm hover:text-primary dark:text-[#8c9097] dark:text-white/50 dark:hover:text-white"
+                                    id="pills-on-gray-color-item-4" data-hs-tab="#pills-on-gray-color-4"
+                                    aria-controls="pills-on-gray-color-4">
+                                    System Fixing
                                 </button>
                             </nav>
 
@@ -457,6 +463,38 @@
                                 </div>
                                 <div id="pills-on-gray-color-3" class="hidden" role="tabpanel"
                                     aria-labelledby="pills-on-gray-color-item-3">
+                                    <h3>Smart Device Setup:</h3>
+                                    <p class="mt-2"> From your home networks to workplace security devices, we will help
+                                        you optimize them for optimal functioning only.</p>
+                                    <div class="flex mt-5">
+                                        <div class="tab-icon">
+                                            <i class="bi bi-hdd-stack"></i>
+                                            <p>Watch Over Your Family from Anywhere</p>
+                                        </div>
+                                        <div class="tab-icon">
+                                            <i class="bi bi-hdd-stack"></i>
+                                            <p>Eliminate Common & Complex Issues</p>
+                                        </div>
+                                        <div class="tab-icon">
+                                            <i class="bi bi-hdd-stack"></i>
+                                            <p>Top-Notch Experience Without Lagging</p>
+                                        </div>
+                                    </div>
+                                    <div class="flex pt-2 pb-2">
+                                        <div class="price-tab">
+                                            <h4>$69</h4>
+                                        </div>
+                                        <div class="price-cnt">
+                                            <h5>1 Time</h5>
+                                            <p>Home Network Setup</p>
+                                        </div>
+                                    </div>
+                                    <div class="tab-btn-buy">
+                                        <a href="#">Buy Now</a>
+                                    </div>
+                                </div>
+                                <div id="pills-on-gray-color-4" class="hidden" role="tabpanel"
+                                    aria-labelledby="pills-on-gray-color-item-4">
                                     <h3>Software Installation:</h3>
                                     <p class="mt-2"> Software installation can be difficult to eliminate without
                                         professional advice. So, contact us to ensure your software is set up and
