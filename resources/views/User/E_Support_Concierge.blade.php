5bd3b7eaffdb041dc3235432883ec7b1ece892e8
neeraj <tbs.neeraj@gmail.com>
2024-06-25 13:23:37 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -106,7 +106,7 @@
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
                         <h2 class="mb-5">What We Offer?</h2>
                         <img src="{{asset('assets/e-support/What-We-Offer.png') }}" alt=""
-                    class="img-fluid" width="100%">
+                    class="img-fluid" width="95%">
                     </div>
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
                         <div class="sct-spacing"></div>
