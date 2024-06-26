f9ecbd914ab3b20dad42b6c746c63a31aa83f8c6
neeraj <tbs.neeraj@gmail.com>
2024-06-26 15:45:21 +0530

    resources\views\User\E_Support_Concierge.blade.php

--- resources/views/User/E_Support_Concierge.blade.php
+++ resources/views/User/E_Support_Concierge.blade.php
@@ -19,8 +19,8 @@
                 <div class="grid grid-cols-12 gap-6">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-span-12 col-span-12">
                         <h2>Your Personal Tech Lifeline</h2>
-                        <h3>On-Demand Remote Services for All Things Tech</h3>
-                        <p class="mt-3">Technology hiccups can be frustrating, and when they happen, you need a reliable
+                        <h3 class="mt-3">On-Demand Remote Services for All Things Tech</h3>
+                        <p class="mt-5">Technology hiccups can be frustrating, and when they happen, you need a reliable
                             partner to a turn to. TTB eSupport Concierge can help fill the blanks.</p>
                         <p class="mt-5">From hardware setup to software troubles, and everything in between, we got you
                             covered!</p>
