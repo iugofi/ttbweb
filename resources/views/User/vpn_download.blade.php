dab5a01f1f11987364522275013380fe9da4cea7
neeraj <tbs.neeraj@gmail.com>
2024-06-27 12:48:20 +0530

    assets\css\style.css resources\views\User\vpn_download.blade.php

--- resources/views/User/vpn_download.blade.php
+++ resources/views/User/vpn_download.blade.php
@@ -24,14 +24,14 @@
                     <div class="grid grid-cols-12 gap-6">
                         <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 md:col-span-3 col-span-12 text-center">
                         </div>
-                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 md:col-span-3 col-span-12 text-center">
+                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 md:col-span-3 col-span-12 text-center box-down">
                             <img src="{{asset('assets/img/mac.png') }}" alt="" class="img-fluid" width="20%">
                             <div>
                                 <a href="#">Download For Mac</a>
                                 <span> X10.15 or above</span>
                             </div>
                         </div>
-                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 md:col-span-3 col-span-12 text-center">
+                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 md:col-span-3 col-span-12 text-center box-down">
                             <img src="{{asset('assets/img/ios.png') }}" alt="" class="img-fluid" width="20%">
                             <div>
                                 <a href="#">Download For iOS</a>
