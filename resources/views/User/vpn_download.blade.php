6dfc4fb36945a2fc5d73f57292d7e85f9cb5a224
neeraj <tbs.neeraj@gmail.com>
2024-06-27 12:01:20 +0530

    assets\css\style.css resources\views\User\vpn_download.blade.php

--- resources/views/User/vpn_download.blade.php
+++ resources/views/User/vpn_download.blade.php
@@ -16,9 +16,9 @@
             <section class="vpn-download-one">
                 <div class="container">
                     <div class="grid grid-cols-12 gap-6">
-                        <div class="xxl:col-span-12 xl:col-span12 lg:col-span-12 md:col-span-12 col-span-12">
-                            <h2>Add An Extra Layer Of Protection To Your Device.</h2>
-                            <p>Select the option that works best for you, below: </p>
+                        <div class="xxl:col-span-12 xl:col-span12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
+                            <h2>Add An Extra Layer Of <span>Protection</span> To Your Device.</h2>
+                            <p class="mt-2">Select the option that works best for you, below: </p>
                         </div>
                     </div>
                     <div class="grid grid-cols-12 gap-6">
