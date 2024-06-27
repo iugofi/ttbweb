b72d2c068759ca501f83ec0ced1dec5a2eb0c259
kunal <kunal.iugofi@gmail.com>
2024-06-27 14:19:33 +0530

    resources\views\Errors\500.blade.php resources\views\User\passwordreset.blade.php

--- resources/views/User/passwordreset.blade.php
+++ resources/views/User/passwordreset.blade.php
@@ -8,17 +8,16 @@
     <!-- META DATA -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Author" content="Spruko Technologies Private Limited">
-    <meta name="Description" content="Sign in">
+    <meta name="Author" content="">
+    <meta name="Description" content="Password reset">
     <meta name="keywords"
-        content="admin template, admin dashboard, php admin panel, admin, tailwind css dashboard, php admin dashboard, tailwind admin template, tailwind template, php dashboard, dashboard, tailwind, tailwind dashboard, tailwind css, tailwind css template">
+        content="password resret">
 
     <!-- TITLE -->
     <title> Reset Password </title>
 
     <!-- FAVICON -->
-    <link rel="icon" href="https://php.spruko.com/tailwind/ynex/ynex/assets/images/brand-logos/favicon.ico"
-        type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/images/brand-logos/favicon.png') }}" type="image/x-icon">
     <!-- ICONS CSS -->
     <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet">
 
