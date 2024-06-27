53b28cf42f6c19514e5a762762d1370764d5acf1
neeraj <tbs.neeraj@gmail.com>
2024-06-27 17:05:31 +0530

    resources\views\User\antivirus_download.blade.php

--- resources/views/User/antivirus_download.blade.php
+++ resources/views/User/antivirus_download.blade.php
@@ -16,13 +16,13 @@
             <section class="vpn-download-one">
                 <div class="container">
                     <div class="grid grid-cols-12 gap-6">
-                        <div class="xxl:col-span-12 xl:col-span12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
+                        <div class="xxl:col-span-12 xl:col-span12 lg:col-span-12 md:col-span-12 col-span-12 text-center ">
                             <h2>Add An Extra Layer Of <span>Protection</span> To Your Device.</h2>
                             <p class="mt-2">Select the option that works best for you, below: </p>
                         </div>
                     </div>
                     <div class="grid grid-cols-12 gap-6 pt-top">
-                        <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12 text-center">
+                        <div class="xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 col-span-12 text-center  box-down">
                             <img src="{{ asset('assets/img/ios.png') }}" alt="" class="img-fluid" width="25%">
                             <div class="btn-down">
                                 <a href="#">Download For iOS</a>
