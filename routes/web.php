0e48873cdd98a2055250c619c2edb407f54ac519
kunal <kunal.iugofi@gmail.com>
2024-06-27 15:53:44 +0530

    routes\web.php

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use App\Http\Controllers\ExcelController;
 //     return view('welcome');
 // });
 
-Artisan::call('down');
+Artisan::call('up');
 
 //forget pass
 Route::post('/forget-pass', [UserController::class, 'forgetpass'])->name('user.forgetpass');
