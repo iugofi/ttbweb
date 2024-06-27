4c40eb2b8a08154d976e600bcd9f44c4b30db467
kunal <kunal.iugofi@gmail.com>
2024-06-27 12:16:24 +0530

    resources\views\User\resetpassword-basic.blade.php

--- resources/views/User/resetpassword-basic.blade.php
+++ resources/views/User/resetpassword-basic.blade.php
@@ -32,7 +32,11 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="{{ asset('assets/js/message.js') }}"></script>
 
-
+    <style>
+        body {
+       background-color: rgb(var(--body-bg));
+   }
+   </style>
 
 </head>
 
