29ad31ee8a562099d1e726f5654d55aa5db3e103
kunal <kunal.iugofi@gmail.com>
2024-06-28 13:06:24 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -54,9 +54,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            </div>
+                           
                         </div>
                     </div>
                   
