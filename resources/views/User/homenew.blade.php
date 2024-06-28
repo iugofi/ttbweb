595ff7935bbb1e290620184b8c96bdfc5c695eb1
kunal <kunal.iugofi@gmail.com>
2024-06-28 15:04:59 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -24,11 +24,12 @@
     <div class="content" style="background-color:#F3F2EA;">
         <div class="main-content">
 
-            <button type="button" class="hs-dropdown-toggle toggle_new ti-btn ti-btn-primary-full"
-                data-hs-overlay="#hs-extralarge-modal" style="display: none;">
-                Extra Large modal
-            </button>
+          
             @if ($events == true)
+            <button type="button" class="hs-dropdown-toggle toggle_new ti-btn ti-btn-primary-full"
+            data-hs-overlay="#hs-extralarge-modal" style="display: none;">
+            Extra Large modal
+        </button>
                 <div id="hs-extralarge-modal" class="hs-overlay hidden ti-modal">
                     <div class="hs-overlay-open:mt-7 ti-modal-box mt-0 ease-out lg:!max-w-4xl lg:w-full m-3 lg:!mx-auto">
                         <div class="ti-modal-content">
