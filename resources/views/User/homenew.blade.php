8ab6b1c25cf943de75ecf37e0d4d2d3ae99e20cd
kunal <kunal.iugofi@gmail.com>
2024-06-28 13:15:57 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -6,6 +6,15 @@
 @section('canonical', '')
 @section('content')
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        function clickButton() {
+            document.querySelector(".hs-dropdown-toggle").click();
+        }
+        clickButton();
+        setTimeout(clickButton, 30);
+    });
+</script>
 
     <div class="content" style="background-color:#F3F2EA;">
         <div class="main-content">
