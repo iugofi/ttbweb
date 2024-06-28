976833e5ca9318a880b090f4823bdbf5367cc38d
kunal <kunal.iugofi@gmail.com>
2024-06-28 12:06:55 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -39,11 +39,10 @@
                     </div>
                 </div>
             </div>
-
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
             <script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
                 document.addEventListener('DOMContentLoaded', function () {
                     var myModal = new bootstrap.Modal(document.getElementById('eventsModal'), {
                         backdrop: 'static',
