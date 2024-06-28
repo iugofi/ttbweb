d104cf6ace094f6e252806394377cfae405631ad
kunal <kunal.iugofi@gmail.com>
2024-06-28 12:13:24 +0530

    app\Http\Controllers\UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -247,7 +247,7 @@ public function resetpass($reset_id)
         ->where('storepick.PICK_ID','=',501)
         ->get();
         $events =Event::where('event_status', 101)->get();
-        dd($events);
+        dd($vpnshield);
         return view('User.homenew',['antivirus'=>$antivirus,'vpnshield'=>$vpnshield,'events'=>$events]);
     }
     public function malware_detection()
