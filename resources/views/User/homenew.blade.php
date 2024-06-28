ab4cd92d78358132875e0f93cf825634cc9bf23e
kunal <kunal.iugofi@gmail.com>
2024-06-28 12:51:31 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -41,16 +41,13 @@
             </div>
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
             <script>
-                document.addEventListener('DOMContentLoaded', function () {
-                    var myModal = new bootstrap.Modal(document.getElementById('eventsModal'), {
-                        backdrop: 'static',
-                        keyboard: false
-                    });
-                    myModal.show();
+                $(document).ready(function(){
+                    $('#eventsModal').modal('show');
                 });
             </script>
+
 
             <section class="banner-home-one">
                 <div class="container">
