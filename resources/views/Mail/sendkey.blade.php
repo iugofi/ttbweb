03e111ab4714ae09b417510909450ad280158b83
kunal <kunal.iugofi@gmail.com>
2024-06-29 17:27:21 +0530

    resources\views\Mail\sendkey.blade.php

--- resources/views/Mail/sendkey.blade.php
+++ resources/views/Mail/sendkey.blade.php
@@ -1,3 +1,17 @@
+@php
+	
+    // Perform the database query directly in PHP within Blade
+    $paymentDetails = DB::table('payments')
+        ->join('product_details', 'product_details.id', '=', 'payments.product_id')
+        ->join('usersall', 'usersall.id', '=', 'payments.user_id')
+        ->select('payments.*', 'product_details.*', 'usersall.*')
+        ->where('payments.pay_id', $payment_intent)
+        ->where('payments.product_key', $main_key)
+        ->first();
+
+@endphp
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +20,23 @@
     <title>Vpn Kay</title>
 </head>
 <body>
-    <h1>Your Vpn Key: {{ $main_key }}</h1>
-    <p>payment_intent: {{ $payment_intent }}</p>
+    @if ($paymentDetails)
+    <h2>User Information</h2>
+    <p>Name: {{ $paymentDetails->firstname }}</p>
+    <p>Email: {{ $paymentDetails->email }}</p>
+    <h2>Product Information</h2>
+    <p>key type:{{$paymentDetails->key_type}}</p>
+    <p>key type:{{$paymentDetails->plan_id}}</p>
+  
+
+    <h2>Payment Information</h2>
+    <p>Payment ID: {{ $paymentDetails->pay_id }}</p>
+    <p>Amount: {{ $paymentDetails->amount_total }}</p>
+    <p>Status: {{ $paymentDetails->payment_status }}</p>
+
+    <p>{{$main_key}}</p>
+    @else
+    <p>No payment details found.</p>
+@endif
 </body>
 </html>
