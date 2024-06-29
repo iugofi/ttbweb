0bf40ca850fd4362ff8f0e24d06f7d8bca389c4e
kunal <kunal.iugofi@gmail.com>
2024-06-29 11:15:13 +0530

    app\Http\Kernel.php config\cookie-consent.php lang\vendor\cookie-consent\ar\texts.php lang\vendor\cookie-consent\bg\texts.php lang\vendor\cookie-consent\bn\texts.php lang\vendor\cookie-consent\cs\texts.php lang\vendor\cookie-consent\da\texts.php lang\vendor\cookie-consent\de\texts.php lang\vendor\cookie-consent\el\texts.php lang\vendor\cookie-consent\en\texts.php lang\vendor\cookie-consent\eo\texts.php lang\vendor\cookie-consent\es\texts.php lang\vendor\cookie-consent\et\texts.php lang\vendor\cookie-consent\fr\texts.php lang\vendor\cookie-consent\hu\texts.php lang\vendor\cookie-consent\id\texts.php lang\vendor\cookie-consent\it\texts.php lang\vendor\cookie-consent\kr\texts.php lang\vendor\cookie-consent\mk\texts.php lang\vendor\cookie-consent\nb\texts.php lang\vendor\cookie-consent\nl\texts.php lang\vendor\cookie-consent\oc\texts.php lang\vendor\cookie-consent\pl\texts.php lang\vendor\cookie-consent\pt\texts.php lang\vendor\cookie-consent\pt_BR\texts.php lang\vendor\cookie-consent\ro\texts.php lang\vendor\cookie-consent\ru\texts.php lang\vendor\cookie-consent\sk\texts.php lang\vendor\cookie-consent\sv\texts.php lang\vendor\cookie-consent\th\texts.php lang\vendor\cookie-consent\tr\texts.php lang\vendor\cookie-consent\vi\texts.php resources\views\vendor\cookie-consent\dialogContents.blade.php resources\views\vendor\cookie-consent\index.blade.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         // \Statikbe\CookieConsent\CookieConsentMiddleware::class,
-        
+        Spatie\CookieConsent\CookieConsentServiceProvider::class,
     ];
 
     /**
