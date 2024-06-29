15e862e091e57c5c4788d81c02c65f958be886c2
kunal <kunal.iugofi@gmail.com>
2024-06-29 15:08:17 +0530

    routes\web.php

--- routes/web.php
+++ routes/web.php
@@ -134,7 +134,11 @@ Route::get('/network-protection', [UserController::class, 'network_protection'])
 Route::get('/', [UserController::class, 'index'])->name('user.index');
 
 
-
+Route::group([
+   'middleware' => ['cookie-consent']
+], function(){
+   // ...
+});
 
 
 
