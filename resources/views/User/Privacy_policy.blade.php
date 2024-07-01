bd568077edecc84967bd4dc46f6326ab6eb98af6
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-01 11:16:54 +0530

    resources\views\User\Privacy_policy.blade.php

--- resources/views/User/Privacy_policy.blade.php
+++ resources/views/User/Privacy_policy.blade.php
@@ -32,7 +32,7 @@
         <!-- End::first Content -->
 
 
-        <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem] ">
+        <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]" style="padding-top: 10rem;">
             <div class="container Privacy-container">
                 <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
                     
@@ -42,7 +42,7 @@
                             And Better Skills</h1> --}}
                         <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">We at TTB care deeply about privacy, security and online safety. Our mission is to protect our users from the risks of theft,
                              disruption, and unauthorized access to their online information. In our privacy policy, we explain what we do, how we collect, use and share your personal information through our website.
-                            <br>
+                            <br><br>
                             TTB sells products and services directly to the consumers and this privacy policy applies to the information we collect from you or your device when you download our services.
                         </p>
 
