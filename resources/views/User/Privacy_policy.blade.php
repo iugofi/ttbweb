89953810dd95800c9d9c04e9825146fe07746030
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-01 11:00:37 +0530

    resources\views\User\Privacy_policy.blade.php

--- resources/views/User/Privacy_policy.blade.php
+++ resources/views/User/Privacy_policy.blade.php
@@ -19,7 +19,7 @@
             <section class="section">
                 <div class="container main-banner-container">
                     <div class="grid grid-cols-12 gap-6">
-                        <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
+                        <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12 text-center">
                             <div class="lg:py-[3rem]">
                                 <span class="first-color text-4xl font-bold">Privacy Policy</span>
 
