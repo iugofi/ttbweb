a83ba6695cdba95d853dd23938bda49719f62c2b
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-01 17:56:59 +0530

    resources\views\User\Return_Refund_Policy.blade.php

--- resources/views/User/Return_Refund_Policy.blade.php
+++ resources/views/User/Return_Refund_Policy.blade.php
@@ -65,9 +65,9 @@
                 <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
                     
                     <h2 class="lg:text-center font-semibold mb-2 text-[1.8rem]">Cancellation & Refund Policy</h2>
-                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">Techboy Solutions offer a 30 day money-back guarantee on subscriptions for certain Services and Solutions that the end-user purchases directly from us through our online retail stores or through our partner e-commerce stores. If your purchase qualifies, and you follow all the instructions in this “Cancellation and Refund Policy” within 30 days of the date of purchase, we will terminate your subscription and refund 100% of the price you have paid for your then-current Subscription Term.<br>
-                        We also allow you to cancel your subscription and request a refund (prorated for the unexpired or unused portion of the Subscription Term) if we provide notice to you that we are amending the End User License Agreement we entered into with you in respect of such subscription and/or Solution (the “EULA”), and you object to such amendment within 30 days of the date of such notice.<br>
-                        If your subscription is conditioned on you paying a fee or charge, and if we have not received payment by the 15th day following the payment due date, you will be deemed to have surrendered your license to use the Solution, and the license will terminate immediately without further action by you or us.<br><br>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">Techboy Solutions offer a 30 day money-back guarantee on subscriptions for certain Services and Solutions that the end-user purchases directly from us through our online retail stores or through our partner e-commerce stores. If your purchase qualifies, and you follow all the instructions in this “Cancellation and Refund Policy” within 30 days of the date of purchase, we will terminate your subscription and refund 100% of the price you have paid for your then-current Subscription Term.<br><br>
+                        We also allow you to cancel your subscription and request a refund (prorated for the unexpired or unused portion of the Subscription Term) if we provide notice to you that we are amending the End User License Agreement we entered into with you in respect of such subscription and/or Solution (the “EULA”), and you object to such amendment within 30 days of the date of such notice.<br><br>
+                        If your subscription is conditioned on you paying a fee or charge, and if we have not received payment by the 15th day following the payment due date, you will be deemed to have surrendered your license to use the Solution, and the license will terminate immediately without further action by you or us.<br><br><br>
                     </p>
 
                 </div>
@@ -101,7 +101,7 @@
                         You can find the Transaction ID by:<br><br>
                         <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>finding the email sent on your email address to confirm your purchase; or</p>
                         <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i> logging into the e-commerce account and then finding the specific transaction in the Orders section.</p><br>
-                        <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">Once you have both these details, click on Request a Refund from TTB and follow the instructions on the linked page.<br>,br>
+                        <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">Once you have both these details, click on Request a Refund from TTB and follow the instructions on the linked page.<br>
                    </p>
                 </div>
             </div>
