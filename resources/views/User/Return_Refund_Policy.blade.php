504bef6e514c042f4912c66a70a84a8bee44dab3
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-01 17:00:26 +0530

    resources\views\User\Return_Refund_Policy.blade.php

--- resources/views/User/Return_Refund_Policy.blade.php
+++ resources/views/User/Return_Refund_Policy.blade.php
@@ -7,7 +7,7 @@
 @section('keywords', '')
 @section('canonical', '')
 
-
+@section('content')
 
 <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70" id="totel-internet-security">
 
