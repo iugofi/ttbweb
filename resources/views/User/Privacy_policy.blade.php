3c3e0beb122702ad0703ab5d9d27bfba82a60bcc
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-01 16:09:48 +0530

    resources\views\User\Privacy_policy.blade.php

--- resources/views/User/Privacy_policy.blade.php
+++ resources/views/User/Privacy_policy.blade.php
@@ -57,13 +57,36 @@
                     
                     <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
                         
-                        <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">What kind of information do we collect?</h3>
+                        <h1 class="lg:text-center font-semibold mb-2 text-[1.8rem]">What kind of information do we collect?</h1>
                         <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">We collect information in order to provide our product and services.
                              Some information, you provide directly to us, some of the information is collected through our services and some through third-parties.
                               The type of personal information may include:<br><br>
                               <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Contact information (such as name, email address, phone number, and mailing address)</p>
                               <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Payment information (such as payment card number, billing address, bank account information)</p>
                               <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Account log in credentials</p>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem;">
+            <div class="container Privacy-container">
+                <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                    
+                    <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                        
+                        <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">How do we use the information that we collect?</h3>
+                        <span class="lg:text-center font-semibold mb-2 text-[1.5rem]">To help protect you</span>
+                        <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">TTB may use your information for:<br><br>
+                              <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Analyze the data to isolate and identify threats, vulnerabilities, suspicious activities, spam, viruses and attacks and communicate with you about potential threats.</p>
+                              <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Participate in threat intelligence networks, conduct research, and adapt services and products to help respond to threats.</p>
+                              <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Encrypt your data, back-up or recover your data.</p>
+                              <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Check for service updates</p>
+                              <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 "><i style="width: 6%; color: #12D2B3; font-size: 18px;" class="bi bi-check2-all me-2"></i>Look for misuses of your data</p>
+
                         </p>
 
                     </div>
