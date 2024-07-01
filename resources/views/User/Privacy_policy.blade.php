91a1614099dc2aced08defc09dbe12e4f9421b31
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-01 11:11:00 +0530

    resources\views\User\Privacy_policy.blade.php

--- resources/views/User/Privacy_policy.blade.php
+++ resources/views/User/Privacy_policy.blade.php
@@ -32,6 +32,26 @@
         <!-- End::first Content -->
 
 
+        <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem] ">
+            <div class="container ttbantivirus-container">
+                <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                    
+                    <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                        
+                        {{-- <h1 class="lg:text-center font-semibold mb-2 text-[2rem]">Good Qualification Services
+                            And Better Skills</h1> --}}
+                        <p class=" text-[0.9rem] text-[#8c9097] dark:text-white/50 mb-4 ">We at TTB care deeply about privacy, security and online safety. Our mission is to protect our users from the risks of theft,
+                             disruption, and unauthorized access to their online information. In our privacy policy, we explain what we do, how we collect, use and share your personal information through our website.
+                            <br>
+                            TTB sells products and services directly to the consumers and this privacy policy applies to the information we collect from you or your device when you download our services.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
 
 
 
