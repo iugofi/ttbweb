0605a7f133f13b6f4e10ae3b76dc6e29a29aabe4
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-02 13:01:20 +0530

    resources\views\User\success.blade.php

--- resources/views/User/success.blade.php
+++ resources/views/User/success.blade.php
@@ -22,7 +22,7 @@ hidefooter.style.display="none";
     <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70" id="totel-internet-security">
 
 
-    <meta charset="UTF-8">
+    {{-- <meta charset="UTF-8">
 
         <meta charset="UTF-8">
 
@@ -147,7 +147,83 @@ hidefooter.style.display="none";
 
 
         
+    </div> --}}
+
+    <!-- Mirrored from php.spruko.com/tailwind/ynex/ynex/pages/error500.php by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 13 Mar 2024 10:03:46 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+
+    <!-- META DATA -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Spruko Technologies Private Limited">
+    <meta name="Description" content="Ynex - PHP Tailwind CSS Admin & Dashboard Template">
+    <meta name="keywords" content="admin template, admin dashboard, php admin panel, admin, tailwind css dashboard, php admin dashboard, tailwind admin template, tailwind template, php dashboard, dashboard, tailwind, tailwind dashboard, tailwind css, tailwind css template">
+    
+    <!-- TITLE -->
+    <title> Ynex - PHP Tailwind CSS Admin & Dashboard Template </title>
+
+    <!-- FAVICON -->
+    <link rel="icon" href="https://php.spruko.com/tailwind/ynex/ynex/assets/images/brand-logos/favicon.ico" type="image/x-icon">
+
+    <!-- ICONS CSS -->
+    <link href="assets/css/icons.css" rel="stylesheet">
+    
+    <!-- STYLE CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- MAIN JS -->
+    <script src="assets/js/authentication-main.js"></script>
+
+    
+
+
+
+</head>
+
+<body>
+
+
+
+    <div class="page error-bg dark:!bg-bodybg" id="particles-js">
+        <!-- Start::error-page -->
+        <div class="error-page">
+            <div class="container text-defaulttextcolor dark:text-defaulttextcolor/70 text-defaultsize">
+                <div class="text-center p-5 my-auto">
+                    <div class="flex items-center justify-center h-full">
+                      <div class="xl:col-span-3"></div>
+                        <div class="xl:col-span-6 col-span-12">
+                            <!-- <p class="error-text sm:mb-0 mb-2">500</p> -->
+                            <img src="assets/images/media/paysuccess.jpg" alt="">
+                            <p class="text-[1.125rem] font-semibold mb-4">Oops 😭,The page you are looking for is not available.</p>
+                            <div class="flex justify-center items-center mb-[3rem]">
+                                <div class="xl:col-span-6 w-[50%]">
+                                    <p class="mb-0 opacity-[0.7]">We are sorry for the inconvenience,The page you are trying to access has been removed or never been existed.</p>
+                                </div>
+                            </div>
+                            <a href="index.html" class="ti-btn bg-primary text-white font-semibold"><i class="ri-arrow-left-line align-middle inline-block"></i>BACK TO HOME</a>
+                        </div>
+                        <div class="xl:col-span-3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+
+    
+    <!-- SCRIPTS -->
+
+    
+    <!-- Particles JS -->
+    <script src="assets/libs/particles.js/particles.js"></script>
+
+    <!-- Error JS -->
+    <script src="assets/js/error.js"></script>
+
+
+    <!-- END SCRIPTS -->
+
 </div>
 
          
