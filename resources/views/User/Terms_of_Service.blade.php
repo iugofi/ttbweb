4f14d3dedabef5ebda6c69d8fa488f73fe22a031
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-02 10:55:18 +0530

    resources\views\User\Terms_of_Service.blade.php

--- resources/views/User/Terms_of_Service.blade.php
+++ resources/views/User/Terms_of_Service.blade.php
@@ -2,7 +2,7 @@
 
 @extends('User.Layouts.layout')
 
-@section('title', 'Terms of Service')
+@section('title', 'Terms& Conditions')
 @section('Description', '')
 @section('keywords', '')
 @section('canonical', '')
@@ -11,21 +11,158 @@
 
 @section('content')
 
+<div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70" id="totel-internet-security">
 
-<div class="content">
-    <div class="main-content">
+    <!-- Start::first Content -->
+    <div class="Return-Refund-Policy" id="Return-Refund">
+        <section class="section">
+            <div class="container main-banner-container">
+                <div class="grid grid-cols-12 gap-6 mt-5">
+                    <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12 text-center pt-5 mt-5">
+                        <div class="lg:py-[3rem] mt-5">
+                            <h1 class="first-color text-4xl font-bold" style="padding: 6rem">Terms & Conditions</h1>
 
-
-
-
-
-
-        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
+    <!-- End::first Content -->
+
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]" style="padding-top: 0; padding-bottom: 0rem">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 mt-5 pb-0 lg:px-2 !text-start">
+                    
+                    {{-- <h1 class="lg:text-center font-semibold mb-2 text-[2rem]">Good Qualification Services
+                        And Better Skills</h1> --}}
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">This page consists the terms and conditions of the usage of the TTB Security Products.
+                         Where ever the term we, our, us, TTB, TTB Security System, TTB Security Products and TTB Securities is used is referred to the brand name TTB. 
+                         The terms you, yours, yourself, user are in reference for the user/ buyer.
+                        <br><br>
+                        Please read carefully the terms and conditions before installing or using any of our products. You should understand that if you are using any 
+                        product of TTB Security System, you are bound by these terms and conditions. These terms and conditions constitute a legal contract between you and TTB Securities. If you do not agree to these terms and conditions, you may not accept the agreement and may not our products.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem; padding-bottom: 0rem;">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    
+                    <h2 class="lg:text-center font-semibold mb-2 text-[1.8rem]">USER CONDITIONS</h2>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">Your use of TTB Security products is governed by these
+                         terms and conditions which you should read carefully before using any product of TTB Securities. These conditions 
+                         represent a contract between you and TTB. If you disagree to the terms and conditions, please do not install or
+                          use any product from TTB Securities.</p>
+                          <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">These terms and conditions can be updated or amended
+                             with time. TTB takes no responsibility in providing any notification regarding updates and amendments to the user.
+                              Kindly, keep yourself updated timely.</p>
+                              <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">These terms and conditions can be updated or amended with time.</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem; padding-bottom: 0rem;">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    
+                    <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">CONFIDENTIAL AGREEMENT</h3>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">The parties may disclose to each other confidential information
+                         which may include information concerning their respective business and technology. The products and product information are the 
+                         confidential information of TTB. Each party must agree to maintain all the confidential information of all the other party in 
+                         confidence, to not use or disclose any confidential information except permitted by this agreement.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem; padding-bottom: 0rem;">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    
+                    <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">OWNERSHIP</h3>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">TTB own and retain all rights, title and interest to the products,
+                         including all the intellectual property and proprietary rights. TTB owns and retains all right, title and interest in all software,
+                          documentation, questionnaire, algorithms, logic, methodologies, charts, reports or any other item. TTB reserves all rights.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem; padding-bottom: 0rem;">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    
+                    <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">SUPPORT</h3>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">TTB may, in its sole discretion, provide certain support and
+                         consultation to its customers. TTB is obligated to correct any bugs, defects or errors in the products.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem; padding-bottom: 0rem;">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    
+                    <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">UPDATES</h3>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">TTB will provide you updates to the product, malware signatures
+                         and content. A user needs to update the security from time-to-time.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem; padding-bottom: 0rem;">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    
+                    <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">PRIVACY POLICY</h3>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">TTB Securities recognizes the importance of maintaining the 
+                        privacy of personal data which it possesses and collects. By accepting the terms and conditions, you agree to be bound by TTB.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section text-defaulttextcolor dark:text-defaulttextcolor/70 text-[0.813rem]"style="padding-top: 0rem; padding-bottom: 0rem; margin-bottom: 2rem;">
+        <div class="container Privacy-container">
+            <div class="grid grid xl:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 justify-center align-center justify-center align-center">
+                
+                <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 pt-5 pb-0 lg:px-2 !text-start">
+                    
+                    <h3 class="lg:text-center font-semibold mb-2 text-[1.8rem]">LANGUAGE</h3>
+                    <p class=" text-[1rem] text-[#000000bd] dark:text-white/50 mb-4 ">The governing language is English. This agreement can
+                         only be modified or amended with TTB’s written consent.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 </div>
-
-         
-
-
 
 @endsection
