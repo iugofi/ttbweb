92a37961c42c17c43642b5cf40e5ce91a5f4e19f
kunal <kunal.iugofi@gmail.com>
2024-07-03 10:22:33 +0530

    app\Http\Controllers\UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -777,9 +777,9 @@ public function signdata(Request $request)
 
     public function invoice($pay_id)
     {
-        $decryptid=\Crypt::decrypt($pay_id);
-        dd($decryptid);
-        $printpay=Payments::find($decryptid);
+        $printpay=\Crypt::decrypt($pay_id);
+        // dd($decryptid);
+        // $printpay=Payments::where($decryptid);
         return view('invoice.ttbinvoice',['printpay'=>$printpay]);
     }
 
