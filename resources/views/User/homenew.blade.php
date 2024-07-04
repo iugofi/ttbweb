599efbf6d8e7efb330a305d3050f7f20c9961bf6
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-04 15:45:14 +0530

    resources\views\User\homenew.blade.php

--- resources/views/User/homenew.blade.php
+++ resources/views/User/homenew.blade.php
@@ -1175,193 +1175,346 @@
     </section>
 
 ===================================================================================================================================
-<div class="container py-5">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="testimonial-bg">
-                <div id="testimonial-slider" class="owl-carousel">
-                    <div class="testimonial">
-                        <p class="description">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-                        </p>
-                        <div class="pic">
-                            <img src="http://www.webcoderskull.com/img/team2.png" alt="">
-                        </div>
-                        <h3 class="title">Manish,</h3>
-                        <span class="post">Web Developer</span>
-                    </div>
- 
-                    <div class="testimonial">
-                        <p class="description">
-                           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since theLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-                        </p>
-                        <div class="pic">
-                            <img src="http://www.webcoderskull.com/img/team1.png" alt="">
-                        </div>
-                        <h3 class="title">Kundan,</h3>
-                        <span class="post">Front End Developer</span>
-                    </div>
-                </div>
-            </div>
+<div class="gtco-testimonials">
+    <h2>Testimonials Carousel - Cards Comments</h2>
+    <div class="owl-carousel owl-carousel1 owl-theme">
+      <div>
+        <div class="card text-center"><img class="card-img-top" src="https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300" alt="">
+          <div class="card-body">
+            <h5>Ronne Galle <br />
+              <span> Project Manager </span>
+            </h5>
+            <p class="card-text">“ Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
+              impedit quo minus id quod maxime placeat ” </p>
+          </div>
         </div>
+      </div>
+      <div>
+        <div class="card text-center"><img class="card-img-top" src="https://images.unsplash.com/photo-1588361035994-295e21daa761?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=301&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=301" alt="">
+          <div class="card-body">
+            <h5>Missy Limana<br />
+              <span> Engineer </span>
+            </h5>
+            <p class="card-text">“ Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
+              impedit quo minus id quod maxime placeat ” </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card text-center"><img class="card-img-top" src="https://images.unsplash.com/photo-1575377222312-dd1a63a51638?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=302&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=302" alt="">
+          <div class="card-body">
+            <h5>Martha Brown<br />
+              <span> Project Manager </span>
+            </h5>
+            <p class="card-text">“ Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
+              impedit quo minus id quod maxime placeat ” </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card text-center"><img class="card-img-top" src="https://images.unsplash.com/photo-1549836938-d278c5d46d20?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=303&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=303" alt="">
+          <div class="card-body">
+            <h5>Hanna Lisem<br />
+              <span> Project Manager </span>
+            </h5>
+            <p class="card-text">“ Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
+              impedit quo minus id quod maxime placeat ” </p>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-<style>
-
-    body{
+  </div>
   
-  background: #ee0979;
-  background: -webkit-linear-gradient(to right, #11998e, #38ef7d);
-  background: linear-gradient(to right, #11998e, #38ef7d)
-
-}
-.testimonial-bg{
- background-color:#fff;
-  border: 5px solid #ee0979;
-   font-family: 'Raleway', sans-serif;
-  padding: 35px 70px;
-  text-align: center;
-  position: relative;
-}
-.testimonial-bg:before{
-  content: "\f10d";
-  font-family: "fontawesome";
-  width: 75px;
-  height: 75px;
-border: 5px solid #ee0979;
-  line-height: 75px;
-  background: #fff;
-  text-align: center;
-  font-size: 50px;
-  color: #3c414c;
-  position: absolute;
-  top: -40px;
-  left: 2%;
-}
-.testimonial{
-  padding: 0 15px;
-}
-.testimonial .description{
-  font-size: 20px;
-  font-weight:400;
-  font-style: italic;
-  color: #848484;
-  line-height: 30px;
-  padding-bottom: 25px;
-  margin-bottom: 15px;
-  position: relative;
-}
-.testimonial .description:before{
-  content: "";
-  width: 75%;
-  border-top: 1px solid #ee0979;
-  margin: 0 auto;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-}
-.testimonial .description:after{
-  content: "";
-  width: 20px;
-  height: 20px;
-  background: #fff;
-  position: absolute;
-  bottom: -10px;
-  left: 50%;
-  border-bottom: 1px solid #ee0979;
-  border-right: 1px solid #ee0979;
-  transform: translateX(-50%) rotate(45deg);
-}
-.testimonial .pic{
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  margin: 25px auto;
-  overflow: hidden;
-}
-.testimonial .pic img{
-  width: 100%;
-  height: auto;
-}
-.testimonial .title{
-font-family: 'Raleway', sans-serif;
-  display: inline-block;
-  font-size: 24px;
-  font-weight: 700;
-  color: #ee0979;
-  text-transform: capitalize;
-  margin: 0;
-}
-.testimonial .post{
-font-family: 'Raleway', sans-serif;
-  display: inline-block;
-  font-size: 20px;
-  color: #848484;
-}
-.owl-theme .owl-controls{
-  margin-top: 10px;
-  position: absolute;
-  bottom: -20%;
-  right: 0;
-}
-.owl-theme .owl-controls .owl-buttons div{
-  width: 50px;
-  height: 50px;
-  line-height: 50px;
-  background: #34363b;
-  opacity: 1;
-  padding: 0;
- display:inline-block;
- margin:0 10px; 
-}
-.owl-prev:before,
-.owl-next:before{
-  content: "\f104";
-  font-family: "fontawesome";
-  font-size: 23px;
-  font-weight: 700;
-  color: #fff;
-}
-.owl-next:before{
-  content:"\f105";
-}
-@media only screen and (max-width:767px){
-  .testimonial-bg{ padding: 50px 40px; }
-  .owl-theme .owl-controls{ bottom: -22%; }
-}
-@media only screen and (max-width:480px){
-  .testimonial-bg:before{
-      width: 55px;
-      height: 55px;
-      line-height: 55px;
-      font-size: 40px;
+  
+  <style>
+  
+  .gtco-testimonials {
+    position: relative;
+    margin-top: 30px;
+  
+    h2 {
+      font-size: 30px;
+      text-align: center;
+      color: #333333;
+      margin-bottom: 50px;
+    }
+  
+    .owl-stage-outer {
+      padding: 30px 0;
+    }
+  
+    .owl-nav {
+      display: none;
+    }
+  
+    .owl-dots {
+      text-align: center;
+  
+      span {
+        position: relative;
+        height: 10px;
+        width: 10px;
+        border-radius: 50%;
+        display: block;
+        background: #fff;
+        border: 2px solid #01b0f8;
+        margin: 0 5px;
+      }
+  
+      .active {
+        box-shadow: none;
+  
+        span {
+          background: #01b0f8;
+          box-shadow: none;
+          height: 12px;
+          width: 12px;
+          margin-bottom: -1px;
+        }
+      }
+    }
+  
+    .card {
+      background: #fff;
+      box-shadow: 0 8px 30px -7px #c9dff0;
+      margin: 0 20px;
+      padding: 0 10px;
+      border-radius: 20px;
+      border: 0;
+  
+      .card-img-top {
+        max-width: 100px;
+        border-radius: 50%;
+        margin: 15px auto 0;
+        box-shadow: 0 8px 20px -4px #95abbb;
+        width: 100px;
+        height: 100px;
+      }
+  
+      h5 {
+        color: #01b0f8;
+        font-size: 21px;
+        line-height: 1.3;
+  
+        span {
+          font-size: 18px;
+          color: #666666;
+        }
+      }
+  
+      p {
+        font-size: 18px;
+        color: #555;
+        padding-bottom: 15px;
+      }
+    }
+  
+    .active {
+      opacity: 0.5;
+      transition: all 0.3s;
+    }
+  
+    .center {
+      opacity: 1;
+  
+      h5 {
+        font-size: 24px;
+  
+        span {
+          font-size: 20px;
+        }
+      }
+  
+      .card-img-top {
+        max-width: 100%;
+        height: 120px;
+        width: 120px;
+      }
+    }
   }
-  .testimonial-bg{ padding: 30px 10px; }
-  .owl-theme .owl-controls{ bottom: -15%; }
-}
-@media only screen and (max-width:360px){
-  .testimonial .title,
-  .testimonial .post{
-      font-size: 16px;
+  
+  @media (max-width: 767px) {
+    .gtco-testimonials {
+      margin-top: 20px;
+    }
   }
-  .owl-theme .owl-controls{ bottom: -12%; }
-}
-</style>
+  
+  .owl-carousel {
+    .owl-nav button {
+      &.owl-next, &.owl-prev {
+        outline: 0;
+      }
+    }
+  
+    button.owl-dot {
+      outline: 0;
+    }
+  }
+  
+  body {
+    background: aliceblue;
+  }
+  
+  .gtco-testimonials {
+    position: relative;
+    margin-top: 30px;
+  
+    h2 {
+      font-size: 30px;
+      text-align: center;
+      color: #333333;
+      margin-bottom: 50px;
+    }
+  
+    .owl-stage-outer {
+      padding: 30px 0;
+    }
+  
+    .owl-nav {
+      display: none;
+    }
+  
+    .owl-dots {
+      text-align: center;
+  
+      span {
+        position: relative;
+        height: 10px;
+        width: 10px;
+        border-radius: 50%;
+        display: block;
+        background: #fff;
+        border: 2px solid #01b0f8;
+        margin: 0 5px;
+      }
+  
+      .active {
+        box-shadow: none;
+  
+        span {
+          background: #01b0f8;
+          box-shadow: none;
+          height: 12px;
+          width: 12px;
+          margin-bottom: -1px;
+        }
+      }
+    }
+  
+    .card {
+      background: #fff;
+      box-shadow: 0 8px 30px -7px #c9dff0;
+      margin: 0 20px;
+      padding: 0 10px;
+      border-radius: 20px;
+      border: 0;
+  
+      .card-img-top {
+        max-width: 100px;
+        border-radius: 50%;
+        margin: 15px auto 0;
+        box-shadow: 0 8px 20px -4px #95abbb;
+        width: 100px;
+        height: 100px;
+      }
+  
+      h5 {
+        color: #01b0f8;
+        font-size: 21px;
+        line-height: 1.3;
+  
+        span {
+          font-size: 18px;
+          color: #666666;
+        }
+      }
+  
+      p {
+        font-size: 18px;
+        color: #555;
+        padding-bottom: 15px;
+      }
+    }
+  
+    .active {
+      opacity: 0.5;
+      transition: all 0.3s;
+    }
+  
+    .center {
+      opacity: 1;
+  
+      h5 {
+        font-size: 24px;
+  
+        span {
+          font-size: 20px;
+        }
+      }
+  
+      .card-img-top {
+        max-width: 100%;
+        height: 120px;
+        width: 120px;
+      }
+    }
+  }
+  
+  @media (max-width: 767px) {
+    .gtco-testimonials {
+      margin-top: 20px;
+    }
+  }
+  
+  .owl-carousel {
+    .owl-nav button {
+      &.owl-next, &.owl-prev {
+        outline: 0;
+      }
+    }
+  
+    button.owl-dot {
+      outline: 0;
+    }
+  }
+  
+  <script>
+  (function () {
+    "use strict";
+  
+    var carousels = function () {
+      $(".owl-carousel1").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          680: {
+            items: 2,
+            nav: false,
+            loop: false
+          },
+          1000: {
+            items: 3,
+            nav: true
+          }
+        }
+      });
+    };
+  
+    (function ($) {
+      carousels();
+    })(jQuery);
+  })();
+  
+  </script>
 
-<script>
-    $(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
-        items:1,
-        itemsDesktop:[1000,1],
-        itemsDesktopSmall:[979,1],
-        itemsTablet:[768,1],
-        pagination:false,
-        navigation:true,
-        navigationText:["",""],
-        autoPlay:true
-    });
-});
-</script>
 
+
+  
 @endsection
