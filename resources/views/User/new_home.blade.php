9ce3dc4b839b34ba3d3782773930e900cb19c122
neeraj <tbs.neeraj@gmail.com>
2024-07-04 11:50:50 +0530

    resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -19,12 +19,7 @@
                 <div class="container">
                     <div class="xl:col-span-4 lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
                         <div class="box custom-box">
-                            <div class="box-header">
-                                <div class="box-title">
-                                    Mouse Wheel Control
-                                </div>
-                            </div>
-                            <div class="box-body">
+
                                 <div class="swiper vertical vertical-mouse-control !h-[352px]">
                                     <div class="swiper-wrapper">
                                         <div class="swiper-slide"><img src="assets/images/media/media-28.jpg"
@@ -36,7 +31,7 @@
                                     </div>
                                     <div class="swiper-pagination"></div>
                                 </div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
