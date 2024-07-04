bf48586b9c0e46f984447118c10d397a724612f6
neeraj <tbs.neeraj@gmail.com>
2024-07-04 11:49:24 +0530

    resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('User.Layouts.layout')
 
 @section('title', 'Home')
@@ -12,37 +10,44 @@
 @section('content')
 
 
-<div class="content">
-    <div class="main-content">
+    <div class="content">
+        <div class="main-content">
 
-        <div class="xl:col-span-4 lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
-            <div class="box custom-box">
-                <div class="box-header">
-                    <div class="box-title">
-                        Mouse Wheel Control
-                    </div>
-                </div>
-                <div class="box-body">
-                    <div class="swiper vertical vertical-mouse-control !h-[352px]">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide"><img src="assets/images/media/media-28.jpg" class="!rounded-md" alt=""></div>
-                            <div class="swiper-slide"><img src="assets/images/media/media-30.jpg" class="!rounded-md" alt=""></div>
-                            <div class="swiper-slide"><img src="assets/images/media/media-32.jpg" class="!rounded-md" alt=""></div>
+
+
+            <section class="home-new-banner">
+                <div class="container">
+                    <div class="xl:col-span-4 lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
+                        <div class="box custom-box">
+                            <div class="box-header">
+                                <div class="box-title">
+                                    Mouse Wheel Control
+                                </div>
+                            </div>
+                            <div class="box-body">
+                                <div class="swiper vertical vertical-mouse-control !h-[352px]">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide"><img src="assets/images/media/media-28.jpg"
+                                                class="!rounded-md" alt=""></div>
+                                        <div class="swiper-slide"><img src="assets/images/media/media-30.jpg"
+                                                class="!rounded-md" alt=""></div>
+                                        <div class="swiper-slide"><img src="assets/images/media/media-32.jpg"
+                                                class="!rounded-md" alt=""></div>
+                                    </div>
+                                    <div class="swiper-pagination"></div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="swiper-pagination"></div>
                     </div>
                 </div>
-            </div>
+            </section>
+
+
+
         </div>
-
-
-
-
-        
     </div>
-</div>
 
-         
+
 
 
 
