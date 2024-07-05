59f1a5c099abdf189fe633d2978a1afe12fbdd62
neeraj <tbs.neeraj@gmail.com>
2024-07-05 09:34:50 +0530

    assets\css\style.css resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -13,7 +13,7 @@
 
 
     <div class="content">
-        {{-- <div class="main-content"> --}}
+        <div class="main-content">
 
 
 
@@ -25,34 +25,19 @@
                         <div class="swiper-slide">
                             <div class="slide-inner slide-bg-image"
                                 data-background="{{ asset('assets/home-two-img/TTB-Banner-Without-Content.jpg') }}">
-                                <!-- <div class="gradient-overlay"></div> -->
                                 <div class="wraper">
                                     <div class="relative h-[900px] lg:h-[680px]  md:h-[600px] sm:h-[500px]">
                                         <div
                                             class="max-w-[550px] lg:w-[600px] md:w-full  absolute top-1/2 left-0 transform  -translate-y-1/2">
 
                                             <div data-swiper-parallax="200" class="wpo-hero-title-top">
-                                                <span
-                                                    class="text-[22px] col:text-[18px] text-[#e4e4e4] font-heading-font">We
-                                                    bring the
-                                                    right people
-                                                    together.</span>
+                                                <span></span>
                                             </div>
                                             <div data-swiper-parallax="300" class="slide-title">
-                                                <h2
-                                                    class="text-[70px] font-black leading-[90px] mt-[10px] mb-[25px] lg:text-[50px] md:text-[40px] md:leading-[55px] col:text-[30px] col:leading-[35px] text-white">
-                                                    Helping You Solve Your Problems</h2>
+                                                <h2></h2>
                                             </div>
                                             <div data-swiper-parallax="400" class="slide-text">
-                                                <p
-                                                    class="text-[25px] text-white font-heading-font 
-                                                                           leading-[35px]
-                                                   max-w-[500px] pl-[20px] border-l-[2px]
-                                                   border-l-white mb-[60px] 
-                                                   md:text-[18px] col:leading-[22px] 
-                                                   col:mb-[30px]">
-                                                    Facilitating client learning that is,teaching clients how to resolve
-                                                    similar problems in the future.</p>
+                                                <p></p>
                                             </div>
 
                                             <div data-swiper-parallax="500" class="slide-btn">
@@ -121,7 +106,7 @@
 
 
 
-        {{-- </div> --}}
+        </div>
     </div>
 
 
