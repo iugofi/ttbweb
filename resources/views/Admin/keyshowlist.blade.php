4f66aea7ee0970f9dde3f2b1f1c49a1ab3aec440
kunal <kunal.iugofi@gmail.com>
2024-07-08 10:44:35 +0530

    resources\views\Admin\keyshowlist.blade.php

--- resources/views/Admin/keyshowlist.blade.php
+++ resources/views/Admin/keyshowlist.blade.php
@@ -110,8 +110,8 @@
                                                         $id = 1;
                                                     @endphp
                                                     <td class="border px-4 py-2">{{ $key + 1 }}</td>
-                                                    <td class="border px-4 py-2">{{ $item->main_key }}</td>
-                                                    <td class="border px-4 py-2" style="font-weight: bold;">
+                                                    <td class="border px-4 py-2" style="font-weight: bold;">{{ $item->main_key }}</td>
+                                                    <td class="border px-4 py-2" >
                                                         @php
                                                             $storeId = 'key_type';
                                                             $pickId = $item->key_type;
