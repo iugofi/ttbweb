c6112fc5dc256bc537fa2af63a8e9fa9b84c8e06
kulvinder <iugofi.kulvinder@gmail.com>
2024-07-08 17:39:17 +0530

    resources\views\User\welcome.blade.php

--- resources/views/User/welcome.blade.php
+++ resources/views/User/welcome.blade.php
@@ -945,6 +945,9 @@
         <div class="owl-nav">
             <button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">&#10094;</span></button>
             <button type="button" role="presentation" class="owl-next"><span aria-label="Next">&#10095;</span></button>
+        
+            <button class="button left" onclick="scrollCarousel(-1)">‹</button>
+            <button class="button right" onclick="scrollCarousel(1)">›</button>
         </div>
 	</div>
 </section>
