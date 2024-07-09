cd699f4a6a66834b6f2fb167282799d4216e0769
neeraj <tbs.neeraj@gmail.com>
2024-07-09 09:15:33 +0530

    assets\css\style.css resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -363,7 +363,7 @@
             <section class="section-home-new-4">
                 <div class="container">
                     <div class="grid grid-cols-12 gap-6">
-                        <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12">
+                        <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
                             <h2>Services</h2>
                         </div>
                     </div>
