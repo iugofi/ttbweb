c125dba873d129002518283608dffa2b84525afb
kunal <kunal.iugofi@gmail.com>
2024-07-11 13:24:27 +0530

    resources\views\User\myprofile.blade.php

--- resources/views/User/myprofile.blade.php
+++ resources/views/User/myprofile.blade.php
@@ -856,65 +856,34 @@
             </div>
             <!--End::row-1 -->
 
-            <div class="main-content !p-0 landing-main dark:text-defaulttextcolor/70">
-                <section class="vpn-banner-top">
-                    <div class="container main-banner-container">
-                        <div class="grid grid-cols-12 gap-6">
-                            <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                                <div class="lg:py-[3rem]">
-                                    <h2>Explore The World Wide Easy With Netric VPN</h2>
-                                    <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                                        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-                                        ridiculus
-                                        mus.</p>
-                                    <a href="index.html" class="m-1 ti-btn ti-btn-primary-full">
-                                        Download Now
-                                        <i class="bx bxs-cloud-download icon-hm"></i>
-                                    </a>
-                                    <a href="#" class="popup-youtube  btn-white btn-circle btn-icon"><i class="bx bx-play watch-icon"></i>
-                                        <span class="pl-2 text-white wtc-spn">Watch Now</span></a>
-                                </div>
-                            </div>
-                            <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-
-                                <div class="text-end landing-main-image landing-heading-img flex justify-end w-full">
-                                    <img src="https://nighthawk-routerlogin.com/assets/img/ttb vpn shield.png" alt="" class="img-fluid" width="100%">
-                                </div>
-
-                            </div>
+            <section class="section-two-iconbox pt-8">
+                <div class="container main-banner-container">
+                    <h3 class="text-center">About VPN</h3>
+                    <h4 class="text-center">Why Should Use Netric VPN?</h4>
+                    <div class="grid grid-cols-12 gap-6">
+                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
+                            <i class="fa-solid fa-eye"></i>
+                            <h5>Anonymous Access</h5>
+                            <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
+                            <i class="fa-solid fa-user"></i>
+                            <h5>No Usage Logs</h5>
+                            <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
+                            <i class="fa-solid fa-server"></i>
+                            <h5>No Usage Limits</h5>
+                            <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                        <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
+                            <i class="fa-solid fa-lock"></i>
+                            <h5>100% White Listed</h5>
+                            <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
                         </div>
                     </div>
-                </section>
-
-
-                <section class="section-two-iconbox pt-8">
-                    <div class="container main-banner-container">
-                        <h3 class="text-center">About VPN</h3>
-                        <h4 class="text-center">Why Should Use Netric VPN?</h4>
-                        <div class="grid grid-cols-12 gap-6">
-                            <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
-                                <i class="fa-solid fa-eye"></i>
-                                <h5>Anonymous Access</h5>
-                                <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
-                            </div>
-                            <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
-                                <i class="fa-solid fa-user"></i>
-                                <h5>No Usage Logs</h5>
-                                <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
-                            </div>
-                            <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
-                                <i class="fa-solid fa-server"></i>
-                                <h5>No Usage Limits</h5>
-                                <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
-                            </div>
-                            <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
-                                <i class="fa-solid fa-lock"></i>
-                                <h5>100% White Listed</h5>
-                                <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor.</p>
-                            </div>
-                        </div>
-                    </div>
-            </section></div>
+                </div>
+        </section>
 
 
         </div>
