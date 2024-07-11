e2a6cdcd78299d0dfdbdeab7a6c0efb142ef81ea
kunal <kunal.iugofi@gmail.com>
2024-07-11 14:20:37 +0530

    resources\views\User\myprofile.blade.php

--- resources/views/User/myprofile.blade.php
+++ resources/views/User/myprofile.blade.php
@@ -858,8 +858,8 @@
 
             <section class="section-two-iconbox pt-8 bg-white">
                 <div class="container main-banner-container">
-                    <h3 class="text-center mb-2">TTBTM Antivirus</h3>
-                    <h3 class="text-center mb-2">0 of 0 License(s) USED:<a href="#"> Download </a></h3>
+                    <h3 class="text-center mb-2" style="color:#000;">TTBTM Antivirus</h3>
+                    <h3 class="text-center mb-2"  style="color:#000;">0 of 0 License(s) USED:<a href="#"> Download </a></h3>
 
                     <div class="grid grid-cols-12 gap-6">
                         <div class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 col-span-12 con-box">
