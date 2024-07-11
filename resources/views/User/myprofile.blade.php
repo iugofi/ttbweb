e98271623ad93ea5732abb253772f79be31722e0
kunal <kunal.iugofi@gmail.com>
2024-07-11 12:27:29 +0530

    resources\views\User\myprofile.blade.php assets\userprofile\default-profile.png

--- resources/views/User/myprofile.blade.php
+++ resources/views/User/myprofile.blade.php
@@ -334,8 +334,9 @@
                                                                                 <label for="exampleInputPassword1"
                                                                                     class="form-label">Profile</label>
                                                                                     <span class="avatar avatar-xl me-1">
-                                                                                        <img src="{{asset('assets/userprofile/'.$user_data->profile ? $user_data->profile : '' )}}" alt="">
+                                                                                        <img src="{{ asset('assets/userprofile/' . ($user_data->profile ? $user_data->profile : 'default-profile.png')) }}" alt="">
                                                                                     </span>
+
                                                                                     <input type="file" class="form-control" id="profile_images" name="profile_images"
                                                                                     multiple data-allow-reorder="true" data-max-file-size="3MB"
                                                                                     data-max-files="6">
