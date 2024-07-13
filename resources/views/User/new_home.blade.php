7319716a871d7bfdc5370f9e2932832cecfa13be
neeraj <tbs.neeraj@gmail.com>
2024-07-13 14:52:58 +0530

    resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -484,10 +484,10 @@
                                                 <div
                                                     class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 md:col-span-3 sm:col-span-12 col-span-12 md:border-e md:border-b-0 border-b border-dashed dark:border-defaultborder/10 px-0 box-price">
                                                     <div class="p-6 pricing-offer overflow-hidden">
-                                                        <span class="pricing-offer-details shadow">
-                                                            {{-- <span class="font-semibold">10%</span> <span
-                                                                class="text-[0.625rem] op-8 ms-1">Off</span> --}}
-                                                        </span>
+                                                        {{-- <span class="pricing-offer-details shadow">
+                                                            <span class="font-semibold">10%</span> <span
+                                                                class="text-[0.625rem] op-8 ms-1">Off</span>
+                                                        </span> --}}
                                                         <h6 class="font-semibold text-center text-[1rem]">Deluxe</h6>
                                                         <div class="py-4 flex items-center justify-center">
                                                             <div class="pricing-svg1">
@@ -730,10 +730,10 @@
                                                 <div
                                                     class="xxl:col-span-3 xl:col-span-3 lg:col-span-3 md:col-span-3 sm:col-span-12 col-span-12 md:border-e md:border-b-0 border-b border-dashed dark:border-defaultborder/10 px-0 box-price">
                                                     <div class="p-6 pricing-offer overflow-hidden">
-                                                        <span class="pricing-offer-details shadow">
-                                                            {{-- <span class="font-semibold">10%</span> <span
-                                                                class="text-[0.625rem] op-8 ms-1">Off</span> --}}
-                                                        </span>
+                                                        {{-- <span class="pricing-offer-details shadow">
+                                                            <span class="font-semibold">10%</span> <span
+                                                                class="text-[0.625rem] op-8 ms-1">Off</span>
+                                                        </span> --}}
                                                         <h6 class="font-semibold text-center text-[1rem]">Deluxe</h6>
                                                         <div class="py-4 flex items-center justify-center">
                                                             <div class="pricing-svg1">
