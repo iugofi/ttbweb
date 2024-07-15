8a7165b357f4b1f40504bf90385a6be38933d557
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-07-15 11:43:29 +0530

    resources\views\User\Layouts\footer.blade.php

--- resources/views/User/Layouts/footer.blade.php
+++ resources/views/User/Layouts/footer.blade.php
@@ -3,15 +3,11 @@
         <div class="grid grid-cols-12 gap-6">
             <div class="xl:col-span-4 col-span-12">
                 <div class="px-6">
-                    <p class="font-semibold mb-4"><a aria-label="anchor" href="{{ route('user.index') }}"><img
-                                src="{{ asset('assets/images/brand-logos/favicon.png') }}" alt=""></a></p>
+                    <p class="font-semibold mb-4"><a aria-label="anchor" href="{{ route('user.index') }}"><img src="{{asset('assets/images/brand-logos/favicon.png')}}" alt=""></a></p>
                     <p class="mb-2  font-normal">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit et magnam, fuga est
-                        mollitia eius, quo illum illo inventore optio aut quas omnis rem. Dolores accusantium aspernatur
-                        minus ea incidunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit et magnam, fuga est mollitia eius, quo illum illo inventore optio aut quas omnis rem. Dolores accusantium aspernatur minus ea incidunt.
                     </p>
-                    <p class="mb-0  font-normal">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem ea esse
-                        ad</p>
+                    <p class="mb-0  font-normal">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem ea esse ad</p>
                 </div>
             </div>
             <div class="xl:col-span-2 col-span-12">
@@ -31,12 +27,10 @@
                             <a href="javascript:void(0);" class="text-white">Projects</a>
                         </li>
                         <li class="slide">
-                            <a href="{{ route('user.virus_removal_service') }}" class="side-menu__item">virus removal
-                                service</a>
+                            <a href="{{route('user.virus_removal_service')}}" class="side-menu__item">virus removal service</a>
                         </li>
                         <li class="slide">
-                            <a href="{{ route('user.E_Support_Concierge') }}" class="side-menu__item">E Support
-                                Concierge</a>
+                            <a href="{{route('user.E_Support_Concierge')}}" class="side-menu__item">E Support Concierge</a>
                         </li>
                     </ul>
                 </div>
@@ -71,43 +65,34 @@
                     <h6 class="font-semibold text-[1rem] mb-2">CONTACT</h6>
                     <ul class="list-unstyled font-normal landing-footer-list">
                         <li>
-                            <a href="javascript:void(0);" class="text-white "><i
-                                    class="ri-home-4-line me-1 align-middle"></i> New York, NY 10012, US</a>
+                            <a href="javascript:void(0);" class="text-white "><i class="ri-home-4-line me-1 align-middle"></i> New York, NY 10012, US</a>
                         </li>
                         <li>
-                            <a href="javascript:void(0);" class="text-white "><i
-                                    class="ri-mail-line me-1 align-middle"></i> info@fmail.com</a>
+                            <a href="javascript:void(0);" class="text-white "><i class="ri-mail-line me-1 align-middle"></i> info@fmail.com</a>
                         </li>
                         <li>
-                            <a href="javascript:void(0);" class="text-white "><i
-                                    class="ri-phone-line me-1 align-middle"></i> +(555)-1920 1831</a>
+                            <a href="javascript:void(0);" class="text-white "><i class="ri-phone-line me-1 align-middle"></i> +(555)-1920 1831</a>
                         </li>
                         <li>
-                            <a href="javascript:void(0);" class="text-white "><i
-                                    class="ri-printer-line me-1 align-middle"></i> +(123) 1293 123</a>
+                            <a href="javascript:void(0);" class="text-white "><i class="ri-printer-line me-1 align-middle"></i> +(123) 1293 123</a>
                         </li>
                         <li class="mt-4 !mb-0">
                             <p class="mb-2 font-semibold  text-[1rem]">FOLLOW US ON :</p>
                             <div class="mb-0">
                                 <div class="btn-list">
-                                    <button aria-label="button" type="button"
-                                        class="ti-btn ti-btn-sm !mb-0 ti-btn-primary me-[0.365rem]">
+                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-primary me-[0.365rem]">
                                         <i class="ri-facebook-line font-bold"></i>
                                     </button>
-                                    <button aria-label="button" type="button"
-                                        class="ti-btn ti-btn-sm !mb-0 ti-btn-secondary me-[0.365rem]">
+                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-secondary me-[0.365rem]">
                                         <i class="ri-twitter-line font-bold"></i>
                                     </button>
-                                    <button aria-label="button" type="button"
-                                        class="ti-btn ti-btn-sm !mb-0 ti-btn-warning me-[0.365rem]">
+                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-warning me-[0.365rem]">
                                         <i class="ri-instagram-line font-bold"></i>
                                     </button>
-                                    <button aria-label="button" type="button"
-                                        class="ti-btn ti-btn-sm !mb-0 ti-btn-success me-[0.365rem]">
+                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-success me-[0.365rem]">
                                         <i class="ri-github-line font-bold"></i>
                                     </button>
-                                    <button aria-label="button" type="button"
-                                        class="ti-btn ti-btn-sm !mb-0 ti-btn-danger">
+                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-danger">
                                         <i class="ri-youtube-line font-bold"></i>
                                     </button>
                                 </div>
@@ -121,11 +106,10 @@
 </section>
 <div class="text-center landing-main-footer py-4">
     <span class="text-[#8c9097] text-white text-[0.9375rem]"> Copyright © <span id="year"></span> <a
-            href="https://www.ttbinternetsecurity.com/"
-            class="!text-primary font-semibold"><u>ttbinternetsecurity</u></a>.
+        href="https://www.ttbinternetsecurity.com/" class="!text-primary font-semibold"><u>ttbinternetsecurity</u></a>.
     </span>
 </div>
-<!-- FOOTER -->
+        <!-- FOOTER -->
 </div>
 <!-- END MAIN-CONTENT -->
 
@@ -137,34 +121,34 @@
 
 <!-- SCROLL-TO-TOP -->
 <div class="scrollToTop">
-    <span class="arrow"><i class="ri-arrow-up-s-fill  text-[1.25rem]"></i></span>
+<span class="arrow"><i class="ri-arrow-up-s-fill  text-[1.25rem]"></i></span>
 </div>
 <div id="responsive-overlay"></div>
 
 <!-- POPPER JS -->
-<script src="{{ asset('assets/libs/%40popperjs/core/umd/popper.min.js') }}"></script>
+<script src="{{asset('assets/libs/%40popperjs/core/umd/popper.min.js')}}"></script>
 
 <!-- COLOR PICKER JS -->
-<script src="{{ asset('assets/libs/%40simonwep/pickr/pickr.es5.min.js') }}"></script>
+<script src="{{asset('assets/libs/%40simonwep/pickr/pickr.es5.min.js')}}"></script>
 
 <!-- SWIPER JS -->
-<script src="{{ asset('assets/libs/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{asset('assets/libs/swiper/swiper-bundle.min.js')}}"></script>
 
 <!-- DEFAULTMENU JS -->
-<script src="{{ asset('assets/js/defaultmenu.js') }}"></script>
+<script src="{{asset('assets/js/defaultmenu.js')}}"></script>
 
 <!-- SIMPLEBAR JS -->
-<script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
 
 <!-- INTERNAL LANDING JS -->
-<script src="{{ asset('assets/js/landing.js') }}"></script>
+<script src="{{asset('assets/js/landing.js')}}"></script>
 
 <!-- PRELINE JS -->
-<script src="{{ asset('assets/libs/preline/preline.js') }}"></script>
+<script src="{{asset('assets/libs/preline/preline.js')}}"></script>
 
-<script src="{{ asset('assets/js/custom-switcher.js') }}"></script>
-<script src="{{ asset('assets/js/swiper.js') }}"></script>
-<script src="{{ asset('assets/js/switch.js') }}"></script>
+<script src="{{asset('assets/js/custom-switcher.js')}}"></script>
+<script src="{{asset('assets/js/swiper.js')}}"></script>
+<script src="{{asset('assets/js/switch.js')}}"></script>
 
 
 
