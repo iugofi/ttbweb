75eb2c5635f5d60a961847e396fbd237d43ebb5e
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-07-17 15:10:12 +0530

    resources\views\User\welcome.blade.php

--- resources/views/User/welcome.blade.php
+++ resources/views/User/welcome.blade.php
@@ -1105,7 +1105,6 @@
         </script>
 
 
-        --------------------------
 
 
 
@@ -1115,3 +1114,45 @@
 
 
     @endsection
+
+
+-----------------
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dropdown Menu</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="dropdown">
+        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            All Services
+        </a>
+
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/netgear/">Netgear</a></li>
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/tp-link/">TP-Link</a></li>
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/belkin/">Belkin</a></li>
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/linksys/">Linksys</a></li>
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/d-link/">D-Link</a></li>
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/asus/">Asus</a></li>
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/spectrum/">Spectrum</a></li>
+            <li><a class="dropdown-item" href="https://www.routerloginsupport.com/category/calix/">Calix</a></li>
+        </ul>
+    </div>
+    <style>
+        .dropdown-btn a#dropdownMenuLink {
+    padding: 6px 27px;
+    font-size: 18px;
+    border-radius: 37px;
+    background-color: #ed6205;
+    border: none;
+}
+    </style>
+</body>
+</html>
+
