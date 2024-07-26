54d0cfef56cf07520aa7fd68038382d076c72cd6
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-07-26 14:24:32 +0530

    resources\views\User\Layouts\footer.blade.php

--- resources/views/User/Layouts/footer.blade.php
+++ resources/views/User/Layouts/footer.blade.php
@@ -1,117 +1,4 @@
-{{-- <section class="section landing-footer  text-white text-[0.813rem]">
-    <div class="container">
-        <div class="grid grid-cols-12 gap-6">
-            <div class="xl:col-span-4 col-span-12">
-                <div class="px-6">
-                    <p class="font-semibold mb-4"><a aria-label="anchor" href="{{ route('user.index') }}"><img src="{{asset('assets/images/brand-logos/favicon.png')}}" alt=""></a></p>
-                    <p class="mb-2  font-normal">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit et magnam, fuga est mollitia eius, quo illum illo inventore optio aut quas omnis rem. Dolores accusantium aspernatur minus ea incidunt.
-                    </p>
-                    <p class="mb-0  font-normal">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem ea esse ad</p>
-                </div>
-            </div>
-            <div class="xl:col-span-2 col-span-12">
-                <div class="px-6">
-                    <h6 class="font-semibold text-[1rem] mb-4">PAGES</h6>
-                    <ul class="list-unstyled  font-normal landing-footer-list">
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Email</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Profile</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Timeline</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Projects</a>
-                        </li>
-                        <li class="slide">
-                            <a href="{{route('user.virus_removal_service')}}" class="side-menu__item">virus removal service</a>
-                        </li>
-                        <li class="slide">
-                            <a href="{{route('user.E_Support_Concierge')}}" class="side-menu__item">E Support Concierge</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="xl:col-span-2 col-span-12">
-                <div class="px-6">
-                    <h6 class="font-semibold text-[1rem] mb-2">INFO</h6>
-                    <ul class="list-unstyled font-normal landing-footer-list">
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Our Team</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Contact US</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">About</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Services</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Blog</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Terms &amp; Conditions</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="xl:col-span-4 col-span-12">
-                <div class="px-6">
-                    <h6 class="font-semibold text-[1rem] mb-2">CONTACT</h6>
-                    <ul class="list-unstyled font-normal landing-footer-list">
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-home-4-line me-1 align-middle"></i> New York, NY 10012, US</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-mail-line me-1 align-middle"></i> info@fmail.com</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-phone-line me-1 align-middle"></i> +(555)-1920 1831</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-printer-line me-1 align-middle"></i> +(123) 1293 123</a>
-                        </li>
-                        <li class="mt-4 !mb-0">
-                            <p class="mb-2 font-semibold  text-[1rem]">FOLLOW US ON :</p>
-                            <div class="mb-0">
-                                <div class="btn-list">
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-primary me-[0.365rem]">
-                                        <i class="ri-facebook-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-secondary me-[0.365rem]">
-                                        <i class="ri-twitter-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-warning me-[0.365rem]">
-                                        <i class="ri-instagram-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-success me-[0.365rem]">
-                                        <i class="ri-github-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-danger">
-                                        <i class="ri-youtube-line font-bold"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<div class="text-center landing-main-footer py-4">
-    <span class="text-[#8c9097] text-white text-[0.9375rem]"> Copyright © <span id="year"></span> <a
-        href="https://www.ttbinternetsecurity.com/" class="!text-primary font-semibold"><u>ttbinternetsecurity</u></a>.
-    </span>
-</div> --}}
- {{-- Start New footer html --}}
-
- <section class="section landing-footer  text-white text-[0.813rem]">
+<section class="section landing-footer  text-white text-[0.813rem]">
     <div class="container">
         <div class="grid grid-cols-12 gap-6">
 
@@ -343,11 +230,14 @@
 
         </div>
 
-    </div>
+<div class="text-center landing-main-footer py-4">
+    <span class="text-[#8c9097] text-white text-[0.9375rem]"> Copyright © <span id="year"></span> <a
+        href="https://www.ttbinternetsecurity.com/" class="!text-primary font-semibold"><u>ttbinternetsecurity</u></a>.
+    </span>
+</div>
+
+</div>
 </section>
-
-<!-- FOOTER -->
-
 </div>
 <!-- END MAIN-CONTENT -->
 
