ff276b9ef7e97d9178127190c23641b604f049dd
“kunalverma320” <kunalverma320@gmail.com>
2024-07-26 15:12:50 +0530

    resources\views\User\Layouts\footer.blade.php vendor\laravel\sail\stubs\mariadb.stub

--- resources/views/User/Layouts/footer.blade.php
+++ resources/views/User/Layouts/footer.blade.php
@@ -1,114 +1,237 @@
 <section class="section landing-footer  text-white text-[0.813rem]">
     <div class="container">
         <div class="grid grid-cols-12 gap-6">
-            <div class="xl:col-span-4 col-span-12">
-                <div class="px-6">
-                    <p class="font-semibold mb-4"><a aria-label="anchor" href="{{ route('user.index') }}"><img src="{{asset('assets/images/brand-logos/favicon.png')}}" alt=""></a></p>
-                    <p class="mb-2  font-normal">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit et magnam, fuga est mollitia eius, quo illum illo inventore optio aut quas omnis rem. Dolores accusantium aspernatur minus ea incidunt.
-                    </p>
-                    <p class="mb-0  font-normal">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem ea esse ad</p>
-                </div>
-            </div>
-            <div class="xl:col-span-2 col-span-12">
-                <div class="px-6">
-                    <h6 class="font-semibold text-[1rem] mb-4">PAGES</h6>
-                    <ul class="list-unstyled  font-normal landing-footer-list">
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Email</a>
+
+            <div class="xl:col-span-3 col-span-12">
+                <div class="footer-widget">
+                    <h2 class="text-white text-2xl font-semibold mb-2">Contact Us: </h2>
+                    <ul class="justify-center lg:justify-end space-x-2">
+                        <li class="footer-icon">
+                            <a href="https://www.facebook.com/ttbinternetsecurities/">
+                                <i class="fa-solid fa-phone"></i>
+                            </a><a href="tel:800-710-9311"> 800-710-9311</a>
                         </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Profile</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Timeline</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Projects</a>
-                        </li>
-                        <li class="slide">
-                            <a href="{{route('user.virus_removal_service')}}" class="side-menu__item">virus removal service</a>
-                        </li>
-                        <li class="slide">
-                            <a href="{{route('user.E_Support_Concierge')}}" class="side-menu__item">E Support Concierge</a>
+                        <li class="footer-icon">
+                            <a href="https://x.com/i/flow/login?redirect_after_login=%2FTTBisecure">
+                                <i class="fa-solid fa-envelope"></i>
+                            </a><a href="mailto:care@ttbinternetsecurity.com" class="text-sm">
+                                care@ttbinternetsecurity.com</a>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="xl:col-span-2 col-span-12">
-                <div class="px-6">
-                    <h6 class="font-semibold text-[1rem] mb-2">INFO</h6>
-                    <ul class="list-unstyled font-normal landing-footer-list">
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Our Team</a>
+
+            <div class="xl:col-span-3 col-span-12">
+                <div class="footer-widget">
+                    <h2 class="text-white text-2xl font-semibold mb-2">Find Us On:</h2>
+                    <div class="list-none text-white">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit et magnam, fuga
+                            est mollitia eius, quo illum illo inventore optio aut quas omnis rem.</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="xl:col-span-3 col-span-12">
+                <div class="footer-widget">
+                    <ul class="justify-center lg:justify-end space-x-2">
+                        <li class="icon-list">
+                            <img src="{{ asset('assets/img/USA-Flag.png') }}" alt="" class="img-fluid"
+                                style="width: 50px; height: 50px; border-radius: 30px;">
+                            <div class="box-location d-flex">
+                                <h2 class="text-white text-lg font-semibold mb-2">USA</h2>
+                                <p>Sugarpine Court Brampton, Ontario, CA</p>
+                            </div>
                         </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Contact US</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">About</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Services</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Blog</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white">Terms &amp; Conditions</a>
+                        <li class="icon-list">
+                            <img src="{{ asset('assets/img/Canada-Flag.png') }}" alt="" class="img-fluid"
+                                style="width: 50px; height: 50px; border-radius: 30px;">
+                            <div class="box-location d-flex">
+                                <h2 class="text-white text-lg font-semibold mb-2">CANADA</h2>
+                                <p>Bestech Business Tower, Sector 66, Mohali, IN</p>
+                            </div>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="xl:col-span-4 col-span-12">
-                <div class="px-6">
-                    <h6 class="font-semibold text-[1rem] mb-2">CONTACT</h6>
-                    <ul class="list-unstyled font-normal landing-footer-list">
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-home-4-line me-1 align-middle"></i> New York, NY 10012, US</a>
+            <div class="xl:col-span-3 col-span-12">
+                <div class="footer-widget">
+                    <ul class="justify-center lg:justify-end space-x-2">
+                        <li class="icon-list">
+                            <img src="{{ asset('assets/img/UK-Flag.png') }}" alt="" class="img-fluid"
+                                style="width: 50px; height: 50px; border-radius: 30px;">
+                            <div class="box-location d-flex">
+                                <h2 class="text-white text-lg font-semibold mb-2">UNITED KINGDOM</h2>
+                                <p>Sugarpine Court Brampton, Ontario, CA</p>
+                            </div>
                         </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-mail-line me-1 align-middle"></i> info@fmail.com</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-phone-line me-1 align-middle"></i> +(555)-1920 1831</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="text-white "><i class="ri-printer-line me-1 align-middle"></i> +(123) 1293 123</a>
-                        </li>
-                        <li class="mt-4 !mb-0">
-                            <p class="mb-2 font-semibold  text-[1rem]">FOLLOW US ON :</p>
-                            <div class="mb-0">
-                                <div class="btn-list">
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-primary me-[0.365rem]">
-                                        <i class="ri-facebook-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-secondary me-[0.365rem]">
-                                        <i class="ri-twitter-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-warning me-[0.365rem]">
-                                        <i class="ri-instagram-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-success me-[0.365rem]">
-                                        <i class="ri-github-line font-bold"></i>
-                                    </button>
-                                    <button aria-label="button" type="button" class="ti-btn ti-btn-sm !mb-0 ti-btn-danger">
-                                        <i class="ri-youtube-line font-bold"></i>
-                                    </button>
-                                </div>
+                        <li class="icon-list">
+                            <img src="{{ asset('assets/img/India-Flag.png') }}" alt="" class="img-fluid"
+                                style="width: 50px; height: 50px; border-radius: 30px;">
+                            <div class="box-location d-flex">
+                                <h2 class="text-white text-lg font-semibold mb-2">INDIA</h2>
+                                <p>Bestech Business Tower, Sector 66, Mohali, IN</p>
                             </div>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
+        <hr class="hr">
+
+        <div class="grid grid-cols-12 gap-6">
+
+            <div class="xl:col-span-3 col-span-12">
+                <div class="footer-widget">
+                    <h2 class="text-white text-2xl font-semibold mb-2">Follow Us</h2>
+                    <ul class="justify-center lg:justify-end space-x-2">
+                        <li class="footer-icon">
+                            <a href="https://www.facebook.com/ttbinternetsecurities/">
+                                <i class="ri-facebook-line font-bold"></i>
+                                Facebook</a>
+                        </li>
+                        <li class="footer-icon">
+                            <a href="https://x.com/i/flow/login?redirect_after_login=%2FTTBisecure">
+                                <i class="fa-brands fa-x-twitter"></i>
+                                Twitter</a>
+                        </li>
+                        <li class="footer-icon">
+                            <a href="https://www.instagram.com/ttbinternetsecurity/">
+                                <i class="ri-instagram-line font-bold"></i>
+                                Instagram</a>
+                        </li>
+                        <li class="footer-icon">
+                            <a href="https://www.youtube.com/channel/UCda5n02Q-bHeQlyWwm3ieWg">
+                                <i class="ri-youtube-line font-bold"></i>
+                                YouTube</a>
+                        </li>
+                        <li class="footer-icon">
+                            <a href="https://www.linkedin.com/company/ttbinternetsecurity">
+                                <i class="fa-brands fa-linkedin font-bold"></i>
+                                linkedin</a>
+                        </li>
+                        <li class="footer-icon">
+                            <a href="https://in.pinterest.com/ttbinternetsecurity/_created/">
+                                <i class="fa-brands fa-square-pinterest font-bold"></i>
+                                pinterest</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="xl:col-span-2 col-span-12">
+                <div class="footer-widget">
+                    <h2 class="text-white text-2xl font-semibold mb-2"> Quick Link</h2>
+                    <ul class="list-none text-white">
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Home</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">About</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Support</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Sign In</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Personal</a></li>
+                        <li><a href="#" class="hover:text-yellow-500">Commercial</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="xl:col-span-2 col-span-12">
+                <div class="footer-widget">
+                    <h2 class="text-white text-2xl font-semibold mb-2"> Products</h2>
+                    <ul class="list-none text-white">
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">TTB Antivirus</a>
+                        </li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">TTB Internet
+                                Security</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">TTB Total
+                                Security</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">TTB VPN Shield</a>
+                        </li>
+                    </ul>
+                    <h2 class="text-white text-2xl font-semibold mb-2">Resources</h2>
+                    <ul class="list-none text-white">
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">News</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Blogs</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="xl:col-span-2 col-span-12">
+                <div class="footer-widget">
+                    <h2 class="text-white text-2xl font-semibold mb-2"> Solutions</h2>
+                    <ul class="list-none text-white">
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">EndPoint
+                                Protection</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Network
+                                Protection</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Threat
+                                Protection</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Behavioral
+                                Protection</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Enhanced
+                                Firewall</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Malware
+                                Detection</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Real Time
+                                Anti-phishing</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">Virus Removal
+                                Service</a></li>
+                        <li class="mb-2"><a href="#" class="hover:text-yellow-500">E-Support
+                                Concierge</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="xl:col-span-3 col-span-12">
+                <div class="inner-column text-center lg:text-right">
+                    <h2 class="text-white text-2xl font-semibold mb-2">Sign up For Newsletter</h2>
+                    <div class="subscribe-form">
+                        <form method="post" action="https://www.nighthawk-routerlogin.com/saveemailsub"
+                            id="mail-subscribe">
+                            <input type="hidden" name="_token"
+                                value="iBdPTkSAy1r2FkYmXRlfaYojRVg8F4oXZcKr5jYP">
+                            <div class="form-group relative">
+                                <input type="email" name="email_subscribe" id="email_subscribe"
+                                    placeholder="Enter mail address..." required=""
+                                    class="w-40 h-12 px-8 py-2 rounded-full bg-transparent text-black border border-white outline-none">
+                                <button type="submit" id="sub-button"
+                                    class="mt-6 h-12 bg-yellow-500 ti-btn-primary-full text-indigo-800 font-semibold rounded-full px-6 uppercase">
+                                    Subscribe Now
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr class="hr">
+        <div class="grid grid-cols-12 gap-6">
+            <div class="xl:col-span-6 col-span-12">
+                <div class="support-link">
+                    <ul class="flex lg:flex-row lg:space-x-4">
+                            <li><a href="#" class="text-white">Support</a></li>
+                            <li><a href="#" class="text-white">Privacy Policy</a></li>
+                            <li><a href="#" class="text-white">Terms Of Service</a></li>
+                            <li><a href="#" class="text-white">Return &amp; Refund Policy</a></li>
+                            <li><a href="#" class="text-white">Sitemap</a></li>
+                        </ul>
+                </div>
+            </div>
+
+            <div class="xl:col-span-6 col-span-12">
+                <div class="footer-widget">
+                    <div class="mb-4 lg:mb-0">
+                    <p class="text-white text-sm text-right">
+                        Copyright © 2024
+                        <a href="https://nighthawk-routerlogin.com/">
+                            <b style="color:#11CDBE;">TTB Antivirus.</b>
+                        </a>
+                        All Rights Reserved
+                    </p>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 </section>
-<div class="text-center landing-main-footer py-4">
-    <span class="text-[#8c9097] text-white text-[0.9375rem]"> Copyright © <span id="year"></span> <a
-        href="https://www.ttbinternetsecurity.com/" class="!text-primary font-semibold"><u>ttbinternetsecurity</u></a>.
-    </span>
-</div>
 
 
 </div>
