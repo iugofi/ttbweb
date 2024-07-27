7c9f38c850872f74132ab0ea8d287b1a8c136e70
“kunalverma320” <kunalverma320@gmail.com>
2024-07-27 12:46:08 +0530

    config\services.php

--- config/services.php
+++ config/services.php
@@ -31,15 +31,20 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'github' => [
-    'client_id' => env('GITHUB_CLIENT_ID'),
-    'client_secret' => env('GITHUB_CLIENT_SECRET'),
-    'redirect' => 'http://127.0.0.1:8000/callback',
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://127.0.0.1:8000/callback',
     ],
 
     'google' => [
-   'client_id' => env('GOOGLE_CLIENT_ID'),
-   'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-   'redirect' => env('GOOGLE_REDIRECT_URL'),
-   ]
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
+    ],
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT_URL'),
+    ]
 
 ];
