00e8fcbcd30a4bb4a48c8b4b27fff777ef4f9e14
“kunalverma320” <kunalverma320@gmail.com>
2024-07-27 11:18:55 +0530

    app\Http\Controllers\ProviderController.php

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -15,21 +15,40 @@ class ProviderController extends Controller
         return Socialite::driver($provider)->redirect();
     }
     public function callback($provider)
-    {
-        $googleUser = Socialite::driver($provider)->user();
-        dd($googleUser);
-        $user = Users::updateOrCreate([
+{
+    $googleUser = Socialite::driver($provider)->user();
+    dd($googleUser);
+
+    $nameParts = explode(' ', $googleUser->name);
+    $firstName = $nameParts[0];
+    $lastName = isset($nameParts[1]) ? $nameParts[1] : '';
+
+    $user = Users::where('email', $googleUser->email)->first();
+
+    if ($user) {
+        // Update existing user
+        $user->update([
             'google_id' => $googleUser->id,
-        ], [
-            'name' => $googleUser->name,
-            'email' =>$googleUser->email,
-            'provider' =>$provider,
+            'firstname' => $firstName,
+            'lastname' => $lastName,
+            'provider' => $provider,
             'google_token' => $googleUser->token,
             'google_refresh_token' => $googleUser->refreshToken,
         ]);
-
-        // dd($user->id);
-
-        return redirect('/home');
+    } else {
+        // Create new user
+        $user = Users::create([
+            'google_id' => $googleUser->id,
+            'firstname' => $firstName,
+            'lastname' => $lastName,
+            'email' => $googleUser->email,
+            'provider' => $provider,
+            'google_token' => $googleUser->token,
+            'google_refresh_token' => $googleUser->refreshToken,
+        ]);
     }
+
+    return redirect('/home');
+}
+
 }
