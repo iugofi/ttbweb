fb52475abbe60928d13925c2da9c4758e6047c9c
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-07-29 09:59:26 +0530

    resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -1340,36 +1340,7 @@
 
             <script>
                 $(document).ready(function() {
-                    const owl = $(".testimonial-content");
-                    owl.owlCarousel({
-                        loop: true,
-                        margin: 8,
-                        dots: false,
-                        nav: false,
-                        mouseDrag: true,
-                        autoplay: true,
-                        autoplayTimeout: 3000,
-                        smartSpeed: 1000,
-                        responsive: {
-                            0: {
-                                items: 1
-                            },
-                            600: {
-                                items: 1
-                            },
-                            1000: {
-                                items: 2
-                            }
-                        }
-                    });
-
-                    $(".owl-prev").click(function() {
-                        owl.trigger('prev.owl.carousel');
-                    });
-
-                    $(".owl-next").click(function() {
-                        owl.trigger('next.owl.carousel');
-                    });
+                    testimonials();
                 });
             </script>
 
