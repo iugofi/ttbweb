a5e5781c29f731dbd07d83612e61393ffc42e434
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-07-29 15:12:39 +0530

    resources\views\User\vpnshield.blade.php resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshield.blade.php
+++ resources/views/User/vpnshield.blade.php
@@ -501,10 +501,7 @@
         </section>
 
 
-        ------------
-
-        <section
-            class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70"
+        <section class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70"
             id="faq" style="overflow: hidden; padding-bottom: 0rem; height: 420px;">
             <div class="container main-banner-container">
                 <section class="testimonial-area">
