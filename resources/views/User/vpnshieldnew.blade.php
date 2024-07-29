fead4e2a4e6f5ae7e6294610dfbfcdbad88bb9f8
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-07-29 15:14:21 +0530

    resources\views\User\vpnshieldnew.blade.php

--- resources/views/User/vpnshieldnew.blade.php
+++ resources/views/User/vpnshieldnew.blade.php
@@ -445,7 +445,7 @@
 
 
 <section class="section !bg-[#f9fafb] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70"
-    id="faq" style="overflow: hidden; padding-bottom: 0rem; height: 420px;">
+    id="faq" style="overflow: hidden; padding-bottom: 3rem; height: 420px;">
     <div class="container main-banner-container">
         <section class="testimonial-area">
             <div class="container">
