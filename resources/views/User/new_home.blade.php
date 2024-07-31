d3b50ee30d7622a3631fe54d995c7e03d38ef921
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-07-31 11:18:12 +0530

    resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -1486,12 +1486,6 @@
 
 
 
-<input type="Name" placeholder="Name" class="form-name" required>
-<input type="password" placeholder="Password" required>
-<a href="https://www.nighthawk-routerlogin.com/"><input type="button" value="Send Here"></a>
-
-
-
 
 
 
