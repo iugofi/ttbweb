8a3945f5c5645e15c312d9333623214e37656ae1
“kunalverma320” <kunalverma320@gmail.com>
2024-08-01 17:41:24 +0530

    resources\views\User\Layouts\footer.blade.php resources\views\User\Layouts\nav.blade.php

--- resources/views/User/Layouts/nav.blade.php
+++ resources/views/User/Layouts/nav.blade.php
@@ -29,6 +29,9 @@
                                 <li class="slide">
                                     <a href="{{route('user.new_home')}}" class="side-menu__item">New-Home</a>
                                 </li>
+                                <li class="slide">
+                                    <div id="google_translate_element"></div>
+                                </li>
                             </ul>
                         </li>
                         <li class="slide has-sub">
@@ -137,7 +140,7 @@
                         </li>
                         @endif
                     </ul>
-                    
+
                 <div class="slide-right hidden" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191"
                         width="24" height="24" viewBox="0 0 24 24">
                         <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z">
@@ -163,7 +166,7 @@
                             class="ri-settings-3-line animate-spin-slow"></i></a>
                 </div>
                 @endif
-              
+
             </nav>
             <!-- End::nav -->
 
