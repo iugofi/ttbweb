f78b2c0cfccb0d92fb26b8f251229b10373e62ee
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-08-05 10:18:42 +0530

    assets\css\style.css resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -976,7 +976,7 @@
 
 
             {{-- forth section start --}}
-            <section class="section-home-new-4">
+            <section class="!bg-[##F3F2EA] dark:!bg-black/10 section-bg text-defaulttextcolor dark:text-defaulttextcolor/70 section-home-new-4">
                 <div class="container">
                     <div class="grid grid-cols-12 gap-6">
                         <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
