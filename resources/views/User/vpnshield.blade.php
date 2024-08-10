94cd9844b3fdc3ff794e1255ff17ce53ad01b885
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-08-10 12:29:44 +0530

    resources\views\User\vpnshield.blade.php

--- resources/views/User/vpnshield.blade.php
+++ resources/views/User/vpnshield.blade.php
@@ -740,7 +740,133 @@
                                 </div>
                             </div>
                             <!-- Repeat the above block for more testimonials -->
+                            <div class="review-btn">
+                                <a class="google-button"
+                                    href="https://www.trustpilot.com/review/ttbinternetsecurity.com?utm_medium=trustbox&utm_source=MicroReviewCount"
+                                    target="_blank">Click here to leave a <strong>google review</strong></a>
+                                <style>
+                                    .google-button {
+                                        display: block;
+                                        margin: 15px auto;
+                                        width: 75%;
+                                        max-width: 208px;
+                                        padding: 4px 10px 4px 65px;
+                                        border: 2px solid #ffffff;
+                                        border-radius: 50px;
+                                        text-transform: uppercase;
+                                        text-decoration: none;
+                                        text-align: center;
+                                        font-size: 10px;
+                                        line-height: 20px;
+                                        color: #ffffff;
+                                        background: url(https://cdn2.hubspot.net/hubfs/1961464/Support%20images/new-google-favicon-512.png) no-repeat left 20px center / 40px 40px;
+                                        -webkit-transition: all 0.3s ease;
+                                        -moz-transition: all 0.3s ease;
+                                        -o-transition: all 0.3s ease;
+                                        transition: all 0.3s ease;
+                                        -webkit-animation: wiggle 2s ease-in infinite;
+                                        -moz-animation: wiggle 2s ease-in infinite;
+                                        -o-animation: wiggle 2s ease-in infinite;
+                                        animation: wiggle 2s ease-in infinite;
+                                    }
 
+                                    .google-button:hover {
+                                        color: #ffffff;
+                                        background-color: #00a0dc;
+                                        background-image: url(https://cdn2.hubspot.net/hubfs/1961464/Support%20images/new-google-favicon-512-white.png);
+                                    }
+
+                                    .google-button strong {
+                                        font-size: 18px;
+                                        display: block;
+                                    }
+
+
+                                    @-webkit-keyframes wiggle {
+
+                                        0%,
+                                        20%,
+                                        100% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        5% {
+                                            background-position: left 15px center;
+                                        }
+
+                                        10% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        15% {
+                                            background-position: left 25px center;
+                                        }
+                                    }
+
+                                    @-moz-keyframes wiggle {
+
+                                        0%,
+                                        20%,
+                                        100% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        5% {
+                                            background-position: left 15px center;
+                                        }
+
+                                        10% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        15% {
+                                            background-position: left 25px center;
+                                        }
+                                    }
+
+                                    @-o-keyframes wiggle {
+
+                                        0%,
+                                        20%,
+                                        100% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        5% {
+                                            background-position: left 15px center;
+                                        }
+
+                                        10% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        15% {
+                                            background-position: left 25px center;
+                                        }
+                                    }
+
+                                    @keyframes wiggle {
+
+                                        0%,
+                                        20%,
+                                        100% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        5% {
+                                            background-position: left 15px center;
+                                        }
+
+                                        10% {
+                                            background-position: left 20px center;
+                                        }
+
+                                        15% {
+                                            background-position: left 25px center;
+                                        }
+                                    }
+                                </style>
+                            </div>
                         </div>
 
 
@@ -749,142 +875,7 @@
                             <button class="owl-next"><i class="fa-solid fa-arrow-right"></i></button>
                         </div>
                     </div>
-                    <div class="review-btn">
-                        <hr>
-                        <a class="google-button"
-                            href="https://www.trustpilot.com/review/ttbinternetsecurity.com?utm_medium=trustbox&utm_source=MicroReviewCount"
-                            target="_blank">Click here to leave a <strong>google review</strong></a>
-                        <style>
-                            hr {
-                                margin: 20px 0px;
-                            }
 
-                            p {
-                                text-align: center;
-                            }
-
-                            .google-button {
-                                display: block;
-                                margin: 15px auto;
-                                width: 75%;
-                                max-width: 208px;
-                                padding: 4px 10px 4px 65px;
-                                border: 2px solid #ffffff;
-                                border-radius: 50px;
-                                text-transform: uppercase;
-                                text-decoration: none;
-                                text-align: center;
-                                font-size: 10px;
-                                line-height: 20px;
-                                color: #ffffff;
-                                background: url(https://cdn2.hubspot.net/hubfs/1961464/Support%20images/new-google-favicon-512.png) no-repeat left 20px center / 40px 40px;
-                                -webkit-transition: all 0.3s ease;
-                                -moz-transition: all 0.3s ease;
-                                -o-transition: all 0.3s ease;
-                                transition: all 0.3s ease;
-                                -webkit-animation: wiggle 2s ease-in infinite;
-                                -moz-animation: wiggle 2s ease-in infinite;
-                                -o-animation: wiggle 2s ease-in infinite;
-                                animation: wiggle 2s ease-in infinite;
-                            }
-
-                            .google-button:hover {
-                                color: #ffffff;
-                                background-color: #00a0dc;
-                                background-image: url(https://cdn2.hubspot.net/hubfs/1961464/Support%20images/new-google-favicon-512-white.png);
-                            }
-
-                            .google-button strong {
-                                font-size: 18px;
-                                display: block;
-                            }
-
-
-                            @-webkit-keyframes wiggle {
-
-                                0%,
-                                20%,
-                                100% {
-                                    background-position: left 20px center;
-                                }
-
-                                5% {
-                                    background-position: left 15px center;
-                                }
-
-                                10% {
-                                    background-position: left 20px center;
-                                }
-
-                                15% {
-                                    background-position: left 25px center;
-                                }
-                            }
-
-                            @-moz-keyframes wiggle {
-
-                                0%,
-                                20%,
-                                100% {
-                                    background-position: left 20px center;
-                                }
-
-                                5% {
-                                    background-position: left 15px center;
-                                }
-
-                                10% {
-                                    background-position: left 20px center;
-                                }
-
-                                15% {
-                                    background-position: left 25px center;
-                                }
-                            }
-
-                            @-o-keyframes wiggle {
-
-                                0%,
-                                20%,
-                                100% {
-                                    background-position: left 20px center;
-                                }
-
-                                5% {
-                                    background-position: left 15px center;
-                                }
-
-                                10% {
-                                    background-position: left 20px center;
-                                }
-
-                                15% {
-                                    background-position: left 25px center;
-                                }
-                            }
-
-                            @keyframes wiggle {
-
-                                0%,
-                                20%,
-                                100% {
-                                    background-position: left 20px center;
-                                }
-
-                                5% {
-                                    background-position: left 15px center;
-                                }
-
-                                10% {
-                                    background-position: left 20px center;
-                                }
-
-                                15% {
-                                    background-position: left 25px center;
-                                }
-                            }
-                        </style>
-                    </div>
             </div>
         </section>
 
