3258bf5076e641cea056dd7eb1a5e7c120803097
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-08-10 10:20:44 +0530

    resources\views\User\vpnshield.blade.php

--- resources/views/User/vpnshield.blade.php
+++ resources/views/User/vpnshield.blade.php
@@ -145,7 +145,7 @@
                                     <div class="box-body !p-0 sliders-box">
                                         <div class="!p-6">
                                             <div class="flex justify-between items-center mb-4">
-                                                <div class="text-[1.125rem] font-semibold m-auto">TTB VPN SHIELD
+                                                <div class="text-[1.125rem] font-semibold m-auto dark-text">TTB VPN SHIELD
                                                     {{ $plandetailsitem->name }}</div>
                                                 <div>
                                                     {{-- <span class="badge bg-success/10 text-success">
