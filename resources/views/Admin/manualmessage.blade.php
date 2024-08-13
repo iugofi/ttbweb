335995ff3158e10a652b34e9a4954f6d013f0a7f
“kunalverma320” <kunalverma320@gmail.com>
2024-08-13 11:22:50 +0530

    resources\views\Admin\manualmessage.blade.php

--- resources/views/Admin/manualmessage.blade.php
+++ resources/views/Admin/manualmessage.blade.php
@@ -196,7 +196,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
 crossorigin="anonymous"></script>
-<script src="{{asset('assets/multiselect-dropdown.js')}}"></script>
+<script src="{{asset('assets/js/multiselect-dropdown.js')}}"></script>
 
 <!-- Latest compiled JavaScript -->
 
