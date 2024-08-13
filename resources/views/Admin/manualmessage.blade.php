1f07c047635c48d0b9153345cb736a8c06b6a75a
“kunalverma320” <kunalverma320@gmail.com>
2024-08-13 09:45:59 +0530

    resources\views\Admin\manualmessage.blade.php

--- resources/views/Admin/manualmessage.blade.php
+++ resources/views/Admin/manualmessage.blade.php
@@ -60,13 +60,13 @@
 
                                 <div class="xl:col-span-12 col-span-12">
                                     <label class="form-label">Email Content</label>
-                                    <textarea name="blog_description" id="editoronebest" height="210"></textarea>
+                                    <textarea name="blog_description" id="editoronebest" height="210px"></textarea>
                                     <div class="invalid-feedback"></div>
                                 </div>
 
                                 <div class="xl:col-span-12 col-span-12">
                                     <label class="form-label">Email Id</label>
-                                    <textarea class="form-control block w-full" height="210"></textarea>
+                                    <textarea class="form-control block w-full" height="210px"></textarea>
                                     <div class="invalid-feedback"></div>
                                 </div>
 
