b4e67284f2fe80cc2161e34340677b9325b47962
“kunalverma320” <kunalverma320@gmail.com>
2024-08-13 11:21:34 +0530

    resources\views\Admin\manualmessage.blade.php assets\js\multiselect-dropdown.js

--- resources/views/Admin/manualmessage.blade.php
+++ resources/views/Admin/manualmessage.blade.php
@@ -80,6 +80,76 @@
                                     <textarea name="blog_description" id="editoronebest" class="typing-text"></textarea>
                                     <div class="invalid-feedback"></div>
                                 </div>
+                                <div class="xl:col-span-12 col-span-12">
+                                    <label class="form-label">Email Id</label>
+                                    <select name="field2" id="field2" multiple multiselect-search="true" multiselect-select-all="true"
+                                    multiselect-max-items="3" onchange="console.log(this.selectedOptions)">
+                                    <option>Abarth</option>
+                                    <option>Alfa Romeo</option>
+                                    <option>Aston Martin</option>
+                                    <option>Audi</option>
+                                    <option>Bentley</option>
+                                    <option>BMW</option>
+                                    <option>Bugatti</option>
+                                    <option>Cadillac</option>
+                                    <option>Chevrolet</option>
+                                    <option>Chrysler</option>
+                                    <option>Citroën</option>
+                                    <option>Dacia</option>
+                                    <option>Daewoo</option>
+                                    <option>Daihatsu</option>
+                                    <option>Dodge</option>
+                                    <option>Donkervoort</option>
+                                    <option>DS</option>
+                                    <option>Ferrari</option>
+                                    <option>Fiat</option>
+                                    <option>Fisker</option>
+                                    <option>Ford</option>
+                                    <option>Honda</option>
+                                    <option>Hummer</option>
+                                    <option>Hyundai</option>
+                                    <option>Infiniti</option>
+                                    <option>Iveco</option>
+                                    <option>Jaguar</option>
+                                    <option>Jeep</option>
+                                    <option>Kia</option>
+                                    <option>KTM</option>
+                                    <option>Lada</option>
+                                    <option>Lamborghini</option>
+                                    <option>Lancia</option>
+                                    <option>Land Rover</option>
+                                    <option>Landwind</option>
+                                    <option>Lexus</option>
+                                    <option>Lotus</option>
+                                    <option>Maserati</option>
+                                    <option>Maybach</option>
+                                    <option>Mazda</option>
+                                    <option>McLaren</option>
+                                    <option>Mercedes-Benz</option>
+                                    <option>MG</option>
+                                    <option>Mini</option>
+                                    <option>Mitsubishi</option>
+                                    <option>Morgan</option>
+                                    <option>Nissan</option>
+                                    <option>Opel</option>
+                                    <option>Peugeot</option>
+                                    <option>Porsche</option>
+                                    <option>Renault</option>
+                                    <option>Rolls-Royce</option>
+                                    <option>Rover</option>
+                                    <option>Saab</option>
+                                    <option>Seat</option>
+                                    <option>Skoda</option>
+                                    <option>Smart</option>
+                                    <option>SsangYong</option>
+                                    <option>Subaru</option>
+                                    <option>Suzuki</option>
+                                    <option>Tesla</option>
+                                    <option>Toyota</option>
+                                    <option>Volkswagen</option>
+                                    <option>Volvo</option>
+                                  </select>
+                                </div>
 
                                 <div class="xl:col-span-12 col-span-12">
                                     {{-- <label class="form-label">Email Id</label> --}}
@@ -123,6 +193,10 @@
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
+crossorigin="anonymous"></script>
+<script src="{{asset('assets/multiselect-dropdown.js')}}"></script>
 
 <!-- Latest compiled JavaScript -->
 
