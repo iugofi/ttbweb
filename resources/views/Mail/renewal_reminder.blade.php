81740c67400a2fb397da770089a5cea0d14a5217
“kunalverma320” <kunalverma320@gmail.com>
2024-08-23 14:45:03 +0530

    app\Console\Commands\SendRenewalReminder.php app\Console\Kernel.php app\Mail\SubscriptionRenewalReminder.php resources\views\Mail\renewal_reminder.blade.php

--- resources/views/Mail/renewal_reminder.blade.php
+++ resources/views/Mail/renewal_reminder.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <p>Dear User,</p>
-    <p>Your subscription is expiring soon! The expiration date is {{ $subscription->key_expirey_date }}. Please renew your subscription to continue enjoying our services.</p>
+    <p>Your subscription is expiring soon! The expiration date is . Please renew your subscription to continue enjoying our services.</p>
     <p>Thank you for your attention.</p>
 </body>
 </html>
