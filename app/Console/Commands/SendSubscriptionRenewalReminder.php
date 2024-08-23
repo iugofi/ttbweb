22451c1b6c4690007b11e4a7b1200d44d91cfb12
“kunalverma320” <kunalverma320@gmail.com>
2024-08-23 13:26:46 +0530

    app\Console\Commands\SendSubscriptionRenewalReminder.php

--- app/Console/Commands/SendSubscriptionRenewalReminder.php
+++ app/Console/Commands/SendSubscriptionRenewalReminder.php
@@ -33,29 +33,32 @@ class SendSubscriptionRenewalReminder extends Command
         parent::__construct();
     }
     public function handle()
-    {
-        $users = DB::table('payments')
-            ->join('ttbkey', 'payments.product_id', '=', 'ttbkey.product_id')
-            ->join('usersall', 'usersall.id', '=', 'payments.user_id')
-            ->select(
-                'payments.user_id',
-                'payments.product_id',
-                'usersall.email',
-                'ttbkey.key_activation_date',
-                'ttbkey.key_expirey_date',
-                DB::raw('DATEDIFF(ttbkey.key_expirey_date, CURDATE()) AS day_difference')
-            )
-            ->where('ttbkey.is_key_used', 1)
-            ->having('day_difference', 1)
-            ->get();
+{
+    $users = DB::table('payments')
+        ->join('ttbkey', 'payments.product_id', '=', 'ttbkey.product_id')
+        ->join('usersall', 'usersall.id', '=', 'payments.user_id')
+        ->select(
+            'payments.user_id',
+            'payments.product_id',
+            'usersall.email',
+            'ttbkey.key_activation_date',
+            'ttbkey.key_expirey_date',
+            DB::raw('DATEDIFF(ttbkey.key_expirey_date, CURDATE()) AS day_difference')
+        )
+        ->where('ttbkey.is_key_used', 1)
+        ->having('day_difference', 1)
+        ->get();
 
-        foreach ($users as $user) {
-            Mail::raw('Your subscription is about to expire. Please renew it to continue enjoying our services.', function ($message) use ($user) {
-                $message->to($user->email)
-                        ->subject('Subscription Renewal Reminder');
-            });
-        }
+    foreach ($users as $user) {
+        \Log::info('Sending renewal reminder to: ' . $user->email);
 
-        $this->info('Subscription renewal reminders sent successfully.');
+        Mail::raw('Your subscription is about to expire. Please renew it to continue enjoying our services.', function ($message) use ($user) {
+            $message->to($user->email)
+                    ->subject('Subscription Renewal Reminder');
+        });
     }
+
+    $this->info('Subscription renewal reminders sent successfully.');
+}
+
 }
