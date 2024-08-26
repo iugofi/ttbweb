df2339bfb024f604ab93208b9bd48765ffa8e922
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-08-26 11:50:06 +0530

    resources\views\User\endpoint_protection.blade.php

--- resources/views/User/endpoint_protection.blade.php
+++ resources/views/User/endpoint_protection.blade.php
@@ -223,9 +223,9 @@
                     </div>
 
                     <!-- jQuery -->
-                    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
                     <!-- Owl Carousel JS -->
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+                    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script> --}}
                     <!-- Initialize Owl Carousel -->
                     <script>
                         $(document).ready(function() {
