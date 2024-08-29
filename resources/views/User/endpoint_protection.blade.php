69d2e616204ef11d7557c7f99b6bd4ce277c2ed1
kulvinder pathania <kulvinder.iugofi@gmail.com>
2024-08-29 11:49:52 +0530

    resources\views\User\endpoint_protection.blade.php

--- resources/views/User/endpoint_protection.blade.php
+++ resources/views/User/endpoint_protection.blade.php
@@ -409,7 +409,7 @@
                 </div>
                 <div class="grid grid-cols-12 gap-6">
                     <div class="xxl:col-span-6 xl:col-span-6 lg:col-span-6 col-span-12">
-                        <div class="mt-3 flex items-start text-sm text-black font-semibold">
+                        <div class="mt-3 flex items-start text-black text-base text-black">
                             <i class="bx bx-check-double mt-1 mr-2 me-3"></i>
                             <p>Provides you with remote management and virtual infrastructure monitoring.</p>
                         </div>
