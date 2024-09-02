3671946e9d747e69c4b7e695a4e2bf47174314fb
neeraj <tbs.neeraj@gmail.com>
2024-09-02 11:25:48 +0530

    resources\views\User\networkprotection.blade.php

--- resources/views/User/networkprotection.blade.php
+++ resources/views/User/networkprotection.blade.php
@@ -31,14 +31,33 @@
                                         <span class="dark-black">18,249
                                             Reviews on</span><i class="fa-solid fa-star"></i></a><a
                                         href="https://www.av-test.org/en/antivirus/mobile-devices/manufacturer/ttb/"
-                                        target="_blank"><img class="logo-av1" src="{{ asset('assets/home-two-img/AV-Test.png') }}"></a>
+                                        target="_blank"><img class="logo-av1"
+                                            src="{{ asset('assets/home-two-img/AV-Test.png') }}"></a>
                                 </div>
 
                             </div>
                         </div>
                     </div>
                 </div>
+
+            </section class="">
+            <div class="container">
+                <div class="grid grid-cols-12 gap-6">
+                    <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
+                        <h2>Introduction</h2>
+                        <p>Generally, network protection involves multi-layered defense to keep the users, infrastructure,
+                            and networking components protected from misuse & unauthorized access. Likewise, in this current
+                            threat landscape, TTB helps protect your network from such threats.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <section>
+
             </section>
+
+
+
         </div>
 
 
