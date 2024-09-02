903e7bdc6a0c5579a0a872a026988ec0064e9dc0
neeraj <tbs.neeraj@gmail.com>
2024-09-02 11:42:46 +0530

    resources\views\User\networkprotection.blade.php

--- resources/views/User/networkprotection.blade.php
+++ resources/views/User/networkprotection.blade.php
@@ -19,7 +19,7 @@
                 <div class="container network-container">
                     <div class="grid grid-cols-12 gap-6">
                         <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
-                            <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 10rem;">
+                            <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 12rem;">
                                 <h1 class="text-left font-semibold mb-2 text-[2rem] text-center" style="color: #fff;">Secure
                                     Your Network Perimeters Using <br> Our Enhanced Security Solution <br> <span
                                         class="text-color-ttb heading-style">TTB Network Protection</span></h1>
