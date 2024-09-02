825e63e95f4bb963c19747f7b4736990848a89b4
neeraj <tbs.neeraj@gmail.com>
2024-09-02 10:31:47 +0530

    assets\css\style.css resources\views\User\networkprotection.blade.php

--- resources/views/User/networkprotection.blade.php
+++ resources/views/User/networkprotection.blade.php
@@ -20,7 +20,7 @@
                     <div class="grid grid-cols-12 gap-6">
                         <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 col-span-12">
                             <div class="lg:py-[3rem] text-vartical" style="position: relative; top: 10rem;">
-                                <h1 class="text-left font-semibold mb-2 text-[2rem] text-center" style="color: #fff;">Secure Your Network Perimeters Using <br> Our Enhanced Security Solution <br>TTB Network Protection</h1>
+                                <h1 class="text-left font-semibold mb-2 text-[2rem] text-center" style="color: #fff;">Secure Your Network Perimeters Using <br> Our Enhanced Security Solution <br> <span>TTB Network Protection</span></h1>
                                 <div class="spacing-top-cl"><a
                                     href="https://www.trustpilot.com/review/ttbinternetsecurity.com?utm_medium=trustbox&utm_source=MicroReviewCount"
                                     target="_blank">
