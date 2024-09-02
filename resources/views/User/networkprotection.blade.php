b513eca992ad6530d569ea791478c5454f95f31f
neeraj <tbs.neeraj@gmail.com>
2024-09-02 11:28:49 +0530

    resources\views\User\networkprotection.blade.php

--- resources/views/User/networkprotection.blade.php
+++ resources/views/User/networkprotection.blade.php
@@ -40,7 +40,10 @@
                     </div>
                 </div>
 
-            </section class="net-prot-sec-two">
+            </section>
+
+        </div>
+        <section class="net-prot-sec-two">
             <div class="container">
                 <div class="grid grid-cols-12 gap-6">
                     <div class="xxl:col-span-12 xl:col-span-12 lg:col-span-12 md:col-span-12 col-span-12 text-center">
@@ -51,14 +54,11 @@
                         </p>
                     </div>
                 </div>
-            </div>
-            <section>
-
-            </section>
+        </section>
 
 
 
-        </div>
+    </div>
 
 
 
