1d76a13bd5c11d3ca38ed673f61309bf5eeef34b
“kunalverma320” <kunalverma320@gmail.com>
2024-09-06 12:30:29 +0530

    app\Http\Controllers\AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -50,7 +50,7 @@ class AdminController extends Controller
     public function getTreemapData()
     {
 
-        $data = DB::table('payments')->select('city as x', 'id as y')->distinct()->get();
+        $data = DB::table('payments')->select('city as x', 'id as y')->groupby('city')->get();
 
 
         return response()->json($data);
