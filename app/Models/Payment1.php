afa5845ce1ca30f1d98ed79313fba58c6bc1d94c
“kunalverma320” <kunalverma320@gmail.com>
2024-09-07 10:38:28 +0530

    app\Http\Controllers\PaymentController.php app\Models\Payment1.php

---
+++ app/Models/Payment1.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment1 extends Model
+{
+    protected $table = 'payments1';
+    protected $fillable = [
+        'id',
+        'description',
+        'price',
+        'discount',
+        'coupons',
+        'product_id',
+        'email',
+        'firstname',
+        'lastname',
+        'phoneno',
+        'pincode',
+        'city',
+        'state',
+        'country',
+        'address',
+        'same_as_shipping',
+        'payment_method',
+        'payment_status',
+        'transaction_id',
+        'payment_time',
+    ];
+    use HasFactory;
+}
