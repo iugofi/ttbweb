dd149670a7c4d80816105a3940af36197580d90e
“kunalverma320” <kunalverma320@gmail.com>
2024-09-11 16:11:04 +0530

    config\stripe.php

--- config/stripe.php
+++ config/stripe.php
@@ -2,8 +2,8 @@
 use Illuminate\Support\Facades\DB;
 $stripeKeys = DB::table('api_keys')->where('service_name', 'stripe')->first();
 return [
-    'stripe_pk' => $stripeKeys ? $stripeKeys->public_key : env('STRIPE_KEY'),
-    'stripe_sk' => $stripeKeys ? $stripeKeys->secret_key : env('STRIPE_SECRET'),
+    'stripe_pk' => $stripeKeys ? $stripeKeys->key_name : env('STRIPE_KEY'),
+    'stripe_sk' => $stripeKeys ? $stripeKeys->key_value : env('STRIPE_SECRET'),
 ];
 
 
