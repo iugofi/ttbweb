bd5d02d24abd35fd09840185b3b231d493c3a44c
“kunalverma320” <kunalverma320@gmail.com>
2024-09-19 10:38:56 +0530

    resources\views\Admin\payshow.blade.php

--- resources/views/Admin/payshow.blade.php
+++ resources/views/Admin/payshow.blade.php
@@ -192,11 +192,11 @@
 
                                                 <td class="border px-4 py-2">
                                                     <div class="flex flex-row items-center !gap-2 text-[0.9375rem]">
-                                                        <a aria-label="anchor"
+                                                        {{-- <a aria-label="anchor"
                                                             href="{{ route('edit.payedit', \Crypt::encrypt($item->id)) }}"
                                                             class="ti-btn ti-btn-icon ti-btn-wave !gap-0 !m-0 !h-[1.75rem] !w-[1.75rem] text-[0.8rem] bg-primary/10 text-primary hover:bg-primary hover:text-white hover:border-primary">
                                                             <i class="ri-edit-line"></i>
-                                                        </a>
+                                                        </a> --}}
                                                         <button aria-label="button" type="button"
                                                             class="ti-btn !py-1 !px-2 !text-[0.75rem] ti-btn-danger-full btn-wave delete-vpnpay"
                                                             data-item-id="{{ $item->id }}">
