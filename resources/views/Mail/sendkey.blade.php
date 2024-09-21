37355edf67539b872cf3cdf1ccf5a4d179488064
“kunalverma320” <kunalverma320@gmail.com>
2024-09-21 17:31:07 +0530

    resources\views\Mail\sendkey.blade.php

--- resources/views/Mail/sendkey.blade.php
+++ resources/views/Mail/sendkey.blade.php
@@ -1,8 +1,8 @@
 
-{{$paymentDetails->ttb_main_key}}
 
 
-{{-- <!DOCTYPE html
+
+<!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
 
@@ -941,4 +941,4 @@
     </div>
 </body>
 
-</html> --}}
+</html>
