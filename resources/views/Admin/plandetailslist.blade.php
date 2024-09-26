efce1cce21afbe2aa74b97399cf9ac19c049f51c
“kunalverma320” <kunalverma320@gmail.com>
2024-09-26 12:16:37 +0530

    resources\views\Admin\plandetailslist.blade.php

--- resources/views/Admin/plandetailslist.blade.php
+++ resources/views/Admin/plandetailslist.blade.php
@@ -242,7 +242,7 @@
                                         '<td class="border px-4 py-2">' + item.discount +
                                         '</td>' +
                                         '<td class="border">' + item.coupons + '</td>' +
-                                        '<td class="border">' + item.is_coupons + '</td>' +
+                                        '<td class="border">' + item.id + '</td>' +
                                         '<td class="border px-4 py-2">' +
                                         '<div class="flex flex-row items-center !gap-2 text-[0.9375rem]">' +
                                         '<a aria-label="anchor" href="{{ route('edit.adminuseredit', ':item.id') }}" class="ti-btn ti-btn-icon ti-btn-wave !gap-0 !m-0 !h-[1.75rem] !w-[1.75rem] text-[0.8rem] bg-primary/10 text-primary hover:bg-primary hover:text-white hover:border-primary">' +
