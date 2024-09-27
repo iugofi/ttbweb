85f68ef8cd7178e09f23a743be4c263d6fa85086
kunal <kunal.iugofi@gmail.com>
2024-09-27 12:50:15 +0530

    resources\views\Admin\index.blade.php

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -575,6 +575,82 @@
                 }
             });
 
+            var options = {
+        series: [{
+            name: 'Website Blog',
+            type: 'column',
+            data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
+        }, {
+            name: 'Social Media',
+            type: 'line',
+            data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
+        }],
+        chart: {
+            height: 320,
+            type: 'line',
+        },
+        stroke: {
+            width: [0, 4]
+        },
+        grid: {
+            borderColor: '#f2f5f7',
+        },
+        title: {
+            text: 'Traffic Sources',
+            align: 'left',
+            style: {
+                fontSize: '13px',
+                fontWeight: 'bold',
+                color: '#8c9097'
+            },
+        },
+        dataLabels: {
+            enabled: true,
+            enabledOnSeries: [1]
+        },
+        colors: ["#845adf", "#23b7e5"],
+        labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
+        xaxis: {
+            type: 'datetime',
+            labels: {
+                show: true,
+                style: {
+                    colors: "#8c9097",
+                    fontSize: '11px',
+                    fontWeight: 600,
+                    cssClass: 'apexcharts-xaxis-label',
+                },
+            }
+        },
+        yaxis: [{
+            title: {
+                text: 'Website Blog',
+                style: {
+                    color: "#8c9097",
+                }
+            },
+            labels: {
+                show: true,
+                style: {
+                    colors: "#8c9097",
+                    fontSize: '11px',
+                    fontWeight: 600,
+                    cssClass: 'apexcharts-yaxis-label',
+                },
+            }
+        }, {
+            opposite: true,
+            title: {
+                text: 'Social Media',
+                style: {
+                    color: "#8c9097",
+                }
+            }
+        }]
+    };
+    var chart = new ApexCharts(document.querySelector("#mixed-linecolumn"), options);
+    chart.render();
+
 
 
 
