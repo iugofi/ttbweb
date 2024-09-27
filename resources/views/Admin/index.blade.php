e3a762599a094ee8c4870804956a9c172159b7a3
kunal <kunal.iugofi@gmail.com>
2024-09-27 11:50:13 +0530

    resources\views\Admin\index.blade.php

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -317,12 +317,13 @@
                                 <div class="xxl:col-span-12 xl:col-span-12 col-span-12">
                                     <div class="box custom-box">
                                         <div class="box-header">
-                                            <div class="box-title">User City</div>
+                                            <div class="box-title">Monthly User Register Data</div>
                                         </div>
                                         <div class="box-body">
-                                            <div id="treemap-multis"></div>
+                                            <div id="line-chart"></div>
                                         </div>
                                     </div>
+
                                     {{-- <div class="box">
                                     <div class="box-header !gap-0 !m-0 justify-between">
                                         <div class="box-title">
@@ -356,10 +357,10 @@
                         <div class="xxl:col-span-12 xl:col-span-12 col-span-12">
                             <div class="box custom-box">
                                 <div class="box-header">
-                                    <div class="box-title">Monthly User Register Data</div>
+                                    <div class="box-title">User City</div>
                                 </div>
                                 <div class="box-body">
-                                    <div id="line-chart"></div>
+                                    <div id="treemap-multis"></div>
                                 </div>
                             </div>
                         </div>
@@ -454,11 +455,9 @@
                                     </div>
                                 </div>
                             </div>
-
-
                         </div>
 
-                       
+
                     </div>
                 </div>
             </div>
