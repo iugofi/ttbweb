974a28d72e04854befa824470be083e5c79ab38c
kunal <kunal.iugofi@gmail.com>
2024-09-27 13:07:46 +0530

    app\Http\Controllers\AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -278,13 +278,21 @@ class AdminController extends Controller
     public function getChartDatanb()
     {
 
-        $data = [
-            'blog' => [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160],
-            'news' => [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16],
-            'labels' => ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001',
-                         '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001',
-                         '11 Jan 2001', '12 Jan 2001']
-        ];
+        $blogData = Blog::select('created_at', 'likes')
+        ->orderBy('created_at')
+        ->get();
+
+    $newsData = News::select('created_at', 'likes')
+        ->orderBy('created_at')
+        ->get();
+
+    $data = [
+        'blog' => $blogData->pluck('likes')->toArray(),
+        'news' => $newsData->pluck('likes')->toArray(),
+        'labels' => $blogData->pluck('created_at')->map(function($date) {
+            return date('d M Y', strtotime($date));
+        })->toArray()
+    ];
 
         return response()->json($data);
     }
