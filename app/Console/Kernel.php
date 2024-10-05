575780a0903deafa0d7bc0155f15f66120869c80
“kunalverma320” <kunalverma320@gmail.com>
2024-10-05 12:43:43 +0530

    app\Console\Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
 
      protected $commands =[
         Commands\SendRenewalReminder::class,
+        Commands\GenerateSitemap::class,
      ];
     protected function schedule(Schedule $schedule)
     {
