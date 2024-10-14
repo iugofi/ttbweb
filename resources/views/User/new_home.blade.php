5129d19a14246bc50f94e4d34cfe8fc7cccc8083
neeraj <tbs.neeraj@gmail.com>
2024-10-14 11:09:50 +0530

    resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -90,12 +90,12 @@
                                                         href="https://www.trustpilot.com/review/ttbinternetsecurity.com?utm_medium=trustbox&utm_source=MicroReviewCount"
                                                         target="_blank">
                                                         <span class="dark-black">Excellent</span><img
-                                                            src="{{ asset('assets/home-two-img/Review.png') }}">
+                                                            src="{{ asset('assets/home-two-img/Review.png') }}" class="img-fluid">
                                                         <span class="dark-black">18,249
                                                             Reviews on</span><i class="fa-solid fa-star"></i></a><a
                                                         href="https://www.av-test.org/en/antivirus/mobile-devices/manufacturer/ttb/"
                                                         target="_blank"><img
-                                                            src="{{ asset('assets/home-two-img/AV-Test.png') }}"></a>
+                                                            src="{{ asset('assets/home-two-img/AV-Test.png') }}" class="img-fluid"></a>
                                                 </div>
                                             </div>
                                         </div>
