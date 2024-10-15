f7cd0dd8d7831bb041e0740a4ec04372ab9f1a19
“kunalverma320” <kunalverma320@gmail.com>
2024-10-15 12:10:36 +0530

    resources\views\User\new_home.blade.php

--- resources/views/User/new_home.blade.php
+++ resources/views/User/new_home.blade.php
@@ -467,6 +467,13 @@
                                                                     <p> <del>@php
 
                                                                         $originalPrice = $antivirusitem->price;
+                                                                        if ($antivirusitem->plan_id==604) {
+                                                                            $discountAmount =
+                                                                            ($originalPrice *
+                                                                                $antivirusitem->discount_percent + 10) /
+                                                                            100;
+                                                                        }
+
                                                                         $discountAmount =
                                                                             ($originalPrice *
                                                                                 $antivirusitem->discount_percent) /
